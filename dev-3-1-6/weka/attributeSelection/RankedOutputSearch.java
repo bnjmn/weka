17b1a705221db7bbb0766be080ee881f9d42ba80
(no author) <(no author)@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
1999-11-14 23:10:17 +0000

    This commit was manufactured by cvs2svn to create tag 'dev-3-1-6'.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/tags@327 e0a1b77d-ad91-4216-81b1-defd5f83fa92

---
+++ dev-3-1-6/weka/attributeSelection/RankedOutputSearch.java
@@ -0,0 +1,81 @@
+/*
+ *    RankedOutputSearch.java
+ *    Copyright (C) 1999 Mark Hall
+ *
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+package weka.attributeSelection;
+import java.io.*;
+import weka.core.*;
+
+
+/** 
+ * Interface for search methods capable of producing a
+ * ranked list of attributes.
+ *
+ * @author Mark Hall (mhall@cs.waikato.ac.nz)
+ * @version $Revision 1.1 $
+ */
+public interface RankedOutputSearch {
+
+
+  // ===============
+  // Public methods.
+  // ===============
+  
+  /**
+   * Returns a X by 2 list of attribute indexes and corresponding
+   * evaluations from best (highest) to worst.
+   * @return the ranked list of attribute indexes in an array of ints
+   * @exception Exception if the ranking can't be produced
+   */
+  public abstract double[][] rankedAttributes() throws Exception;
+
+  /**
+   * Sets a threshold by which attributes can be discarded from the
+   * ranking. This threshold is used by the AttributeSelection module
+   * which does the actual discarding of attributes---the implementer
+   * of this method needs only to provide a variable in which to store the
+   * supplied threshold.
+   * @param threshold the threshold.
+   */
+  public abstract void setThreshold(double threshold);
+
+  /**
+   * Gets the threshold by which attributes can be discarded. Discarding
+   * of attributes is done by the AttributeSelection module using the
+   * threshold returned by this method.
+   * @return a threshold by which to discard attributes
+   */
+  public abstract double getThreshold();
+
+  /**
+   * Sets whether or not ranking is to be performed.
+   * When a search method is capable of producing a ranked list
+   * of attributes, the user has the choice of seeing the results of a
+   * normal search or seeing a ranked list.
+   * @param doRanking true if ranked list is to be produced
+   */
+  public abstract void setGenerateRanking(boolean doRanking);
+
+  /**
+   * Gets whether the user has opted to see a ranked list of
+   * attributes rather than the normal result of the search
+   * @return true if a ranked list has been requested.
+   */
+  public abstract boolean getGenerateRanking();
+
+}
---
+++ dev-3-1-6/weka/gui/experiment/DatasetListPanel.java
@@ -0,0 +1,208 @@
+/*
+ *    DatasetListPanel.java
+ *    Copyright (C) 1999 Len Trigg
+ *
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+package weka.gui.experiment;
+
+import weka.experiment.Experiment;
+
+import weka.gui.ExtensionFileFilter;
+import java.io.File;
+import java.awt.Component;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.ActionListener;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JFrame;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.BorderFactory;
+import javax.swing.DefaultListModel;
+import javax.swing.JScrollPane;
+import javax.swing.JList;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
+
+
+/** 
+ * This panel controls setting a list of datasets for an experiment to
+ * iterate over.
+ *
+ * @author Len Trigg (trigg@cs.waikato.ac.nz)
+ * @version $Revision: 1.5 $
+ */
+public class DatasetListPanel extends JPanel implements ActionListener {
+
+  /** The experiment to set the dataset list of */
+  protected Experiment m_Exp;
+
+  /** The component displaying the dataset list */
+  protected JList m_List;
+
+  /** Click to add a dataset */
+  protected JButton m_AddBut = new JButton("Add new...");
+
+  /** Click to remove the selected dataset from the list */
+  protected JButton m_DeleteBut = new JButton("Delete selected");
+
+  /** A filter to ensure only arff files get selected */
+  protected FileFilter m_ArffFilter =
+    new ExtensionFileFilter(".arff", "Arff data files");
+
+  /** The file chooser component */
+  protected JFileChooser m_FileChooser = new JFileChooser();
+
+  
+  /**
+   * Creates the dataset list panel with the given experiment.
+   *
+   * @param exp a value of type 'Experiment'
+   */
+  public DatasetListPanel(Experiment exp) {
+
+    this();
+    setExperiment(exp);
+  }
+
+  /**
+   * Create the dataset list panel initially disabled.
+   */
+  public DatasetListPanel() {
+    
+    m_List = new JList();
+    m_FileChooser.setFileFilter(m_ArffFilter);
+    // Multiselection isn't handled by the current implementation of the
+    // swing look and feels.
+    // m_FileChooser.setMultiSelectionEnabled(true);
+    m_FileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+    m_DeleteBut.setEnabled(false);
+    m_DeleteBut.addActionListener(this);
+    m_AddBut.setEnabled(false);
+    m_AddBut.addActionListener(this);
+    setLayout(new BorderLayout());
+    setBorder(BorderFactory.createTitledBorder("Datasets"));
+    JPanel topLab = new JPanel();
+    GridBagLayout gb = new GridBagLayout();
+    GridBagConstraints constraints = new GridBagConstraints();
+    topLab.setBorder(BorderFactory.createEmptyBorder(10, 5, 10, 5));
+    //    topLab.setLayout(new GridLayout(1,2,5,5));
+    topLab.setLayout(gb);
+    constraints.gridx=0;constraints.gridy=0;constraints.weightx=5;
+    constraints.fill = GridBagConstraints.HORIZONTAL;
+    constraints.gridwidth=1;constraints.gridheight=1;
+    constraints.insets = new Insets(0,2,0,2);
+    topLab.add(m_AddBut,constraints);
+    constraints.gridx=1;constraints.gridy=0;constraints.weightx=5;
+    constraints.gridwidth=1;constraints.gridheight=1;
+    topLab.add(m_DeleteBut,constraints);
+    add(topLab, BorderLayout.NORTH);
+    add(new JScrollPane(m_List), BorderLayout.CENTER);
+  }
+
+  /**
+   * Tells the panel to act on a new experiment.
+   *
+   * @param exp a value of type 'Experiment'
+   */
+  public void setExperiment(Experiment exp) {
+
+    m_Exp = exp;
+    m_AddBut.setEnabled(true);
+    m_List.setModel(m_Exp.getDatasets());
+  }
+  
+  /**
+   * Handle actions when buttons get pressed.
+   *
+   * @param e a value of type 'ActionEvent'
+   */
+  public void actionPerformed(ActionEvent e) {
+
+    if (e.getSource() == m_AddBut) {
+      // Let the user select an arff file from a file chooser
+      int returnVal = m_FileChooser.showOpenDialog(this);
+      if(returnVal == JFileChooser.APPROVE_OPTION) {
+	if (m_FileChooser.isMultiSelectionEnabled()) {
+	  File [] selected = m_FileChooser.getSelectedFiles();
+	  for (int i = 0; i < selected.length; i++) {
+	    m_Exp.getDatasets().addElement(selected[i]);
+	  }
+	  m_DeleteBut.setEnabled(true);
+	} else {
+	  m_Exp.getDatasets().addElement(m_FileChooser.getSelectedFile());
+	  m_DeleteBut.setEnabled(true);
+	}
+      }
+    } else if (e.getSource() == m_DeleteBut) {
+      // Delete the selected files
+      int [] selected = m_List.getSelectedIndices();
+      if (selected != null) {
+	for (int i = 0; i < selected.length; i++) {
+	  int current = selected[i];
+	  m_Exp.getDatasets().removeElementAt(current);
+	  if (m_Exp.getDatasets().size() > current) {
+	    m_List.setSelectedIndex(current);
+	  }
+	}
+      }
+      if (m_List.getSelectedIndex() == -1) {
+	m_DeleteBut.setEnabled(false);
+      }
+    }
+  }
+  
+  /**
+   * Tests out the dataset list panel from the command line.
+   *
+   * @param args ignored
+   */
+  public static void main(String [] args) {
+
+    try {
+      final JFrame jf = new JFrame("Dataset List Editor");
+      jf.getContentPane().setLayout(new BorderLayout());
+      DatasetListPanel dp = new DatasetListPanel();
+      jf.getContentPane().add(dp,
+			      BorderLayout.CENTER);
+      jf.addWindowListener(new WindowAdapter() {
+	public void windowClosing(WindowEvent e) {
+	  jf.dispose();
+	  System.exit(0);
+	}
+      });
+      jf.pack();
+      jf.setVisible(true);
+      System.err.println("Short nap");
+      Thread.currentThread().sleep(3000);
+      System.err.println("Done");
+      dp.setExperiment(new Experiment());
+    } catch (Exception ex) {
+      ex.printStackTrace();
+      System.err.println(ex.getMessage());
+    }
+  }
+}
