6094c17533a6d9d6f531477c5ad1e51b30887efb
trigg <trigg@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2000-06-13 23:54:20 +0000

    Added a bunch of new filters (yippee!). Renamed one existing one. Updated the GenericObjectEditor.props for these changes.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@690 e0a1b77d-ad91-4216-81b1-defd5f83fa92

---
+++ weka/filters/SparseToNonSparseFilter.java
@@ -0,0 +1,117 @@
+/*
+ *    SparseToNonSparseFilter.java
+ *    Copyright (C) 2000 Intelligenesis Corp.
+ *
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+package weka.filters;
+import weka.core.Instances;
+import weka.core.SparseInstance;
+import weka.core.Instance;
+import weka.core.Utils;
+
+
+/** 
+ * A filter that converts all incoming sparse instances into 
+ * non-sparse format.
+ *
+ * @author Len Trigg (len@intelligenesis.net)
+ * @version $Revision: 1.1 $ 
+ */
+public class SparseToNonSparseFilter extends Filter {
+
+  /**
+   * Returns a string describing this filter
+   *
+   * @return a description of the filter suitable for
+   * displaying in the explorer/experimenter gui
+   */
+  public String globalInfo() {
+    return "An instance filter that converts all incoming sparse instances"
+      + " into non-sparse format.";
+  }
+
+  /**
+   * Sets the format of the input instances.
+   *
+   * @param instanceInfo an Instances object containing the input instance
+   * structure (any instances contained in the object are ignored - only the
+   * structure is required).
+   * @return true if the outputFormat may be collected immediately
+   */
+  public boolean inputFormat(Instances instanceInfo) {
+
+    m_InputFormat = new Instances(instanceInfo, 0);
+    setOutputFormat(m_InputFormat);
+    m_NewBatch = true;
+    return true;
+  }
+
+
+  /**
+   * Input an instance for filtering. Ordinarily the instance is processed
+   * and made available for output immediately. Some filters require all
+   * instances be read before producing output.
+   *
+   * @param instance the input instance
+   * @return true if the filtered instance may now be
+   * collected with output().
+   * @exception Exception if the input instance was not of the correct 
+   * format or if there was a problem with the filtering.
+   */
+  public boolean input(Instance instance) throws Exception {
+
+    if (m_InputFormat == null) {
+      throw new Exception("No input instance format defined");
+    }
+    if (m_NewBatch) {
+      resetQueue();
+      m_NewBatch = false;
+    }
+    if (instance instanceof SparseInstance) {
+      push(new Instance(instance.weight(), instance.toDoubleArray()));
+    } else {
+      push(instance);
+    }
+    return true;
+  }
+
+  /**
+   * Main method for testing this class.
+   *
+   * @param argv should contain arguments to the filter: use -h for help
+   */
+  public static void main(String [] argv) {
+    
+    try {
+      if (Utils.getFlag('b', argv)) {
+	Filter.batchFilterFile(new SparseToNonSparseFilter(), argv);
+      } else {
+	Filter.filterFile(new SparseToNonSparseFilter(), argv);
+      }
+    } catch (Exception ex) {
+      System.out.println(ex.getMessage());
+    }
+  }
+}
+
+
+
+
+
+
+
+
