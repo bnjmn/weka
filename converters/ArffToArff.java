c1dc257099433564f9ca9af02f8506f55e8d9739
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2000-10-15 22:55:01 +0000

    Initial import.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk/weka@785 e0a1b77d-ad91-4216-81b1-defd5f83fa92

---
+++ converters/ArffToArff.java
@@ -0,0 +1,191 @@
+/*
+ *    ArffToArff.java
+ *    Copyright (C) 2000 Mark Hall
+ *
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package weka.converters;
+
+import java.io.*;
+import weka.core.Instance;
+import weka.core.Instances;
+
+/**
+ * Reads a text file that is in arff format.
+ *
+ * @author Mark Hall (mhall@cs.waikato.ac.nz)
+ * @version $Revision: 1.1 $
+ * @see Converter
+ * @see Serializable
+ */
+public class ArffToArff implements Converter, Serializable {
+
+  /**
+   * Holds the determined structure (header) of the data set.
+   */
+  //@ protected depends: model_structureDetermined -> m_structure;
+  //@ protected represents: model_structureDetermined <- (m_structure != null);
+  protected Instances m_structure = null;
+
+  /**
+   * Used for reading instances incrementally.
+   */
+  private Instances m_tempHeader = null;
+
+  /**
+   * Holds the source of the data set.
+   */
+  //@ protected depends: model_sourceSupplied -> m_sourceFile;
+  //@ protected represents: model_sourceSupplied <- (m_sourceFile != null);
+  protected File m_sourceFile = null;
+
+  /**
+   * The reader for the source file.
+   */
+  private Reader m_sourceReader = null;
+
+  /**
+   * Resets the converter ready to read a new data set
+   */
+  public void reset() {
+    m_structure = null;
+  }
+
+  /**
+   * Resets the Converter object and sets the source of the data set to be 
+   * the supplied File object.
+   *
+   * @param file the source file.
+   * @exception IOException if an error occurs
+   */
+  public void setSource(File file) throws IOException {
+    reset();
+
+    if (file == null) {
+      throw new IOException("Source file object is null!");
+    }
+
+    m_sourceFile = file;
+    try {
+      m_sourceReader = new BufferedReader(new FileReader(file));
+    } catch (FileNotFoundException ex) {
+      throw new IOException("File not found");
+    }
+  }
+
+  /**
+   * Determines and returns (if possible) the structure (internally the 
+   * header) of the data set as an empty set of instances.
+   *
+   * @return the structure of the data set as an empty set of Instances
+   * @exception IOException if an error occurs
+   */
+  public Instances getStructure() throws IOException {
+    if (m_sourceReader == null) {
+      throw new IOException("No source file specified");
+    }
+
+    if (m_structure == null) {
+      try {
+	m_tempHeader = new Instances(m_sourceReader, 1);
+	m_structure = new Instances(m_tempHeader);
+      } catch (Exception ex) {
+	throw new IOException("Unable to determine structure as arff.");
+      }
+    }
+
+    return m_structure;
+  }
+
+  /**
+   * Return the full data set. If the structure hasn't yet been determined
+   * by a call to getStructure then method should do so before processing
+   * the rest of the data set.
+   *
+   * @return the structure of the data set as an empty set of Instances
+   * @exception IOException if there is no source or parsing fails
+   */
+  public Instances getDataSet() throws IOException {
+    if (m_sourceReader == null) {
+      throw new IOException("No source file specified");
+    }
+    m_sourceReader.close();
+    setSource(m_sourceFile);
+    Instances result = null;
+
+    try {
+      result = new Instances(m_sourceReader);
+      m_structure = new Instances(result, 0);
+    } catch (Exception ex) {
+      throw new IOException(ex.toString());
+    }
+
+    return result;
+  }
+
+  /**
+   * Read the data set incrementally---get the next instance in the data 
+   * set or returns null if there are no
+   * more instances to get. If the structure hasn't yet been 
+   * determined by a call to getStructure then method should do so before
+   * returning the next instance in the data set.
+   *
+   * @return the next instance in the data set as an Instance object or null
+   * if there are no more instances to be read
+   * @exception Exception if there is an error during parsing
+   */
+  public Instance getNextInstance() throws IOException {
+
+    if (m_structure == null) {
+      getStructure();
+    }
+
+    if (!m_tempHeader.readInstance(m_sourceReader)) {
+      return null;
+    }
+   
+    Instance current = m_tempHeader.instance(0);
+    m_tempHeader.delete(0);
+    return current;
+  }
+
+  /**
+   * Main method.
+   *
+   * @param args should contain the name of an input file.
+   */
+  public static void main(String [] args) {
+    if (args.length > 0) {
+      File inputfile;
+      inputfile = new File(args[0]);
+      try {
+	ArffToArff atf = new ArffToArff();
+	atf.setSource(inputfile);
+	System.out.println(atf.getStructure());
+	Instance temp;
+	do {
+	  temp = atf.getNextInstance();
+	  if (temp != null) {
+	    System.out.println(temp);
+	  }
+	} while (temp != null);
+      } catch (Exception ex) {
+	ex.printStackTrace();
+	}
+    } else {
+      System.err.println("Usage:\n\tArffToArff <file.arff>\n");
+    }
+  }
+}
---
+++ converters/Converter.java
@@ -0,0 +1,174 @@
+/*
+ *    Converter.java
+ *    Copyright (C) 2000 Mark Hall
+ *
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+package weka.converters;
+
+import java.io.*;
+import weka.core.Instances;
+import weka.core.Instance;
+
+/** 
+ * Interface to something that can read and convert a text file (of some
+ * format) to arff format (Instances)
+ *
+ * @author Mark Hall (mhall@cs.waikato.ac.nz)
+ * @version $Revision: 1.1 $
+ */
+public interface Converter {
+  
+  /*@ public model instance boolean model_structureDetermined
+    @   initially: model_structureDetermined == false;
+    @*/
+
+  /*@ public model instance boolean model_sourceSupplied
+    @   initially: model_sourceSupplied == false;
+    @*/
+
+  /**
+   * Resets the Converter object, ready to read a new data set.
+   *
+   * <pre><jml>
+   *    public_normal_behavior
+   *      modifiable: model_structureDetermined;
+   *      ensures: model_structureDetermined == false;
+   * </jml></pre>
+   */
+  public void reset();
+
+  /**
+   * Resets the Converter object and sets the source of the data set to be 
+   * the supplied File object.
+   *
+   * @param file the File
+   * @exception IOException if an error occurs
+   *
+   * <pre><jml>
+   *    public_normal_behavior
+   *      requires: file != null
+   *                && (* file exists *);
+   *      modifiable: model_sourceSupplied, model_structureDetermined;
+   *      ensures: model_sourceSupplied == true 
+   *               && model_structureDetermined == false;
+   *  also
+   *    public_exceptional_behavior
+   *      requires: file == null
+   *                || (* file does not exist *);
+   *    signals: (IOException);
+   * </jml></pre>
+   */
+  public void setSource(File file) throws IOException;
+
+  /**
+   * Determines and returns (if possible) the structure (internally the 
+   * header) of the data set as an empty set of instances.
+   *
+   * @return the structure of the data set as an empty set of Instances
+   * @exception IOException if there is no source or parsing fails
+   *
+   * <pre><jml>
+   *    public_normal_behavior
+   *      requires: model_sourceSupplied == true
+   *                && model_structureDetermined == false
+   *                && (* successful parse *);
+   *      modifiable: model_structureDetermined;
+   *      ensures: \result != null
+   *               && \result.numInstances() == 0
+   *               && model_structureDetermined == true;
+   *  also
+   *    public_exceptional_behavior
+   *      requires: model_sourceSupplied == false
+   *                || (* unsuccessful parse *);
+   *      signals: (IOException);
+   * </jml></pre>
+   */
+  public Instances getStructure() throws IOException;
+
+  /**
+   * Return the full data set. If the structure hasn't yet been determined
+   * by a call to getStructure then method should do so before processing
+   * the rest of the data set.
+   *
+   * @return the full data set as an Instances object
+   * @exception IOException if there is an error during parsing
+   *
+   * <pre><jml>
+   *    public_normal_behavior
+   *      requires: model_sourceSupplied == true
+   *                && (* successful parse *);
+   *      modifiable: model_structureDetermined;
+   *      ensures: \result != null
+   *               && \result.numInstances() >= 0
+   *               && model_structureDetermined == true;
+   *  also
+   *    public_exceptional_behavior
+   *      requires: model_sourceSupplied == false
+   *                || (* unsuccessful parse *);
+   *      signals: (IOException);
+   * </jml></pre>
+   */
+  public Instances getDataSet() throws IOException;
+
+  /**
+   * Read the data set incrementally---get the next instance in the data 
+   * set or returns null if there are no
+   * more instances to get. If the structure hasn't yet been 
+   * determined by a call to getStructure then method should do so before
+   * returning the next instance in the data set.
+   *
+   * If it is not possible to read the data set incrementally (ie. in cases
+   * where the data set structure cannot be fully established before all
+   * instances have been seen) then an exception should be thrown.
+   *
+   * @return the next instance in the data set as an Instance object or null
+   * if there are no more instances to be read
+   * @exception Exception if there is an error during parsing
+   *
+   * <pre><jml>
+   *    public_normal_behavior
+   *    {|
+   *       requires: model_sourceSupplied == true
+   *                 && (* successful parse *);
+   *       modifiable: model_structureDetermined;
+   *       ensures: model_structureDetermined == true
+   *                && \result != null;
+   *     also
+   *       requires: model_sourceSupplied == true
+   *                 && (* no further input *);
+   *       modifiable: model_structureDetermined;
+   *       ensures: model_structureDetermined == true
+   *                && \result == null;
+   *    |}
+   *  also
+   *    public_exceptional_behavior
+   *    {|
+   *       requires: model_sourceSupplied == false
+   *                 || (* unsuccessful parse *);
+   *       signals: (IOException);
+   *     also
+   *       requires: (* unable to process data set incrementally *);
+   *       signals: (IOException);
+   *    |}
+   * </jml></pre>
+   */
+  public Instance getNextInstance() throws IOException;
+}
+
+
+
+
+
