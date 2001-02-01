c93934509c6d34c9599c81ce5cacd755f6f6cee0
eibe <eibe@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2001-02-01 22:06:00 +0000

    WEKA now contains a multi-layer perceptron that can be trained by backpropagation.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk/weka@930 e0a1b77d-ad91-4216-81b1-defd5f83fa92

---
+++ classifiers/nn/LinearUnit.java
@@ -0,0 +1,93 @@
+/*
+ *    LinearUnit.java
+ *    Copyright (C) 2001 Malcolm Ware
+ */
+
+
+package weka.classifiers.nn;
+
+import java.io.*;
+
+
+
+/**
+ * This can be used by the 
+ * neuralnode to perform all it's computations (as a Linear unit).
+ *
+ * @author Malcolm Ware (mfw4@cs.waikato.ac.nz)
+ * @version $Revision: 1.1 $
+ */
+public class LinearUnit implements NeuralMethod, Serializable {
+
+  
+  /**
+   * This function calculates what the output value should be.
+   * @param node The node to calculate the value for.
+   * @return The value.
+   */
+  public double outputValue(NeuralNode node) {
+    double[] weights = node.getWeights();
+    NeuralConnection[] inputs = node.getInputs();
+    double value = weights[0];
+    for (int noa = 0; noa < node.getNumInputs(); noa++) {
+      
+      value += inputs[noa].outputValue(true) 
+	* weights[noa+1];
+    }
+     
+    return value;
+  }
+  
+  /**
+   * This function calculates what the error value should be.
+   * @param node The node to calculate the error for.
+   * @return The error.
+   */
+  public double errorValue(NeuralNode node) {
+    //then calculate the error.
+    
+    NeuralConnection[] outputs = node.getOutputs();
+    int[] oNums = node.getOutputNums();
+    double error = 0;
+ 
+    for (int noa = 0; noa < node.getNumOutputs(); noa++) {
+      error += outputs[noa].errorValue(true) 
+	* outputs[noa].weightValue(oNums[noa]);
+    }
+    return error;
+  }
+
+  /**
+   * This function will calculate what the change in weights should be
+   * and also update them.
+   * @param node The node to update the weights for.
+   * @param learn The learning rate to use.
+   * @param momentum The momentum to use.
+   */
+  public void updateWeights(NeuralNode node, double learn, double momentum) {
+
+    NeuralConnection[] inputs = node.getInputs();
+    double[] cWeights = node.getChangeInWeights();
+    double[] weights = node.getWeights();
+    
+    double learnTimesError = 0;
+    try {
+      learnTimesError = learn * node.errorValue(false);
+    } catch(Exception e) {}
+    
+    double c = learnTimesError + momentum * cWeights[0];
+    weights[0] += c;
+    cWeights[0] = c;
+      
+    int stopValue = node.getNumInputs() + 1;
+    for (int noa = 1; noa < stopValue; noa++) {
+      
+      c = learnTimesError * inputs[noa-1].outputValue(false);
+      c += momentum * cWeights[noa];
+      
+      weights[noa] += c;
+      cWeights[noa] = c; 
+    }
+  }
+    
+}
