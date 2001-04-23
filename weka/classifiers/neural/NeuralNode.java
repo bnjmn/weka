166a3c20bb91abc34e0e72ddd37ce6b0526697a6
rkirkby <rkirkby@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2001-04-23 03:11:29 +0000

    General improvements.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@999 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/classifiers/neural/NeuralNode.java
+++ weka/classifiers/neural/NeuralNode.java
@@ -27,7 +27,7 @@ import java.util.Random;
 /**
  * This class is used to represent a node in the neuralnet.
  * @author Malcolm Ware (mfw4@cs.waikato.ac.nz)
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public class NeuralNode extends NeuralConnection {
     
@@ -69,6 +69,10 @@ public class NeuralNode extends NeuralConnection {
   public void setMethod(NeuralMethod m) {
     m_methods = m;
   } 
+
+  public NeuralMethod getMethod() {
+    return m_methods;
+  }
 
   /**
    * Call this to get the output value of this unit. 
