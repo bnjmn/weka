0aa1cd93711324985f83a5892f70510c67f5f4a6
rkirkby <rkirkby@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2001-11-09 03:20:12 +0000

    Added ability to set and retrieve meta data.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@1106 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/core/Attribute.java
+++ weka/core/Attribute.java
@@ -28,6 +28,10 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.Properties;
+import java.io.StreamTokenizer;
+import java.io.StringReader;
+import java.io.IOException;
 
 /** 
  * Class for handling an attribute. Once an attribute has been created,
@@ -70,7 +74,7 @@ import java.util.Hashtable;
  * </code><p>
  *
  * @author Eibe Frank (eibe@cs.waikato.ac.nz)
- * @version $Revision: 1.25 $
+ * @version $Revision: 1.26 $
  */
 public class Attribute implements Copyable, Serializable {
 
@@ -85,6 +89,15 @@ public class Attribute implements Copyable, Serializable {
 
   /** Constant set for attributes with date values. */
   public final static int DATE = 3;
+
+  /** Constant set for symbolic attributes. */
+  public final static int ORDERING_SYMBOLIC = 0;
+
+  /** Constant set for ordered attributes. */
+  public final static int ORDERING_ORDERED  = 1;
+
+  /** Constant set for modulo-ordered attributes. */
+  public final static int ORDERING_MODULO   = 2;
 
   /** Strings longer than this will be stored compressed. */
   private final static int STRING_COMPRESS_THRESHOLD = 200;
@@ -107,6 +120,36 @@ public class Attribute implements Copyable, Serializable {
   /** The attribute's index. */
   private int m_Index;
 
+  /** The attribute's metadata. */
+  private ProtectedProperties m_Metadata;
+
+  /** The attribute's ordering. */
+  private int m_Ordering;
+
+  /** Whether the attribute is regular. */
+  private boolean m_IsRegular;
+
+  /** Whether the attribute is averagable. */
+  private boolean m_IsAveragable;
+
+  /** Whether the attribute has a zeropoint. */
+  private boolean m_HasZeropoint;
+
+  /** The attribute's weight. */
+  private double m_Weight;
+
+  /** The attribute's lower numeric bound. */
+  private double m_LowerBound;
+
+  /** Whether the lower bound is open. */
+  private boolean m_LowerBoundIsOpen;
+
+  /** The attribute's upper numeric bound. */
+  private double m_UpperBound;
+
+  /** Whether the upper bound is open */
+  private boolean m_UpperBoundIsOpen;
+
   /**
    * Constructor for a numeric attribute.
    *
@@ -114,11 +157,23 @@ public class Attribute implements Copyable, Serializable {
    */
   public Attribute(String attributeName) {
 
+    this(attributeName, new ProtectedProperties(new Properties()));
+  }
+
+  /**
+   * Constructor for a numeric attribute, where metadata is supplied.
+   *
+   * @param attributeName the name for the attribute
+   * @param metadata the attribute's properties
+   */
+  public Attribute(String attributeName, ProtectedProperties metadata) {
+
     m_Name = attributeName;
     m_Index = -1;
     m_Values = null;
     m_Hashtable = null;
     m_Type = NUMERIC;
+    setMetadata(metadata);
   }
 
   /**
@@ -129,6 +184,21 @@ public class Attribute implements Copyable, Serializable {
    * SimpleDateFormatter for parsing dates.
    */
   public Attribute(String attributeName, String dateFormat) {
+
+    this(attributeName, dateFormat,
+	 new ProtectedProperties(new Properties()));
+  }
+
+  /**
+   * Constructor for a date attribute, where metadata is supplied.
+   *
+   * @param attributeName the name for the attribute
+   * @param dateFormat a string suitable for use with
+   * SimpleDateFormatter for parsing dates.
+   * @param metadata the attribute's properties
+   */
+  public Attribute(String attributeName, String dateFormat,
+		   ProtectedProperties metadata) {
 
     m_Name = attributeName;
     m_Index = -1;
@@ -141,6 +211,7 @@ public class Attribute implements Copyable, Serializable {
       m_DateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
     }
     m_DateFormat.setLenient(false);
+    setMetadata(metadata);
   }
 
   /**
@@ -154,6 +225,24 @@ public class Attribute implements Copyable, Serializable {
    */
   public Attribute(String attributeName, 
 		   FastVector attributeValues) {
+
+    this(attributeName, attributeValues,
+	 new ProtectedProperties(new Properties()));
+  }
+
+  /**
+   * Constructor for nominal attributes and string attributes, where
+   * metadata is supplied. If a null vector of attribute values is passed
+   * to the method, the attribute is assumed to be a string.
+   *
+   * @param attributeName the name for the attribute
+   * @param attributeValues a vector of strings denoting the 
+   * attribute values. Null if the attribute is a string attribute.
+   * @param metadata the attribute's properties
+   */
+  public Attribute(String attributeName, 
+		   FastVector attributeValues,
+		   ProtectedProperties metadata) {
 
     m_Name = attributeName;
     m_Index = -1;
@@ -179,6 +268,7 @@ public class Attribute implements Copyable, Serializable {
       }
       m_Type = NOMINAL;
     }
+    setMetadata(metadata);
   }
 
   /**
@@ -195,6 +285,7 @@ public class Attribute implements Copyable, Serializable {
     copy.m_Values = m_Values;
     copy.m_Hashtable = m_Hashtable;
     copy.m_DateFormat = m_DateFormat;
+    copy.setMetadata(m_Metadata);
  
     return copy;
   }
@@ -564,6 +655,7 @@ public class Attribute implements Copyable, Serializable {
     copy.m_Type = m_Type;
     copy.m_Values = m_Values;
     copy.m_Hashtable = m_Hashtable;
+    copy.setMetadata(m_Metadata);
  
     return copy;
   }
@@ -684,6 +776,348 @@ public class Attribute implements Copyable, Serializable {
       throw new IllegalArgumentException("Can only parse date values for date"
                                          + " attributes!");
     }
+  }
+
+  /**
+   * Returns the properties supplied for this attribute.
+   *
+   * @return metadata for this attribute
+   */  
+  public final ProtectedProperties getMetadata() {
+
+    return m_Metadata;
+  }
+
+  /**
+   * Returns the ordering of the attribute. One of the following:
+   * 
+   * ORDERING_SYMBOLIC - attribute values should be treated as symbols.
+   * ORDERING_ORDERED  - attribute values have a global ordering.
+   * ORDERING_MODULO   - attribute values have an ordering which wraps.
+   *
+   * @return the ordering type of the attribute
+   */
+  public final int ordering() {
+
+    return m_Ordering;
+  }
+
+  /**
+   * Returns whether the attribute values are equally spaced.
+   *
+   * @return whether the attribute is regular or not
+   */
+  public final boolean isRegular() {
+
+    return m_IsRegular;
+  }
+
+  /**
+   * Returns whether the attribute can be averaged meaningfully.
+   *
+   * @return whether the attribute can be averaged or not
+   */
+  public final boolean isAveragable() {
+
+    return m_IsAveragable;
+  }
+
+  /**
+   * Returns whether the attribute has a zeropoint and may be
+   * added meaningfully.
+   *
+   * @return whether the attribute has a zeropoint or not
+   */
+  public final boolean hasZeropoint() {
+
+    return m_HasZeropoint;
+  }
+
+  /**
+   * Returns the attribute's weight.
+   *
+   * @return the attribute's weight as a double
+   */
+  public final double weight() {
+
+    return m_Weight;
+  }
+
+  /**
+   * Returns the lower bound of a numeric attribute.
+   *
+   * @return the lower bound of the specified numeric range
+   */
+  public final double getLowerNumericBound() {
+
+    return m_LowerBound;
+  }
+
+  /**
+   * Returns whether the lower numeric bound of the attribute is open.
+   *
+   * @return whether the lower numeric bound is open or not (closed)
+   */
+  public final boolean lowerNumericBoundIsOpen() {
+
+    return m_LowerBoundIsOpen;
+  }
+
+  /**
+   * Returns the upper bound of a numeric attribute.
+   *
+   * @return the upper bound of the specified numeric range
+   */
+  public final double getUpperNumericBound() {
+
+    return m_UpperBound;
+  }
+
+  /**
+   * Returns whether the upper numeric bound of the attribute is open.
+   *
+   * @return whether the upper numeric bound is open or not (closed)
+   */
+  public final boolean upperNumericBoundIsOpen() {
+
+    return m_UpperBoundIsOpen;
+  }
+
+  /**
+   * Determines whether a value lies within the bounds of the attribute.
+   *
+   * @return whether the value is in range
+   */
+  public final boolean isInRange(double value) {
+
+    // dates and missing values are a special case 
+    if (m_Type == DATE || value == Instance.missingValue()) return true;
+    if (m_Type != NUMERIC) {
+      // do label range check
+      int intVal = (int) value;
+      if (intVal < 0 || intVal >= m_Hashtable.size()) return false;
+    } else {
+      // do numeric bounds check
+      if (m_LowerBoundIsOpen) {
+	if (value <= m_LowerBound) return false;
+      } else {
+	if (value < m_LowerBound) return false;
+      }
+      if (m_UpperBoundIsOpen) {
+	if (value >= m_UpperBound) return false;
+      } else {
+	if (value > m_UpperBound) return false;
+      }
+    }
+    return true;
+  }
+
+  /**
+   * Sets the metadata for the attribute. Processes the strings stored in the
+   * metadata of the attribute so that the properties can be set up for the
+   * easy-access metadata methods. Any strings sought that are omitted will
+   * cause default values to be set.
+   * 
+   * The following properties are recognised:
+   * ordering, averageable, zeropoint, regular, weight, and range.
+   *
+   * All other properties can be queried and handled appropriately by classes
+   * calling the getMetadata() method.
+   *
+   * @param metadata the metadata
+   * @exception IllegalArgumentException if the properties are not consistent
+   */
+  private void setMetadata(ProtectedProperties metadata) {
+    
+    m_Metadata = metadata;
+
+    if (m_Type == DATE) {
+      m_Ordering = ORDERING_ORDERED;
+      m_IsRegular = true;
+      m_IsAveragable = false;
+      m_HasZeropoint = false;
+    } else {
+
+      // get ordering
+      String orderString = m_Metadata.getProperty("ordering","");
+      
+      // numeric ordered attributes are averagable and zeropoint by default
+      String def;
+      if (m_Type == NUMERIC
+	  && orderString.compareTo("modulo") != 0
+	  && orderString.compareTo("symbolic") != 0)
+	def = "true";
+      else def = "false";
+      
+      // determine boolean states
+      m_IsAveragable =
+	(m_Metadata.getProperty("averageable",def).compareTo("true") == 0);
+      m_HasZeropoint =
+	(m_Metadata.getProperty("zeropoint",def).compareTo("true") == 0);
+      // averagable or zeropoint implies regular
+      if (m_IsAveragable || m_HasZeropoint) def = "true";
+      m_IsRegular =
+	(m_Metadata.getProperty("regular",def).compareTo("true") == 0);
+      
+      // determine ordering
+      if (orderString.compareTo("symbolic") == 0)
+	m_Ordering = ORDERING_SYMBOLIC;
+      else if (orderString.compareTo("ordered") == 0)
+	m_Ordering = ORDERING_ORDERED;
+      else if (orderString.compareTo("modulo") == 0)
+	m_Ordering = ORDERING_MODULO;
+      else {
+	if (m_Type == NUMERIC || m_IsAveragable || m_HasZeropoint)
+	  m_Ordering = ORDERING_ORDERED;
+	else m_Ordering = ORDERING_SYMBOLIC;
+      }
+    }
+
+    // consistency checks
+    if (m_IsAveragable && !m_IsRegular)
+      throw new IllegalArgumentException("An averagable attribute must be"
+					 + " regular");
+    if (m_HasZeropoint && !m_IsRegular)
+      throw new IllegalArgumentException("A zeropoint attribute must be"
+					 + " regular");
+    if (m_IsRegular && m_Ordering == ORDERING_SYMBOLIC)
+      throw new IllegalArgumentException("A symbolic attribute cannot be"
+					 + " regular");
+    if (m_IsAveragable && m_Ordering != ORDERING_ORDERED)
+      throw new IllegalArgumentException("An averagable attribute must be"
+					 + " ordered");
+    if (m_HasZeropoint && m_Ordering != ORDERING_ORDERED)
+      throw new IllegalArgumentException("A zeropoint attribute must be"
+					 + " ordered");
+
+    // determine weight
+    m_Weight = 1.0;
+    String weightString = m_Metadata.getProperty("weight");
+    if (weightString != null) {
+      try{
+	m_Weight = Double.valueOf(weightString).doubleValue();
+      } catch (NumberFormatException e) {
+	// Check if value is really a number
+	throw new IllegalArgumentException("Not a valid attribute weight: '" 
+					   + weightString + "'");
+      }
+    }
+
+    // determine numeric range
+    if (m_Type == NUMERIC) setNumericRange(m_Metadata.getProperty("range"));
+  }
+
+  /**
+   * Sets the numeric range based on a string. If the string is null the range
+   * will default to [-inf,+inf]. A square brace represents a closed interval, a
+   * curved brace represents an open interval, and 'inf' represents infinity.
+   * Examples of valid range strings: "[-inf,20)","(-13.5,-5.2)","(5,inf]"
+   *
+   * @param rangeString the string to parse as the attribute's numeric range
+   * @exception IllegalArgumentException if the range is not valid
+   */
+  private void setNumericRange(String rangeString)
+  {
+    // set defaults
+    m_LowerBound = Double.NEGATIVE_INFINITY;
+    m_LowerBoundIsOpen = false;
+    m_UpperBound = Double.POSITIVE_INFINITY;
+    m_UpperBoundIsOpen = false;
+
+    if (rangeString == null) return;
+
+    // set up a tokenzier to parse the string
+    StreamTokenizer tokenizer =
+      new StreamTokenizer(new StringReader(rangeString));
+    tokenizer.resetSyntax();         
+    tokenizer.whitespaceChars(0, ' ');    
+    tokenizer.wordChars(' '+1,'\u00FF');
+    tokenizer.ordinaryChar('[');
+    tokenizer.ordinaryChar('(');
+    tokenizer.ordinaryChar(',');
+    tokenizer.ordinaryChar(']');
+    tokenizer.ordinaryChar(')');
+
+    try {
+
+      // get opening brace
+      tokenizer.nextToken();
+    
+      if (tokenizer.ttype == '[') m_LowerBoundIsOpen = false;
+      else if (tokenizer.ttype == '(') m_LowerBoundIsOpen = true;
+      else throw new IllegalArgumentException("Expected opening brace on range,"
+					      + " found: "
+					      + tokenizer.toString());
+
+      // get lower bound
+      tokenizer.nextToken();
+      if (tokenizer.ttype != tokenizer.TT_WORD)
+	throw new IllegalArgumentException("Expected lower bound in range,"
+					   + " found: "
+					   + tokenizer.toString());
+      if (tokenizer.sval.compareToIgnoreCase("-inf") == 0)
+	m_LowerBound = Double.NEGATIVE_INFINITY;
+      else if (tokenizer.sval.compareToIgnoreCase("+inf") == 0)
+	m_LowerBound = Double.POSITIVE_INFINITY;
+      else if (tokenizer.sval.compareToIgnoreCase("inf") == 0)
+	m_LowerBound = Double.NEGATIVE_INFINITY;
+      else try {
+	m_LowerBound = Double.valueOf(tokenizer.sval).doubleValue();
+      } catch (NumberFormatException e) {
+	throw new IllegalArgumentException("Expected lower bound in range,"
+					   + " found: '" + tokenizer.sval + "'");
+      }
+
+      // get separating comma
+      if (tokenizer.nextToken() != ',')
+	throw new IllegalArgumentException("Expected comma in range,"
+					   + " found: "
+					   + tokenizer.toString());
+
+      // get upper bound
+      tokenizer.nextToken();
+      if (tokenizer.ttype != tokenizer.TT_WORD)
+	throw new IllegalArgumentException("Expected upper bound in range,"
+					   + " found: "
+					   + tokenizer.toString());
+      if (tokenizer.sval.compareToIgnoreCase("-inf") == 0)
+	m_UpperBound = Double.NEGATIVE_INFINITY;
+      else if (tokenizer.sval.compareToIgnoreCase("+inf") == 0)
+	m_UpperBound = Double.POSITIVE_INFINITY;
+      else if (tokenizer.sval.compareToIgnoreCase("inf") == 0)
+	m_UpperBound = Double.POSITIVE_INFINITY;
+      else try {
+	m_UpperBound = Double.valueOf(tokenizer.sval).doubleValue();
+      } catch (NumberFormatException e) {
+	throw new IllegalArgumentException("Expected upper bound in range,"
+					   + " found: '" + tokenizer.sval + "'");
+      }
+
+      // get closing brace
+      tokenizer.nextToken();
+    
+      if (tokenizer.ttype == ']') m_UpperBoundIsOpen = false;
+      else if (tokenizer.ttype == ')') m_UpperBoundIsOpen = true;
+      else throw new IllegalArgumentException("Expected closing brace on range,"
+					      + " found: "
+					      + tokenizer.toString());
+
+      // check for rubbish on end
+      if (tokenizer.nextToken() != tokenizer.TT_EOF)
+	throw new IllegalArgumentException("Expected end of range string,"
+					   + " found: "
+					   + tokenizer.toString());
+
+    } catch (IOException e) {
+      throw new IllegalArgumentException("IOException reading attribute range"
+					 + " string: " + e.getMessage());
+    }
+
+    if (m_UpperBound < m_LowerBound)
+      throw new IllegalArgumentException("Upper bound (" + m_UpperBound
+					 + ") on numeric range is"
+					 + " less than lower bound ("
+					 + m_LowerBound + ")!");
   }
 
   /**
