62a825dbc96cbb3578c90a7b43b1e200f6e49368
cvs_rbk1 <cvs_rbk1@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2002-01-28 21:12:24 +0000

    Has been rewritten.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk/weka@1169 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- core/SerializedObject.java
+++ core/SerializedObject.java
@@ -16,234 +16,86 @@
 
 /*
  *    SerializedObject.java
- *    Copyright (C) 2000 Webmind Inc.
+ *    Copyright (C) 2001 Richard Kirkby
  *
  */
 
 package weka.core;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
-import java.io.Serializable;
-import java.util.zip.GZIPInputStream;
-import java.util.zip.GZIPOutputStream;
+import java.io.*;
+import java.util.zip.*;
 
 /**
- * This class stores an object serialized in memory. It allows compression,
- * to be used to conserve memory (for example, when storing large strings
- * in memory), or can be used as a mechanism for deep copying objects.
+ * Class for storing an object in serialized form in memory. It can be used 
+ * to make deep copies of objects, and also allows compression to conserve
+ * memory. <p>
  *
- * @author Len Trigg (len@intelligenesis.net)
- * @version $Revision: 1.4 $
+ * @author Richard Kirkby (rbk1@cs.waikato.ac.nz)
+ * @version $Revision: 1.5 $ 
  */
 public class SerializedObject implements Serializable {
 
-  /** Stores the serialized object */
-  private byte [] m_Serialized;
+  /** The array storing the object. */
+  private byte[] m_storedObjectArray;
 
-  /** True if the object has been compressed during storage */
-  private boolean m_Compressed;
+  /** Whether or not the object is compressed. */
+  private boolean m_isCompressed;
 
   /**
-   * Serializes the supplied object into a byte array without compression.
+   * Creates a new serialized object (without compression).
    *
-   * @param obj the Object to serialize.
-   * @exception Exception if the object is not Serializable.
-   */
-  public SerializedObject(Object obj) throws Exception {
+   * @param toStore the object to store
+   * @param compress whether or not to use compression
+   * @exception Exception if the object couldn't be serialized
+   */ 
+  public SerializedObject(Object toStore) throws Exception {
 
-    this(obj, false);
+    this(toStore, false);
   }
 
   /**
-   * Serializes the supplied object into a byte array.
+   * Creates a new serialized object.
    *
-   * @param obj the Object to serialize.
-   * @param compress true if the object should be stored compressed.
-   * @exception Exception if the object is not Serializable.
-   */
-  public SerializedObject(Object obj, boolean compress) throws Exception {
+   * @param toStore the object to store
+   * @param compress whether or not to use compression
+   * @exception Exception if the object couldn't be serialized
+   */ 
+  public SerializedObject(Object toStore, boolean compress) throws Exception {
 
-    //System.err.print("."); System.err.flush();
-    m_Compressed = compress;
-    m_Serialized = toByteArray(obj, m_Compressed);
+    ByteArrayOutputStream ostream = new ByteArrayOutputStream();
+    ObjectOutputStream p;
+    if (!compress)
+      p = new ObjectOutputStream(new BufferedOutputStream(ostream));
+    else
+      p = new ObjectOutputStream(new GZIPOutputStream(new BufferedOutputStream(ostream)));
+    p.writeObject(toStore);
+    p.flush();
+    ostream.close();
+    m_storedObjectArray = ostream.toByteArray();
+
+    m_isCompressed = compress;
   }
 
   /**
-   * Serializes the supplied object to a byte array.
+   * Returns a serialized object.
    *
-   * @param obj the Object to serialize
-   * @param compress true if the object should be compressed.
-   * @return the byte array containing the serialized object.
-   * @exception Exception if the object is not Serializable.
-   */
-  protected static byte [] toByteArray(Object obj, boolean compress) 
-    throws Exception {
-
-    ByteArrayOutputStream bo = new ByteArrayOutputStream();
-    OutputStream os = bo;
-    if (compress) {
-      os = new GZIPOutputStream(os);
-    }
-    os = new BufferedOutputStream(os);
-    ObjectOutputStream oo = new ObjectOutputStream(os);
-    oo.writeObject(obj);
-    oo.close();
-    return bo.toByteArray();
-  }
-
-  /**
-   * Gets the object stored in this SerializedObject. The object returned
-   * will be a deep copy of the original stored object.
-   *
-   * @return the deserialized Object.
-   */
+   * @return the restored object
+   * @exception Exception if the object couldn't be restored
+   */ 
   public Object getObject() {
-    try {
-      InputStream is = new ByteArrayInputStream(m_Serialized);
-      if (m_Compressed) {
-        is = new GZIPInputStream(is);
-      }
-      is = new BufferedInputStream(is);
-      ObjectInputStream oi = new ObjectInputStream(is);
-      Object result = oi.readObject();
-      oi.close();
-      return result;
-    } catch (Exception ex) {
-      ex.printStackTrace();
-    }
-    return null;
-  }
-
-  /**
-   * Compares this object with another for equality.
-   *
-   * @param other the other Object.
-   * @return true if the objects are equal.
-   */
-  public final boolean equals(Object other) {
-
-    // Check class type
-    if ((other == null) || !(other.getClass().equals(this.getClass()))) {
-      return false;
-    }
-    // Check serialized length
-    byte [] os = ((SerializedObject)other).m_Serialized;
-    if (os.length != m_Serialized.length) {
-      return false;
-    }
-    // Check serialized contents
-    for (int i = 0; i < m_Serialized.length; i++) {
-      if (m_Serialized[i] != os[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  /**
-   * Returns a hashcode for this object.
-   *
-   * @return the hashcode for this object.
-   */
-  public final int hashCode() {
-    
-    return m_Serialized.length;
-  }
-
-  /**
-   * Returns a text representation of the state of this object.
-   *
-   * @return a String representing this object.
-   */
-  public String toString() {
-    
-    return (m_Compressed ? "Compressed object: " : "Uncompressed object: ")
-      + m_Serialized.length + " bytes";
-  }
-
-  /**
-   * Test routine, reads text from stdin and measures memory usage
-   */
-  public static void main2(String []args) {
 
     try {
-      Runtime r = Runtime.getRuntime();
-      r.gc();
-      java.io.LineNumberReader lnr = new java.io.LineNumberReader(new java.io.InputStreamReader(System.in));
-      StringBuffer sb = new StringBuffer();
-      String line;
-      while ((line = lnr.readLine()) != null) {
-        sb.append(line).append('\n');
-      }
-      String text = sb.toString();
-      //System.err.println("TEXT:");
-      //System.err.println(text);
-      r.gc();
-
-      System.err.print("TOTAL: " + r.totalMemory());
-      System.err.println("\tFREE: " + r.freeMemory());
-      long used = r.totalMemory() - r.freeMemory();
-      
-      // convert to a compressedobject
-      SerializedObject co = new SerializedObject(text, true);
-
-      System.err.print("TOTAL: " + r.totalMemory());
-      System.err.println("\tFREE: " + r.freeMemory());
-      r.gc();
-      long used1 = r.totalMemory() - r.freeMemory();
-      long csize = used1 - used;
-      System.err.println("CompressedSize = " + csize);
-
-      String newstr = (String)co.getObject();
-      r.gc();
-      System.err.print("TOTAL: " + r.totalMemory());
-      System.err.println("\tFREE: " + r.freeMemory());
-      long used2 = r.totalMemory() - r.freeMemory();
-      long usize = used2 - used1;
-      System.err.println("UncompressedSize = " + usize);
-
-      // A couple of references to the original objects
-      // so they don't get freed prematurely and muck up the
-      // measurements.
-      newstr = newstr.toLowerCase();
-      text = text.toLowerCase();
-      System.err.println("Byte array: " + co.toString());
-      System.err.println("Length of text: " + text.length());
-    } catch (Exception ex) {
-      ex.printStackTrace();
-    }
-  }
-
-
-  /**
-   * Test routine, reads an arff file from stdin and measures memory usage
-   * (the arff file should have long string attribute values)
-   */
-  public static void main(String []args) {
-
-    try {
-      Runtime r = Runtime.getRuntime();
-      r.gc();
-      System.err.print("TOTAL: " + r.totalMemory());
-      System.err.println("\tFREE: " + r.freeMemory());
-      long used = r.totalMemory() - r.freeMemory();
-      Instances inst = new Instances(new java.io.BufferedReader(new java.io.InputStreamReader(System.in)));
-      r.gc();
-      long used1 = r.totalMemory() - r.freeMemory();
-      long csize = used1 - used;
-      System.err.print("\nTOTAL: " + r.totalMemory());
-      System.err.println("\tFREE: " + r.freeMemory());
-      System.err.println("size = " + csize);
-      int blah = inst.numAttributes();
-    } catch (Exception ex) {
-      ex.printStackTrace();
+      ByteArrayInputStream istream = new ByteArrayInputStream(m_storedObjectArray);
+      ObjectInputStream p;
+      if (!m_isCompressed)
+	p = new ObjectInputStream(new BufferedInputStream(istream));
+      else 
+	p = new ObjectInputStream(new GZIPInputStream(new BufferedInputStream(istream)));
+      Object toReturn = p.readObject();
+      istream.close();
+      return toReturn;
+    } catch (Exception e) {
+      return null;
     }
   }
 }
