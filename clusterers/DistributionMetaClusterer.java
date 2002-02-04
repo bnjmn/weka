98b1c224a9004a2f400b3fbb3d04649f9de77719
cvs_rbk1 <cvs_rbk1@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2002-02-04 21:36:45 +0000

    Has been rewritten.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk/weka@1183 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- clusterers/DistributionMetaClusterer.java
+++ clusterers/DistributionMetaClusterer.java
@@ -16,155 +16,187 @@
 
 /*
  *    DistributionMetaClusterer.java
- *    Copyright (C) 2000 Intelligenesis Corp.
+ *    Copyright (C) 2002 Richard Kirkby
  *
  */
 
 package weka.clusterers;
 
+import weka.core.*;
 import java.util.Enumeration;
-import java.util.Random;
 import java.util.Vector;
-import weka.core.Attribute;
-import weka.core.Instance;
-import weka.core.Instances;
-import weka.core.Option;
-import weka.core.OptionHandler;
-import weka.core.Utils;
 
 /**
- * Class that wraps up a Clusterer and presents it as a DistributionClusterer
- * for ease of programmatically handling Clusterers in general -- only the
- * one predict method (distributionForInstance) need be worried about. The
- * distributions produced by this clusterer place a probability of 1 on the
- * class value predicted by the sub-clusterer.<p>
+ * Class for wrapping a Clusterer to make it return a distribution. Simply
+ * outputs a probabiltry of 1 for the chosen cluster and 0 for the others.
  *
- * Valid options are:<p>
- *
- * -W classname <br>
- * Specify the full class name of a sub-clusterer (required).<p>
- *
- * @author Richard Littin (richard@intelligenesis.net)
- * @version $Revision: 1.5 $
+ * @author Richard Kirkby (rkirkby@cs.waikato.ac.nz)
+ * @version $Revision: 1.6 $
  */
-public class DistributionMetaClusterer extends DistributionClusterer
+public class DistributionMetaClusterer extends DistributionClusterer 
   implements OptionHandler {
 
-  /** The clusterer. */
-  private Clusterer m_Clusterer = new weka.clusterers.EM();
+  /** The clusterer being wrapped */
+  private Clusterer m_wrappedClusterer = new weka.clusterers.EM();
 
   /**
-   * Builds the clusterer.
-   *
-   * @param insts the training data.
-   * @exception Exception if a clusterer can't be built
-   */
-  public void buildClusterer(Instances insts) throws Exception {
+   * Default constructor.
+   * 
+   */  
+  public DistributionMetaClusterer() {
 
-    if (m_Clusterer == null) {
-      throw new Exception("No base clusterer has been set!");
+  }
+   
+  /**
+   * Contructs a DistributionMetaClusterer wrapping a given Clusterer.
+   * 
+   * @param toWrap the clusterer to wrap around
+   */    
+  public DistributionMetaClusterer(Clusterer toWrap) {
+
+    setClusterer(toWrap);
+  }
+  
+  /**
+   * Builds a clusterer for a set of instances.
+   *
+   * @param instances the instances to train the clusterer with
+   * @exception Exception if the clusterer hasn't been set or something goes wrong
+   */  
+  public void buildClusterer(Instances data) throws Exception {
+
+    if (m_wrappedClusterer == null) {
+      throw new Exception("No clusterer has been set");
     }
-    m_Clusterer.buildClusterer(insts);
+    m_wrappedClusterer.buildClusterer(data);
+  }
+  
+  /**
+   * Computes the density for a given instance.
+   * 
+   * @param instance the instance to compute the density for
+   * @return the density.
+   * @exception Exception if the density could not be computed successfully
+   */
+  public double densityForInstance(Instance instance) throws Exception {
+
+    return Utils.sum(distributionForInstance(instance));
   }
 
   /**
-   * Returns the distribution for an instance.
+   * Returns the cluster probability distribution for an instance. Will simply have a
+   * probability of 1 for the chosen cluster and 0 for the others.
    *
-   * @exception Exception if the distribution can't be computed successfully
-   */
-  public double[] distributionForInstance(Instance inst) throws Exception {
+   * @param instance the instance to be clustered
+   * @return the probability distribution
+   */  
+  public double[] distributionForInstance(Instance instance) throws Exception {
     
-    double[] result = new double[m_Clusterer.numberOfClusters()];
-    int predictedCluster = m_Clusterer.clusterInstance(inst);
-    result[predictedCluster] = 1.0;
-    return result;
+    double[] distribution = new double[m_wrappedClusterer.numberOfClusters()];
+    distribution[m_wrappedClusterer.clusterInstance(instance)] = 1.0;
+    return distribution;
   }
-
-  /**
-   * Returns the density for an instance.
-   *
-   * @exception Exception if the distribution can't be computed successfully
-   */
-  public double densityForInstance(Instance inst) throws Exception {
-    return Utils.sum(distributionForInstance(inst));
-  }
-
+  
   /**
    * Returns the number of clusters.
    *
    * @return the number of clusters generated for a training dataset.
-   * @exception Exception if number of clusters could not be returned
-   * successfully
+   * @exception Exception if number of clusters could not be returned successfully
    */
   public int numberOfClusters() throws Exception {
-    return  m_Clusterer.numberOfClusters();
+
+    return m_wrappedClusterer.numberOfClusters();
   }
 
   /**
-   * Prints the clusterers.
+   * Returns a description of the clusterer.
+   *
+   * @return a string containing a description of the clusterer
    */
   public String toString() {
-    return "DistributionMetaClusterer: " + m_Clusterer.toString() + "\n";
+
+    return "DistributionMetaClusterer: " + m_wrappedClusterer.toString();
   }
 
   /**
-   * Returns an enumeration describing the available options.
+   * Sets the clusterer to wrap.
+   *
+   * @param toWrap the clusterer
+   */
+  public void setClusterer(Clusterer toWrap) {
+
+    m_wrappedClusterer = toWrap;
+  }
+
+  /**
+   * Gets the clusterer being wrapped.
+   *
+   * @return the clusterer
+   */
+  public Clusterer getClusterer() {
+
+    return m_wrappedClusterer;
+  }
+
+  /**
+   * Returns an enumeration describing the available options..
    *
    * @return an enumeration of all the available options.
    */
   public Enumeration listOptions() {
-
-    Vector vec = new Vector(1);
-    vec.addElement(new Option("\tSets the base clusterer.",
-			      "W", 1, "-W <base clusterer>"));
     
-    if (m_Clusterer != null) {
-      try {
-	vec.addElement(new Option("",
-				  "", 0, "\nOptions specific to clusterer "
-				  + m_Clusterer.getClass().getName() + ":"));
-	Enumeration enum = ((OptionHandler)m_Clusterer).listOptions();
-	while (enum.hasMoreElements()) {
-	  vec.addElement(enum.nextElement());
-	}
-      } catch (Exception e) {
+    Vector newVector = new Vector(1);
+    newVector.addElement(new Option(
+				    "\tClusterer to wrap. (required)\n",
+				    "W", 1,"-W <clusterer name>"));
+
+    if ((m_wrappedClusterer != null) &&
+	(m_wrappedClusterer instanceof OptionHandler)) {
+      newVector.addElement(new Option(
+				      "",
+				      "", 0, "\nOptions specific to clusterer "
+				      + m_wrappedClusterer.getClass().getName() + ":"));
+      Enumeration enum = ((OptionHandler)m_wrappedClusterer).listOptions();
+      while (enum.hasMoreElements()) {
+	newVector.addElement(enum.nextElement());
       }
     }
-    return vec.elements();
+    
+    return newVector.elements();
   }
 
   /**
    * Parses a given list of options. Valid options are:<p>
    *
-   * -W classname <br>
-   * Specify the full class name of a learner as the basis for 
-   * the multiclassclusterer (required).<p>
+   * -W clusterer name <br>
+   * Clusterer to wrap. (required) <p>
    *
    * @param options the list of options as an array of strings
    * @exception Exception if an option is not supported
    */
   public void setOptions(String[] options) throws Exception {
-  
-    String clustererName = Utils.getOption('W', options);
-    if (clustererName.length() == 0) {
-      throw new Exception("A clusterer must be specified with"
-			  + " the -W option.");
+    
+    String wString = Utils.getOption('W', options);
+    if (wString.length() != 0) {
+      setClusterer(Clusterer.forName(wString,
+				       Utils.partitionOptions(options)));
+
+    } else {
+      throw new Exception("A clusterer must be specified with the -W option.");
     }
-    setClusterer(Clusterer.forName(clustererName,
-				     Utils.partitionOptions(options)));
   }
 
   /**
-   * Gets the current settings of the Clusterer.
+   * Gets the current settings of the clusterer.
    *
-   * @return an array of strings suitable for passing to setOptions
+   * @return an array of strings suitable for passing to setOptions()
    */
-  public String [] getOptions() {
-    
+  public String[] getOptions() {
+
     String [] clustererOptions = new String [0];
-    if ((m_Clusterer != null) &&
-	(m_Clusterer instanceof OptionHandler)) {
-      clustererOptions = ((OptionHandler)m_Clusterer).getOptions();
+    if ((m_wrappedClusterer != null) &&
+	(m_wrappedClusterer instanceof OptionHandler)) {
+      clustererOptions = ((OptionHandler)m_wrappedClusterer).getOptions();
     }
     String [] options = new String [clustererOptions.length + 3];
     int current = 0;
@@ -185,35 +217,16 @@ public class DistributionMetaClusterer extends DistributionClusterer
   }
 
   /**
-   * Set the base clusterer. 
-   *
-   * @param newClusterer the Clusterer to use.
-   */
-  public void setClusterer(Clusterer newClusterer) {
-
-    m_Clusterer = newClusterer;
-  }
-
-  /**
-   * Get the clusterer used as the clusterer
-   *
-   * @return the clusterer used as the clusterer
-   */
-  public Clusterer getClusterer() {
-
-    return m_Clusterer;
-  }
-
-  /**
    * Main method for testing this class.
    *
    * @param argv the options
    */
   public static void main(String [] argv) {
-
+    
     try {
-      DistributionClusterer scheme = new DistributionMetaClusterer();
-      System.out.println(ClusterEvaluation.evaluateClusterer(scheme, argv));
+      System.out.println(ClusterEvaluation.
+			 evaluateClusterer(new DistributionMetaClusterer(), 
+					   argv));
     } catch (Exception e) {
       System.err.println(e.getMessage());
     }
