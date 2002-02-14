dbdb29c6c826a0425e990ab63aa29758f6e7fd02
cvs_mhall <cvs_mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2002-02-14 23:40:23 +0000

    Initial import
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@1209 e0a1b77d-ad91-4216-81b1-defd5f83fa92

---
+++ tests/weka/filters/UselessAttributeFilterTest.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2000 Webmind Inc. 
+ */
+
+package weka.filters;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.Attribute;
+
+/**
+ * Tests EmptyAttributeFilter. Run from the command line with:<p>
+ * java weka.filters.UselessAttributeFilterTest
+ *
+ * @author <a href="mailto:len@webmind.com">Len Trigg</a>
+ * @version $Revision: 1.1 $
+ */
+public class UselessAttributeFilterTest extends AbstractFilterTest {
+  
+  public UselessAttributeFilterTest(String name) { super(name);  }
+
+  /** Creates a default UselessAttributeFilter */
+  public Filter getFilter() {
+
+    UselessAttributeFilter f= new UselessAttributeFilter();
+    return f;
+  }
+
+  /**
+   * Make one of the test data attributes empty so the superclass tests
+   * are less trivial 
+   */
+  protected void setUp() throws Exception {
+
+    super.setUp();
+    makeEmpty(1, Instance.missingValue());
+  }
+
+  /** Make some of the attributes empty */
+  protected void makeEmpty(int index, double value) {
+
+    for (int i = 0; i < m_Instances.numInstances(); i++) {
+      if (!m_Instances.instance(i).isMissing(index)) {
+        m_Instances.instance(i).setValue(index, value);
+      }
+    }
+    if (VERBOSE) System.err.println(m_Instances);
+  }
+
+
+  public void testTypical() {
+
+    int del = 1; // setUp already makes one empty
+    for (int i = 0; i < m_Instances.numAttributes(); i++) {
+      makeEmpty(i, 0);
+      if (!m_Instances.attribute(i).isString() && (i != 1)) {
+        del++;
+      }
+      Instances result = useFilter();
+      assertEquals("Emptied <= " + i,
+                   m_Instances.numAttributes() - del, 
+                   result.numAttributes());
+    }
+  }
+
+  public static Test suite() {
+    return new TestSuite(UselessAttributeFilterTest.class);
+  }
+
+  public static void main(String[] args){
+    junit.textui.TestRunner.run(suite());
+  }
+
+}
