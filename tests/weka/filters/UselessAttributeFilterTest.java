d974fdf7fa18c788c4efa1d055a4381af1daec90
cvs_mhall <cvs_mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2002-02-15 00:33:05 +0000

    Fixed bug in testTypical
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@1210 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- tests/weka/filters/UselessAttributeFilterTest.java
+++ tests/weka/filters/UselessAttributeFilterTest.java
@@ -16,7 +16,7 @@ import weka.core.Attribute;
  * java weka.filters.UselessAttributeFilterTest
  *
  * @author <a href="mailto:len@webmind.com">Len Trigg</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class UselessAttributeFilterTest extends AbstractFilterTest {
   
@@ -36,7 +36,7 @@ public class UselessAttributeFilterTest extends AbstractFilterTest {
   protected void setUp() throws Exception {
 
     super.setUp();
-    makeEmpty(1, Instance.missingValue());
+    //    makeEmpty(1, Instance.missingValue());
   }
 
   /** Make some of the attributes empty */
@@ -53,14 +53,12 @@ public class UselessAttributeFilterTest extends AbstractFilterTest {
 
   public void testTypical() {
 
-    int del = 1; // setUp already makes one empty
+    int del = 0;
     for (int i = 0; i < m_Instances.numAttributes(); i++) {
       makeEmpty(i, 0);
-      if (!m_Instances.attribute(i).isString() && (i != 1)) {
-        del++;
-      }
+      del++;
       Instances result = useFilter();
-      assertEquals("Emptied <= " + i,
+      assertEquals("Emptied <= " + i + "("+del+")"+result,
                    m_Instances.numAttributes() - del, 
                    result.numAttributes());
     }
