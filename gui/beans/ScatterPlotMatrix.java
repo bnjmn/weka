cf33cc7c20873da4bb193b5d425a68289bfc3d51
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2003-04-08 22:51:16 +0000

    Initial import
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk/weka@1632 e0a1b77d-ad91-4216-81b1-defd5f83fa92

---
+++ gui/beans/ScatterPlotMatrix.java
@@ -0,0 +1,183 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ *    ScatterPlotMatrix.java
+ *    Copyright (C) 2003 Mark Hall
+ *
+ */
+
+package weka.gui.beans;
+
+import weka.core.Instances;
+import weka.gui.visualize.MatrixPanel;
+import weka.gui.visualize.VisualizePanel;
+import weka.gui.visualize.PlotData2D;
+
+import java.io.Serializable;
+import java.util.Vector;
+import java.util.Enumeration;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.BorderLayout;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
+import java.awt.*;
+
+/**
+ * Bean that encapsulates weka.gui.visualize.MatrixPanel for displaying a
+ * scatter plot matrix.
+ *
+ * @author <a href="mailto:mhall@cs.waikato.ac.nz">Mark Hall</a>
+ * @version $Revision: 1.1 $
+ */
+public class ScatterPlotMatrix extends JPanel
+  implements DataSourceListener, TrainingSetListener,
+	     TestSetListener, Visible, UserRequestAcceptor, Serializable {
+
+  protected BeanVisual m_visual = 
+    new BeanVisual("ScatterPlotMatrix", 
+		   BeanVisual.ICON_PATH+"ScatterPlotMatrix.gif",
+		   BeanVisual.ICON_PATH+"ScatterPlotMatrix_animated.gif");
+
+  private transient Instances m_visualizeDataSet;
+
+  private boolean m_framePoppedUp = false;
+
+  public ScatterPlotMatrix() {
+    setLayout(new BorderLayout());
+    add(m_visual, BorderLayout.CENTER);
+  }
+
+  /**
+   * Accept a training set
+   *
+   * @param e a <code>TrainingSetEvent</code> value
+   */
+  public void acceptTrainingSet(TrainingSetEvent e) {
+    Instances trainingSet = e.getTrainingSet();
+    DataSetEvent dse = new DataSetEvent(this, trainingSet);
+    acceptDataSet(dse);
+  }
+
+  /**
+   * Accept a test set
+   *
+   * @param e a <code>TestSetEvent</code> value
+   */
+  public void acceptTestSet(TestSetEvent e) {
+    Instances testSet = e.getTestSet();
+    DataSetEvent dse = new DataSetEvent(this, testSet);
+    acceptDataSet(dse);
+  }
+
+  /**
+   * Accept a data set
+   *
+   * @param e a <code>DataSetEvent</code> value
+   */
+  public synchronized void acceptDataSet(DataSetEvent e) {
+    m_visualizeDataSet = new Instances(e.getDataSet());
+    if (m_visualizeDataSet.classIndex() <= 0) {
+      m_visualizeDataSet.setClassIndex(m_visualizeDataSet.numAttributes()-1);
+    }
+  }
+
+  /**
+   * Set the visual appearance of this bean
+   *
+   * @param newVisual a <code>BeanVisual</code> value
+   */
+  public void setVisual(BeanVisual newVisual) {
+    m_visual = newVisual;
+  }
+
+  /**
+   * Return the visual appearance of this bean
+   */
+  public BeanVisual getVisual() {
+    return m_visual;
+  }
+
+  /**
+   * Use the default appearance for this bean
+   */
+  public void useDefaultVisual() {
+    m_visual.loadIcons(BeanVisual.ICON_PATH+"DefaultDataVisualizer.gif",
+		       BeanVisual.ICON_PATH+"DefaultDataVisualizer_animated.gif");
+  }
+
+  /**
+   * Return an enumeration of actions that the user can ask this bean to
+   * perform
+   *
+   * @return an <code>Enumeration</code> value
+   */
+  public Enumeration enumerateRequests() {
+    Vector newVector = new Vector(0);
+    if (m_visualizeDataSet != null && !m_framePoppedUp) {
+      newVector.addElement("Show plot");
+    }
+    return newVector.elements();
+  }
+
+  /**
+   * Perform a named user request
+   *
+   * @param request a string containing the name of the request to perform
+   * @exception IllegalArgumentException if request is not supported
+   */
+  public void performRequest(String request) {
+    if (request.compareTo("Show plot") == 0) {
+      try {
+	// popup matrix panel
+	if (!m_framePoppedUp) {
+	  m_framePoppedUp = true;
+	  final MatrixPanel vis = new MatrixPanel();
+	  vis.setInstances(m_visualizeDataSet);
+	  /*	  PlotData2D pd1 = new PlotData2D(m_visualizeDataSet);
+	  pd1.setPlotName(m_visualizeDataSet.relationName());
+	  try {
+	    vis.setMasterPlot(pd1);
+	  } catch (Exception ex) {
+	    System.err.println("Problem setting up "
+			       +"visualization (DataVisualizer)");
+	    ex.printStackTrace();
+	    } */
+	  final javax.swing.JFrame jf = 
+	    new javax.swing.JFrame("Visualize");
+	  jf.setSize(800,600);
+	  jf.getContentPane().setLayout(new BorderLayout());
+	  jf.getContentPane().add(vis, BorderLayout.CENTER);
+	  jf.addWindowListener(new java.awt.event.WindowAdapter() {
+	      public void windowClosing(java.awt.event.WindowEvent e) {
+		jf.dispose();
+		m_framePoppedUp = false;
+	      }
+	    });
+	  jf.setVisible(true);
+	}
+      } catch (Exception ex) {
+	ex.printStackTrace();
+	m_framePoppedUp = false;
+      }
+    } else {
+      throw new IllegalArgumentException(request
+					 + " not supported (ScatterPlotMatrix)");
+    }
+  }
+}
