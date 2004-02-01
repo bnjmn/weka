4b6d12e47ecc2ab3f07fe1979031bb23816e42cb
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2004-02-01 23:06:15 +0000

    Initial import.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk/weka@1935 e0a1b77d-ad91-4216-81b1-defd5f83fa92

---
+++ gui/beans/CustomizerClosingListener.java
@@ -0,0 +1,11 @@
+package weka.gui.beans;
+
+public interface CustomizerClosingListener {
+
+  /**
+   * Customizer classes that want to know when they are being
+   * disposed of can implement this method.
+   */
+  void customizerClosing();
+
+}
