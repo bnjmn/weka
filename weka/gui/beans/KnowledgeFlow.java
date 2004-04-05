38fed6f19251257269786ddf639019e72a526eaf
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2004-04-05 03:01:20 +0000

    Updated date in log
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@1995 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/gui/beans/KnowledgeFlow.java
+++ weka/gui/beans/KnowledgeFlow.java
@@ -100,7 +100,7 @@ import java.beans.IntrospectionException;
  * Main GUI class for the KnowledgeFlow
  *
  * @author <a href="mailto:mhall@cs.waikato.ac.nz">Mark Hall</a>
- * @version  $Revision: 1.16 $
+ * @version  $Revision: 1.17 $
  * @since 1.0
  * @see JPanel
  * @see PropertyChangeListener
@@ -235,7 +235,7 @@ public class KnowledgeFlow extends JPanel implements PropertyChangeListener {
    * connections
    *
    * @author <a href="mailto:mhall@cs.waikato.ac.nz">Mark Hall</a>
-   * @version $Revision: 1.16 $
+   * @version $Revision: 1.17 $
    * @since 1.0
    * @see JPanel
    */
@@ -460,7 +460,7 @@ public class KnowledgeFlow extends JPanel implements PropertyChangeListener {
        .format(new Date());
      m_logPanel.logMessage("Weka Knowledge Flow was written by Mark Hall");
      m_logPanel.logMessage("Weka Knowledge Flow");
-     m_logPanel.logMessage("(c) 2002-2003 Mark Hall");
+     m_logPanel.logMessage("(c) 2002-2004 Mark Hall");
      m_logPanel.logMessage("web: http://www.cs.waikato.ac.nz/~ml/");
      m_logPanel.logMessage( date);
      m_logPanel.statusMessage("Welcome to the Weka Knowledge Flow");
