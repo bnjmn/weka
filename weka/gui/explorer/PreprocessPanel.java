4ed52ae3d6eaf90a60b07db94b99a64afc5eeab1
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2004-05-04 21:34:33 +0000

    Multiple visualize all panels can now be popped up
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@2045 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/gui/explorer/PreprocessPanel.java
+++ weka/gui/explorer/PreprocessPanel.java
@@ -83,7 +83,7 @@ import weka.core.UnassignedClassException;
  *
  * @author Richard Kirkby (rkirkby@cs.waikato.ac.nz)
  * @author Len Trigg (trigg@cs.waikato.ac.nz)
- * @version $Revision: 1.45 $
+ * @version $Revision: 1.46 $
  */
 public class PreprocessPanel extends JPanel {
   
@@ -378,7 +378,6 @@ public class PreprocessPanel extends JPanel {
 	public void actionPerformed(ActionEvent ae) {
 	  if (m_Instances != null) {
 	    try {
-	      visAllBut.setEnabled(false);
 	      final weka.gui.beans.AttributeSummarizer as = 
 		new weka.gui.beans.AttributeSummarizer();
 	      as.setColoringIndex(m_AttVisualizePanel.getColoringIndex());
