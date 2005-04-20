1dd7625cb87cee9d9736e4a5445a1f0c898b406f
fracpete <fracpete@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2005-04-20 03:47:16 +0000

    double click on a classifier has the same functionality as the edit button (makes the editing faster)
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@2349 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/gui/experiment/AlgorithmListPanel.java
+++ weka/gui/experiment/AlgorithmListPanel.java
@@ -42,6 +42,9 @@ import java.awt.Component;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.ActionListener;
@@ -76,7 +79,7 @@ import java.io.File;
  * iterate over.
  *
  * @author Richard Kirkby (rkirkby@cs.waikato.ac.nz)
- * @version $Revision: 1.11 $
+ * @version $Revision: 1.12 $
  */
 public class AlgorithmListPanel extends JPanel implements ActionListener {
 
@@ -194,6 +197,20 @@ public class AlgorithmListPanel extends JPanel implements ActionListener {
   public AlgorithmListPanel() {
     
     m_List = new JList();
+    MouseListener mouseListener = new MouseAdapter() {
+      public void mouseClicked(MouseEvent e) {
+        if (e.getClickCount() == 2) {
+          // unfortunately, locationToIndex only returns the nearest entry
+          // and not the exact one, i.e. if there's one item in the list and
+          // one doublelclicks somewhere in the list, this index will be
+          // returned
+          int index = m_List.locationToIndex(e.getPoint());
+          if (index > -1)
+            actionPerformed(new ActionEvent(m_EditBut, 0, ""));
+        }
+      }
+    };
+    m_List.addMouseListener(mouseListener);
   
     m_ClassifierEditor.setClassType(Classifier.class);
     m_ClassifierEditor.setValue(new weka.classifiers.rules.ZeroR());
@@ -414,6 +431,9 @@ public class AlgorithmListPanel extends JPanel implements ActionListener {
           }
         }
       }
+    }
+    else if (e.getSource() == m_List) {
+      System.out.println("yep!");
     }
   }
 
