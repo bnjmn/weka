b4d4452df67b1444542ec97832b017e96920d5c1
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2005-06-09 02:37:58 +0000

    Fixed a couple of event names
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/branches/book2ndEd-branch@2439 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/gui/beans/ClassifierPerformanceEvaluatorBeanInfo.java
+++ weka/gui/beans/ClassifierPerformanceEvaluatorBeanInfo.java
@@ -28,7 +28,7 @@ import java.beans.*;
  * Bean info class for the classifier performance evaluator
  *
  * @author <a href="mailto:mhall@cs.waikato.ac.nz">Mark Hall</a>
- * @version $Revision: 1.2.2.1 $
+ * @version $Revision: 1.2.2.2 $
  */
 public class ClassifierPerformanceEvaluatorBeanInfo extends SimpleBeanInfo {
   
@@ -40,11 +40,11 @@ public class ClassifierPerformanceEvaluatorBeanInfo extends SimpleBeanInfo {
 			       TextListener.class, 
 			       "acceptText"),
 	new EventSetDescriptor(ClassifierPerformanceEvaluator.class, 
-			       "ThresholdData", 
+			       "thresholdData", 
 			       ThresholdDataListener.class, 
 			       "acceptDataSet"),
       	new EventSetDescriptor(ClassifierPerformanceEvaluator.class, 
-			       "VisualizableError", 
+			       "visualizableError", 
 			       VisualizableErrorListener.class, 
 			       "acceptDataSet")};
       return esds;
