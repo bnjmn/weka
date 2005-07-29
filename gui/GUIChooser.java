210a4563958b26059259200bb69fa6dc1a8faf0d
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2005-07-29 01:33:36 +0000

    Adjusted the size of the KnowledgeFlow window
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk/weka@2585 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- gui/GUIChooser.java
+++ gui/GUIChooser.java
@@ -59,7 +59,7 @@ import javax.swing.BorderFactory;
  *
  * @author Len Trigg (trigg@cs.waikato.ac.nz)
  * @author Mark Hall (mhall@cs.waikato.ac.nz)
- * @version $Revision: 1.23 $
+ * @version $Revision: 1.24 $
  */
 public class GUIChooser extends JFrame {
 
@@ -240,7 +240,7 @@ public class GUIChooser extends JFrame {
                 }
               });
             m_KnowledgeFlowFrame.pack();
-            m_KnowledgeFlowFrame.setSize(800, 600);
+            m_KnowledgeFlowFrame.setSize(900, 600);
             m_KnowledgeFlowFrame.setVisible(true);
           }
         }
