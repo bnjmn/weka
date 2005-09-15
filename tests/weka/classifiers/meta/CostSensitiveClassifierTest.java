4d11b33a914fb332b42b6ed1a1efdd3e2d4bfc55
fracpete <fracpete@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2005-09-15 00:33:50 +0000

    cost matrix can be loaded from a jar archive now
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/branches/book2ndEd-branch@2666 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- tests/weka/classifiers/meta/CostSensitiveClassifierTest.java
+++ tests/weka/classifiers/meta/CostSensitiveClassifierTest.java
@@ -5,18 +5,21 @@
 package weka.classifiers.meta;
 
 import weka.classifiers.*;
+import weka.core.Instances;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-import weka.core.Instances;
+
 import java.io.File;
+import java.io.InputStreamReader;
 
 /**
  * Tests CostSensitiveClassifier. Run from the command line with:<p>
- * java weka.classifiers.CostSensitiveClassifierTest
+ * java weka.classifiers.meta.CostSensitiveClassifierTest
  *
  * @author <a href="mailto:eibe@cs.waikato.ac.nz">Eibe Frank</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.2.2.1 $
  */
 public class CostSensitiveClassifierTest extends AbstractClassifierTest {
 
@@ -26,9 +29,18 @@ public class CostSensitiveClassifierTest extends AbstractClassifierTest {
   public Classifier getClassifier() {
 
     CostSensitiveClassifier cl = new CostSensitiveClassifier();
-    String str = ClassLoader.getSystemResource("weka/classifiers/data").toString();
-    str = str.substring(5);
-    cl.setOnDemandDirectory(new File(str));
+    
+    // load costmatrix
+    try {
+      cl.setCostMatrix(
+          new CostMatrix(
+            new InputStreamReader(ClassLoader.getSystemResourceAsStream(
+                  "weka/classifiers/data/ClassifierTest.cost"))));
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+    
     return cl;
   }
 
