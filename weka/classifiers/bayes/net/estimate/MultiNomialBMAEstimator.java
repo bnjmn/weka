009723c7d135ec4721c2a5e4e3b012ac47a36502
fracpete <fracpete@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2005-09-22 01:18:51 +0000

    added preamble (GPL etc.)
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@2712 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/classifiers/bayes/net/estimate/MultiNomialBMAEstimator.java
+++ weka/classifiers/bayes/net/estimate/MultiNomialBMAEstimator.java
@@ -1,3 +1,19 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 package weka.classifiers.bayes.net.estimate;
 
 import weka.classifiers.bayes.BayesNet;
@@ -10,7 +26,7 @@ import weka.estimators.Estimator;
 import weka.classifiers.bayes.net.search.local.K2;
 
 /**
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  */
 public class MultiNomialBMAEstimator extends BayesNetEstimator {
 
--- weka/classifiers/functions/pace/Matrix.java
+++ weka/classifiers/functions/pace/Matrix.java
@@ -1,3 +1,11 @@
+/*
+ * This software is a cooperative product of The MathWorks and the National
+ * Institute of Standards and Technology (NIST) which has been released to the
+ * public domain. Neither The MathWorks nor NIST assumes any responsibility
+ * whatsoever for its use by other parties, and makes no guarantees, expressed
+ * or implied, about its quality, reliability, or any other characteristic.
+ */
+
 package weka.classifiers.functions.pace;
 
 import java.text.NumberFormat;
@@ -50,7 +58,7 @@ import java.io.StreamTokenizer;
 
 * @author The MathWorks, Inc. and the National Institute of Standards and
 * Technology.
-* @version $Revision: 1.2 $ (5 August 1998)
+* @version $Revision: 1.3 $ (5 August 1998)
 */
 
 /**
