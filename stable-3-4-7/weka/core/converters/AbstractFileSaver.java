5aa60092a710ada899ceefe24e8c27e9992c1a32
(no author) <(no author)@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2005-12-22 02:06:45 +0000

    This commit was manufactured by cvs2svn to create tag 'stable-3-4-7'.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/tags@2852 e0a1b77d-ad91-4216-81b1-defd5f83fa92

---
+++ stable-3-4-7/weka/core/converters/AbstractFileSaver.java
@@ -0,0 +1,381 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ *    AbstractFileSaver.java
+ *    Copyright (C) 2004 Richard Kirkby, Stefan Mutter
+ *
+ */
+
+package weka.core.converters;
+
+import weka.core.Instances;
+import weka.core.Instance;
+import weka.core.OptionHandler;
+import weka.core.FastVector;
+import weka.core.Option;
+import weka.core.Utils;
+
+import java.util.Enumeration;
+import java.io.*;
+
+/**
+ * Abstract class for Savers that save to a file
+ *
+ * Valid options are:
+ *
+ * -i input arff file <br>
+ * The input filw in arff format. <p>
+ *
+ * -o the output file <br>
+ * The output file. The prefix of the output file is sufficient. If no output file is given, Saver tries to use standard out. <p>
+ *
+ *
+ * @author Richard Kirkby (rkirkby@cs.waikato.ac.nz)
+ * @author Stefan Mutter (mutter@cs.waikato.ac.nz)
+ * @version $Revision: 1.1 $
+ */
+public abstract class AbstractFileSaver extends AbstractSaver implements OptionHandler, FileSourcedConverter{
+  
+  
+  /** The destination file. */
+  private File m_outputFile;
+  
+  /** The writer. */
+  private BufferedWriter m_writer;
+  
+  /** The file extension of the destination file. */
+  private String FILE_EXTENSION;
+  
+  
+  /** The prefix for the filename (chosen in the GUI). */
+  private String m_prefix;
+  
+  /** The directory of the file (chosen in the GUI).*/
+  private String m_dir;
+  
+  /** Counter. In incremental mode after reading 100 instances they will be written to a file.*/
+  protected int m_incrementalCounter;
+  
+  
+  /**
+   * resets the options
+   *
+   */
+  public void resetOptions(){
+      
+     super.resetOptions();
+     m_outputFile = null;
+     m_writer = null;
+     m_prefix = "";
+     m_dir = "";
+     m_incrementalCounter = 0;
+  }
+  
+ 
+ 
+  /**
+   * Gets the writer
+   *
+   * @return the BufferedWriter
+   */
+  public BufferedWriter getWriter(){
+   
+      return m_writer;
+  }
+  
+  /** Sets the writer to null. */  
+  public void resetWriter(){
+   
+      m_writer = null;
+  }
+  
+  /**
+   * Gets ihe file extension.
+   *
+   * @return the file extension as a string.
+   */
+  public String getFileExtension(){
+   
+      return FILE_EXTENSION;
+  }
+  
+  
+  /**
+   * Sets ihe file extension.
+   *
+   * @param ext the file extension as a string startin with '.'.
+   */ 
+  protected void setFileExtension(String ext){
+   
+      FILE_EXTENSION = ext;
+  }
+  
+  
+  /**
+   * Gets the destination file.
+   *
+   * @return the destination file.
+   */
+  public File retrieveFile(){
+  
+      return m_outputFile;
+  }
+ 
+  /** Sets the destination file.
+   * @param outputFile the destination file.
+   * @throws IOException throws an IOException if file cannot be set
+   */
+  public void setFile(File outputFile) throws IOException  {
+      
+      m_outputFile = outputFile;
+      
+  }
+
+  
+  /** Sets the file name prefix
+   * @param prefix the file name prefix
+   */  
+  public void setFilePrefix(String prefix){
+   
+      m_prefix = prefix;
+  }
+  
+  /** Gets the file name prefix
+   * @return the prefix of the filename
+   */  
+  public String filePrefix(){
+   
+      return m_prefix;
+  }
+  
+  /** Sets the directory where the instances should be stored
+   * @param dir a string containing the directory path and name
+   */  
+  public void setDir(String dir){
+   
+      m_dir = dir;
+  }
+  
+  /** Gets the directory
+   * @return a string with the file name
+   */  
+  public String retrieveDir(){
+   
+      return m_dir;
+  }
+  
+  
+  /**
+   * Returns an enumeration describing the available options.
+   *
+   * @return an enumeration of all the available options.
+   */
+  public Enumeration listOptions() {
+
+    FastVector newVector = new FastVector(2);
+
+    newVector.addElement(new Option("The input file", "i", 1, 
+				    "-i <the input file>"));
+    
+    newVector.addElement(new Option("The output file", "o", 1, 
+				    "-o <the output file>"));
+    
+    return newVector.elements();
+  }
+
+ 
+/**
+   * Parses a given list of options. Valid option is:<p>
+   *   
+   * -i input arff file <br>
+   * The input filw in arff format. <p>
+   *
+   * -o the output file <br>
+   * The output file. The prefix of the output file is sufficient. If no output file is given, Saver tries to use standard out. <p>
+   *
+   *
+   * @param options the list of options as an array of strings
+   * @exception Exception if an option is not supported 
+   */
+  public void setOptions(String[] options) throws Exception {
+    
+    String outputString = Utils.getOption('o', options);
+    String inputString = Utils.getOption('i', options);
+    
+    ArffLoader loader = new ArffLoader();
+    
+    resetOptions();
+    
+    if(inputString.length() != 0){
+        try{
+            File input = new File(inputString);
+            loader.setFile(input);
+            setInstances(loader.getDataSet());
+        } catch(Exception ex){
+            throw new IOException("No data set loaded. Data set has to be in ARFF format.");
+        }
+    }
+    else
+        throw new IOException("No data set to save.");
+    if (outputString.length() != 0){ 
+        //add appropriate file extension
+        if(!outputString.endsWith(FILE_EXTENSION)){
+            if(outputString.lastIndexOf('.') != -1)
+                outputString = (outputString.substring(0,outputString.lastIndexOf('.'))) + FILE_EXTENSION;
+            else
+                outputString = outputString + FILE_EXTENSION;
+        }
+        try{
+            File output = new File(outputString);
+            setFile(output);
+        } catch(Exception ex){
+            throw new IOException("Cannot create output file. Standard out is used.");
+        } finally{
+            setDestination(m_outputFile);
+        }
+    }
+
+  }
+
+  /**
+   * Gets the current settings of the Saver object.
+   *
+   * @return an array of strings suitable for passing to setOptions
+   */
+  public String [] getOptions() {
+
+    String [] options = new String [10];
+    int current = 0;
+    if(m_outputFile != null){
+        options[current++] = "-o"; options[current++] = "" + m_outputFile;
+    }
+    else{
+        options[current++] = "-o"; options[current++] = "";
+    }
+    if(getInstances() != null){
+        options[current++] = "-i"; options[current++] = "" + getInstances().relationName();
+    }
+    else{
+        options[current++] = "-i"; options[current++] = "";
+    }
+    while (current < options.length) {
+      options[current++] = "";
+    }
+    return options;
+  }
+
+
+  /** Cancels the incremental saving process. */  
+  public void cancel(){
+  
+      if(getWriteMode() == CANCEL){
+        if(m_outputFile != null && m_outputFile.exists()){
+            if(m_outputFile.delete())
+                System.out.println("File deleted.");
+        }
+        resetOptions();
+      }
+  }
+  
+  /**
+   * Sets the destination file (and directories if necessary).
+   *
+   * @param file the File
+   * @exception IOException always
+   */
+  public void setDestination(File file) throws IOException {
+
+    boolean success = false;
+    String out = file.getAbsolutePath();
+    if(m_outputFile != null){
+        try{
+            if(file.exists()){
+                if(file.delete())
+                    System.out.println("File exists and will be overridden.");
+                else
+                    throw new IOException("File already exists."); 
+            }
+            if(out.lastIndexOf(File.separatorChar) == -1){
+                success = file.createNewFile();
+            }
+            else{
+                String outPath = out.substring(0,out.lastIndexOf(File.separatorChar));
+                File dir = new File(outPath);
+                if(dir.exists())
+                    success = file.createNewFile();
+                else{
+                    dir.mkdirs();
+                    success = file.createNewFile();
+                }
+            }
+            if(success){ 
+                m_outputFile = file;
+                setDestination(new FileOutputStream(m_outputFile));
+            }
+        } catch(Exception ex){
+            throw new IOException("Cannot create a new output file. Standard out is used.");
+        } finally{
+            if(!success){
+                System.err.println("Cannot create a new output file. Standard out is used.");
+                m_outputFile = null; //use standard out
+            }
+        }
+    }
+  }
+  
+  
+  /** Sets the destination output stream.
+   * @param output the output stream.
+   * @throws IOException throws an IOException if destination cannot be set
+   */
+  public void setDestination(OutputStream output) throws IOException {
+
+    m_writer = new BufferedWriter(new OutputStreamWriter(output));
+  }
+  
+
+  /** Sets the directory and the file prefix.
+   * This method is used in the KnowledgeFlow GUI
+   * @param relationName the name of the relation to save
+   * @param add additional string which should be part of the filename
+   */  
+  public void setDirAndPrefix(String relationName, String add){
+  
+      try{
+        if(m_dir.equals(""))
+            m_dir = System.getProperty("user.dir");
+        if(m_prefix.equals(""))
+            setFile(new File(m_dir + File.separator + relationName+ add + FILE_EXTENSION));
+        else
+           setFile(new File(m_dir + File.separator + m_prefix + "_" + relationName+ add + FILE_EXTENSION)); 
+        setDestination(m_outputFile);
+      }catch(Exception ex){
+        System.err.println("File prefix and/or directory could not have been set.");
+        ex.printStackTrace();
+      }
+  }
+  
+    /**
+   * to be pverridden
+   *
+   * @return the file type description.
+   */
+  public abstract String getFileDescription();
+  
+  
+  
+}
---
+++ stable-3-4-7/weka/gui/GenericArrayEditor.java
@@ -0,0 +1,544 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ *    GenericArrayEditor.java
+ *    Copyright (C) 1999 Len Trigg
+ *
+ */
+
+
+package weka.gui;
+
+import weka.core.SelectedTag;
+import weka.core.SerializedObject;
+import weka.classifiers.Classifier;
+
+import java.lang.reflect.Array;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeSupport;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyEditor;
+import java.beans.PropertyEditorManager;
+import java.awt.Graphics;
+import java.awt.Insets;
+import java.awt.Rectangle;
+import java.awt.Font;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.BorderLayout;
+import java.awt.FontMetrics;
+import java.awt.FlowLayout;
+import java.awt.event.ItemListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.ListCellRenderer;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JPanel;
+import javax.swing.JList;
+import javax.swing.ListModel;
+import javax.swing.DefaultListModel;
+import javax.swing.JScrollPane;
+import javax.swing.SwingConstants;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.JOptionPane;
+
+
+/** 
+ * A PropertyEditor for arrays of objects that themselves have
+ * property editors.
+ *
+ * @author Len Trigg (trigg@cs.waikato.ac.nz)
+ * @version $Revision: 1.13 $
+ */
+public class GenericArrayEditor extends JPanel
+  implements PropertyEditor {
+
+  /** Handles property change notification */
+  private PropertyChangeSupport m_Support = new PropertyChangeSupport(this);
+
+  /** The label for when we can't edit that type */
+  private JLabel m_Label = new JLabel("Can't edit", SwingConstants.CENTER);
+  
+  /** The list component displaying current values */
+  private JList m_ElementList = new JList();
+
+  /** The class of objects allowed in the array */
+  private Class m_ElementClass = String.class;
+
+  /** The defaultlistmodel holding our data */
+  private DefaultListModel m_ListModel;
+
+  /** The property editor for the class we are editing */
+  private PropertyEditor m_ElementEditor;
+
+  /** Click this to delete the selected array values */
+  private JButton m_DeleteBut = new JButton("Delete");
+
+  /** Click to add the current object configuration to the array */
+  private JButton m_AddBut = new JButton("Add");
+
+  /** Listens to buttons being pressed and taking the appropriate action */
+  private ActionListener m_InnerActionListener =
+    new ActionListener() {
+
+    public void actionPerformed(ActionEvent e) {
+
+      if (e.getSource() == m_DeleteBut) {
+	int [] selected = m_ElementList.getSelectedIndices();
+	if (selected != null) {
+	  for (int i = 0; i < selected.length; i++) {
+	    int current = selected[i];
+	    m_ListModel.removeElementAt(current);
+	    if (m_ListModel.size() > current) {
+	      m_ElementList.setSelectedIndex(current);
+	    }
+	  }
+	  m_Support.firePropertyChange("", null, null);
+	}
+	if (m_ElementList.getSelectedIndex() == -1) {
+	  m_DeleteBut.setEnabled(false);
+	}
+      } else if (e.getSource() == m_AddBut) {
+	int selected = m_ElementList.getSelectedIndex();
+	Object addObj = m_ElementEditor.getValue();
+	
+	// Make a full copy of the object using serialization
+	try {
+          SerializedObject so = new SerializedObject(addObj);
+	  addObj = so.getObject();
+	  if (selected != -1) {
+	    m_ListModel.insertElementAt(addObj, selected);
+	  } else {
+	    m_ListModel.addElement(addObj);
+	  }
+	  m_Support.firePropertyChange("", null, null);
+	} catch (Exception ex) {
+	  JOptionPane.showMessageDialog(GenericArrayEditor.this,
+					"Could not create an object copy",
+					null,
+					JOptionPane.ERROR_MESSAGE);
+	}
+      } 
+    }
+  };
+
+  /** Listens to list items being selected and takes appropriate action */
+  private ListSelectionListener m_InnerSelectionListener =
+    new ListSelectionListener() {
+
+      public void valueChanged(ListSelectionEvent e) {
+
+	if (e.getSource() == m_ElementList) {
+	  // Enable the delete button
+	  if (m_ElementList.getSelectedIndex() != -1) {
+	    m_DeleteBut.setEnabled(true);
+	  }
+	}
+      }
+  };
+    
+
+  /**
+   * Sets up the array editor.
+   */
+  public GenericArrayEditor() {
+
+    setLayout(new BorderLayout());
+    add(m_Label, BorderLayout.CENTER);
+    m_DeleteBut.addActionListener(m_InnerActionListener);
+    m_AddBut.addActionListener(m_InnerActionListener);
+    m_ElementList.addListSelectionListener(m_InnerSelectionListener);
+    m_AddBut.setToolTipText("Add the current item to the list");
+    m_DeleteBut.setToolTipText("Delete the selected list item");
+  }
+
+  /** This class handles the creation of list cell renderers from the 
+   * property editors.
+   */
+  private class EditorListCellRenderer implements ListCellRenderer {
+
+    /** The class of the property editor for array objects */
+    private Class m_EditorClass;
+
+    /** The class of the array values */
+    private Class m_ValueClass;
+
+    /**
+     * Creates the list cell renderer.
+     *
+     * @param editorClass The class of the property editor for array objects
+     * @param valueClass The class of the array values
+     */
+    public EditorListCellRenderer(Class editorClass, Class valueClass) {
+      m_EditorClass = editorClass;
+      m_ValueClass = valueClass;
+    }
+
+    /**
+     * Creates a cell rendering component.
+     *
+     * @param JList the list that will be rendered in
+     * @param Object the cell value
+     * @param int which element of the list to render
+     * @param boolean true if the cell is selected
+     * @param boolean true if the cell has the focus
+     * @return the rendering component
+     */
+    public Component getListCellRendererComponent(final JList list,
+						  final Object value,
+						  final int index,
+						  final boolean isSelected,
+						  final boolean cellHasFocus) {
+      try {
+	final PropertyEditor e = (PropertyEditor)m_EditorClass.newInstance();
+	if (e instanceof GenericObjectEditor) {
+	  //	  ((GenericObjectEditor) e).setDisplayOnly(true);
+	  ((GenericObjectEditor) e).setClassType(m_ValueClass);
+	}
+	e.setValue(value);
+	return new JPanel() {
+
+	  public void paintComponent(Graphics g) {
+
+	    Insets i = this.getInsets();
+	    Rectangle box = new Rectangle(i.left, i.top,
+					  this.getWidth() - i.right,
+					  this.getHeight() - i.bottom );
+	    g.setColor(isSelected
+		       ? list.getSelectionBackground()
+		       : list.getBackground());
+	    g.fillRect(0, 0, this.getWidth(), this.getHeight());
+	    g.setColor(isSelected
+		       ? list.getSelectionForeground()
+		       : list.getForeground());
+	    e.paintValue(g, box);
+	  }
+	  
+	  public Dimension getPreferredSize() {
+
+	    Font f = this.getFont();
+	    FontMetrics fm = this.getFontMetrics(f);
+	    return new Dimension(0, fm.getHeight());
+	  }
+	};
+      } catch (Exception ex) {
+	return null;
+      }
+    }
+  }
+
+  /**
+   * Updates the type of object being edited, so attempts to find an
+   * appropriate propertyeditor.
+   *
+   * @param o a value of type 'Object'
+   */
+  private void updateEditorType(Object o) {
+
+    // Determine if the current object is an array
+    m_ElementEditor = null; m_ListModel = null;
+    removeAll();
+    if ((o != null) && (o.getClass().isArray())) {
+      Class elementClass = o.getClass().getComponentType();    
+      PropertyEditor editor = PropertyEditorManager.findEditor(elementClass);
+      Component view = null;
+      ListCellRenderer lcr = new DefaultListCellRenderer();
+      if (editor != null) {
+	if (editor instanceof GenericObjectEditor) {
+	  ((GenericObjectEditor) editor).setClassType(elementClass);
+	}
+
+        //setting the value in the editor so that
+        //we don't get a NullPointerException
+        //when we do getAsText() in the constructor of
+        //PropertyValueSelector()
+	if(Array.getLength(o) > 0) {
+	  editor.setValue(Array.get(o,0));
+	} else {
+	  if (editor instanceof GenericObjectEditor) {
+	    ((GenericObjectEditor)editor).setDefaultValue();
+	  } else {   
+            try {
+	    editor.setValue(elementClass.newInstance());
+            } catch(Exception ex) {
+              m_ElementEditor=null;
+              System.err.println(ex.getMessage());
+              add(m_Label, BorderLayout.CENTER);
+              m_Support.firePropertyChange("", null, null);
+              validate();
+              return;
+            }
+	  }
+	}
+        
+	if (editor.isPaintable() && editor.supportsCustomEditor()) {
+	  view = new PropertyPanel(editor);
+	  lcr = new EditorListCellRenderer(editor.getClass(), elementClass);
+	} else if (editor.getTags() != null) {
+	  view = new PropertyValueSelector(editor);
+	} else if (editor.getAsText() != null) {
+	  view = new PropertyText(editor);
+	}
+      }
+      if (view == null) {
+	System.err.println("No property editor for class: "
+			   + elementClass.getName());
+      } else {
+	m_ElementEditor = editor;
+
+	// Create the ListModel and populate it
+	m_ListModel = new DefaultListModel();
+	m_ElementClass = elementClass;
+	for (int i = 0; i < Array.getLength(o); i++) {
+	  m_ListModel.addElement(Array.get(o,i));
+	}
+	m_ElementList.setCellRenderer(lcr);
+	m_ElementList.setModel(m_ListModel);
+	if (m_ListModel.getSize() > 0) {
+	  m_ElementList.setSelectedIndex(0);
+	  m_DeleteBut.setEnabled(true);
+	} else {
+	  m_DeleteBut.setEnabled(false);
+	}
+
+        //have already set the value above in the editor
+	//try {
+	  //if (m_ListModel.getSize() > 0) {
+	  //  m_ElementEditor.setValue(m_ListModel.getElementAt(0));
+	  //} else {
+	  //  if (m_ElementEditor instanceof GenericObjectEditor) {
+	  //    ((GenericObjectEditor)m_ElementEditor).setDefaultValue();
+	  //  } else {
+	  //    m_ElementEditor.setValue(m_ElementClass.newInstance());
+	  //  }
+	  //}
+	  
+	  JPanel panel = new JPanel();
+	  panel.setLayout(new BorderLayout());
+	  panel.add(view, BorderLayout.CENTER);
+	  panel.add(m_AddBut, BorderLayout.EAST);
+	  add(panel, BorderLayout.NORTH);
+	  add(new JScrollPane(m_ElementList), BorderLayout.CENTER);
+	  add(m_DeleteBut, BorderLayout.SOUTH);
+	  m_ElementEditor
+	    .addPropertyChangeListener(new PropertyChangeListener() {
+	    public void propertyChange(PropertyChangeEvent e) {
+	      repaint();
+	    }
+	  });
+	//} catch (Exception ex) {
+	//  System.err.println(ex.getMessage());
+	//  m_ElementEditor = null;
+	//}
+      }
+    }
+    if (m_ElementEditor == null) {
+      add(m_Label, BorderLayout.CENTER);
+    }
+    m_Support.firePropertyChange("", null, null);
+    validate();
+  }
+
+  /**
+   * Sets the current object array.
+   *
+   * @param o an object that must be an array.
+   */
+  public void setValue(Object o) {
+
+    // Create a new list model, put it in the list and resize?
+    updateEditorType(o);
+  }
+
+  /**
+   * Gets the current object array.
+   *
+   * @return the current object array
+   */
+  public Object getValue() {
+
+    if (m_ListModel == null) {
+      return null;
+    }
+    // Convert the listmodel to an array of strings and return it.
+    int length = m_ListModel.getSize();
+    Object result = Array.newInstance(m_ElementClass, length);
+    for (int i = 0; i < length; i++) {
+      Array.set(result, i, m_ListModel.elementAt(i));
+    }
+    return result;
+  }
+  
+  /**
+   * Supposedly returns an initialization string to create a classifier
+   * identical to the current one, including it's state, but this doesn't
+   * appear possible given that the initialization string isn't supposed to
+   * contain multiple statements.
+   *
+   * @return the java source code initialisation string
+   */
+  public String getJavaInitializationString() {
+
+    return "null";
+  }
+
+  /**
+   * Returns true to indicate that we can paint a representation of the
+   * string array
+   *
+   * @return true
+   */
+  public boolean isPaintable() {
+    return true;
+  }
+
+  /**
+   * Paints a representation of the current classifier.
+   *
+   * @param gfx the graphics context to use
+   * @param box the area we are allowed to paint into
+   */
+  public void paintValue(java.awt.Graphics gfx, java.awt.Rectangle box) {
+
+    FontMetrics fm = gfx.getFontMetrics();
+    int vpad = (box.height - fm.getAscent()) / 2;
+    String rep = m_ListModel.getSize() + " " + m_ElementClass.getName();
+    gfx.drawString(rep, 2, fm.getHeight() + vpad);
+  }
+
+  /**
+   * Returns null as we don't support getting/setting values as text.
+   *
+   * @return null
+   */
+  public String getAsText() {
+    return null;
+  }
+
+  /**
+   * Returns null as we don't support getting/setting values as text. 
+   *
+   * @param text the text value
+   * @exception IllegalArgumentException as we don't support
+   * getting/setting values as text.
+   */
+  public void setAsText(String text) {
+    throw new IllegalArgumentException(text);
+  }
+
+  /**
+   * Returns null as we don't support getting values as tags.
+   *
+   * @return null
+   */
+  public String[] getTags() {
+    return null;
+  }
+
+  /**
+   * Returns true because we do support a custom editor.
+   *
+   * @return true
+   */
+  public boolean supportsCustomEditor() {
+    return true;
+  }
+  
+  /**
+   * Returns the array editing component.
+   *
+   * @return a value of type 'java.awt.Component'
+   */
+  public java.awt.Component getCustomEditor() {
+    return this;
+  }
+
+  /**
+   * Adds a PropertyChangeListener who will be notified of value changes.
+   *
+   * @param l a value of type 'PropertyChangeListener'
+   */
+  public void addPropertyChangeListener(PropertyChangeListener l) {
+    m_Support.addPropertyChangeListener(l);
+  }
+
+  /**
+   * Removes a PropertyChangeListener.
+   *
+   * @param l a value of type 'PropertyChangeListener'
+   */
+  public void removePropertyChangeListener(PropertyChangeListener l) {
+    m_Support.removePropertyChangeListener(l);
+  }
+
+  /**
+   * Tests out the array editor from the command line.
+   *
+   * @param args ignored
+   */
+  public static void main(String [] args) {
+
+    try {
+      System.err.println("---Registering Weka Editors---");
+      java.beans.PropertyEditorManager
+	.registerEditor(Classifier.class,
+			GenericObjectEditor.class);
+      java.beans.PropertyEditorManager
+	.registerEditor(SelectedTag.class,
+			SelectedTagEditor.class);
+      java.beans.PropertyEditorManager
+	.registerEditor(weka.filters.Filter.class,
+			GenericObjectEditor.class);
+      java.beans.PropertyEditorManager
+	.registerEditor(String [].class,
+			GenericArrayEditor.class);
+      final GenericArrayEditor ce = new GenericArrayEditor();
+
+      final weka.filters.Filter [] initial = new weka.filters.Filter [0];
+	/*
+      {
+	new weka.filters.AddFilter()
+	};*/
+      /*
+      final String [] initial = {
+	"Hello",
+	"There",
+	"Bob"
+	};*/
+      PropertyDialog pd = new PropertyDialog(ce, 100, 100);
+      pd.setSize(200,200);
+      pd.addWindowListener(new WindowAdapter() {
+	public void windowClosing(WindowEvent e) {
+	  System.exit(0);
+	}
+      });
+      ce.setValue(initial);
+      //ce.validate();
+    } catch (Exception ex) {
+      ex.printStackTrace();
+      System.err.println(ex.getMessage());
+    }
+  }
+
+}
+
