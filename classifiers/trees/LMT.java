c454f6cee6342384bd1bd71f7c79582af5af7aa6
fracpete <fracpete@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2006-05-24 01:31:44 +0000

    fixed Technical Information updated Javadoc
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk/weka@3201 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- classifiers/trees/LMT.java
+++ classifiers/trees/LMT.java
@@ -54,20 +54,32 @@ import java.util.Vector;
  * <br/>
  * For more information see: <br/>
  * <br/>
- * Niels Landwehr, Mark Hall, Eibe Frank: Logistic Model Trees. In: 14th European Conference on Machine Learning, 241-252, 2003.
+ * Niels Landwehr, Mark Hall, Eibe Frank (2005). Logistic Model Trees.<br/>
+ * <br/>
+ * Marc Sumner, Eibe Frank, Mark Hall: Speeding up Logistic Model Tree Induction. In: 9th European Conference on Principles and Practice of Knowledge Discovery in Databases, 675-683, 2005.
  * <p/>
  <!-- globalinfo-end -->
  *
  <!-- technical-bibtex-start -->
  * BibTeX:
  * <pre>
- * &#64;incproceedings{Landwehr2003,
+ * &#64;article{Landwehr2005,
  *    author = {Niels Landwehr and Mark Hall and Eibe Frank},
- *    booktitle = {14th European Conference on Machine Learning},
- *    pages = {241-252},
- *    publisher = {Springer},
+ *    booktitle = {Machine Learning},
+ *    number = {1-2},
+ *    pages = {161-205},
  *    title = {Logistic Model Trees},
- *    year = {2003}
+ *    volume = {95},
+ *    year = {2005}
+ * }
+ * 
+ * &#64;inproceedings{Sumner2005,
+ *    author = {Marc Sumner and Eibe Frank and Mark Hall},
+ *    booktitle = {9th European Conference on Principles and Practice of Knowledge Discovery in Databases},
+ *    pages = {675-683},
+ *    publisher = {Springer},
+ *    title = {Speeding up Logistic Model Tree Induction},
+ *    year = {2005}
  * }
  * </pre>
  * <p/>
@@ -94,11 +106,17 @@ import java.util.Vector;
  * <pre> -M &lt;numInstances&gt;
  *  Set minimum number of instances at which a node can be split (default 15)</pre>
  * 
+ * <pre> -W &lt;beta&gt;
+ *  Set beta for weight trimming for LogitBoost. Set to 0 (default) for no weight trimming.</pre>
+ * 
+ * <pre> -A
+ *  The AIC is used to choose the best iteration.</pre>
+ * 
  <!-- options-end -->
  *
  * @author Niels Landwehr 
  * @author Marc Sumner 
- * @version $Revision: 1.7 $
+ * @version $Revision: 1.8 $
  */
 public class LMT 
   extends Classifier 
@@ -287,30 +305,30 @@ public class LMT
   public Enumeration listOptions() {
     Vector newVector = new Vector(8);
     
-    newVector.addElement(new Option("\tBinary splits (convert nominal attributes to binary ones)\n",
+    newVector.addElement(new Option("\tBinary splits (convert nominal attributes to binary ones)",
                                     "B", 0, "-B"));
     
-    newVector.addElement(new Option("\tSplit on residuals instead of class values\n",
+    newVector.addElement(new Option("\tSplit on residuals instead of class values",
                                     "R", 0, "-R"));
     
-    newVector.addElement(new Option("\tUse cross-validation for boosting at all nodes (i.e., disable heuristic)\n",
+    newVector.addElement(new Option("\tUse cross-validation for boosting at all nodes (i.e., disable heuristic)",
                                     "C", 0, "-C"));
     
     newVector.addElement(new Option("\tUse error on probabilities instead of misclassification error "+
-                                    "for stopping criterion of LogitBoost.\n",
+                                    "for stopping criterion of LogitBoost.",
                                     "P", 0, "-P"));
     
     newVector.addElement(new Option("\tSet fixed number of iterations for LogitBoost (instead of using "+
-                                    "cross-validation)\n",
+                                    "cross-validation)",
                                     "I",1,"-I <numIterations>"));
     
-    newVector.addElement(new Option("\tSet minimum number of instances at which a node can be split (default 15)\n",
+    newVector.addElement(new Option("\tSet minimum number of instances at which a node can be split (default 15)",
                                     "M",1,"-M <numInstances>"));
     
-    newVector.addElement(new Option("\tSet beta for weight trimming for LogitBoost. Set to 0 (default) for no weight trimming.\n",
+    newVector.addElement(new Option("\tSet beta for weight trimming for LogitBoost. Set to 0 (default) for no weight trimming.",
                                     "W",1,"-W <beta>"));
     
-    newVector.addElement(new Option("\tThe AIC is used to choose the best iteration.\n",
+    newVector.addElement(new Option("\tThe AIC is used to choose the best iteration.",
                                     "A", 0, "-A"));
     
     return newVector.elements();
@@ -339,6 +357,12 @@ public class LMT
    * 
    * <pre> -M &lt;numInstances&gt;
    *  Set minimum number of instances at which a node can be split (default 15)</pre>
+   * 
+   * <pre> -W &lt;beta&gt;
+   *  Set beta for weight trimming for LogitBoost. Set to 0 (default) for no weight trimming.</pre>
+   * 
+   * <pre> -A
+   *  The AIC is used to choose the best iteration.</pre>
    * 
    <!-- options-end -->
    *
@@ -645,6 +669,7 @@ public class LMT
    */
   public TechnicalInformation getTechnicalInformation() {
     TechnicalInformation 	result;
+    TechnicalInformation 	additional;
       
     result = new TechnicalInformation(Type.ARTICLE);
     result.setValue(Field.AUTHOR, "Niels Landwehr and Mark Hall and Eibe Frank");
@@ -655,13 +680,13 @@ public class LMT
     result.setValue(Field.PAGES, "161-205");
     result.setValue(Field.NUMBER, "1-2");
     
-    result = new TechnicalInformation(Type.INPROCEEDINGS);
-    result.setValue(Field.AUTHOR, "Marc Sumner and Eibe Frank and Mark Hall");
-    result.setValue(Field.TITLE, "Speeding up Logistic Model Tree Induction");
-    result.setValue(Field.BOOKTITLE, "9th European Conference on Principles and Practice of Knowledge Discovery in Databases");
-    result.setValue(Field.YEAR, "2005");
-    result.setValue(Field.PAGES, "675-683");
-    result.setValue(Field.PUBLISHER, "Springer");
+    additional = result.add(Type.INPROCEEDINGS);
+    additional.setValue(Field.AUTHOR, "Marc Sumner and Eibe Frank and Mark Hall");
+    additional.setValue(Field.TITLE, "Speeding up Logistic Model Tree Induction");
+    additional.setValue(Field.BOOKTITLE, "9th European Conference on Principles and Practice of Knowledge Discovery in Databases");
+    additional.setValue(Field.YEAR, "2005");
+    additional.setValue(Field.PAGES, "675-683");
+    additional.setValue(Field.PUBLISHER, "Springer");
     
     return result;
   }
@@ -771,3 +796,4 @@ public class LMT
     } 
   }  
 }
+
