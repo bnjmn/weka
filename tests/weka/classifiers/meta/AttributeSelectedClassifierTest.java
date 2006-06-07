7f6aa30537c9741209a571bcfd44e610aab65297
fracpete <fracpete@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2006-06-07 03:39:56 +0000

    adapted to make option testing work (using specialized user options)
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@3223 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- tests/weka/classifiers/meta/AttributeSelectedClassifierTest.java
+++ tests/weka/classifiers/meta/AttributeSelectedClassifierTest.java
@@ -1,29 +1,70 @@
 /*
- * Copyright 2002 University of Waikato
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ * Copyright 2002-2006 University of Waikato
  */
 
 package weka.classifiers.meta;
 
-import weka.classifiers.*;
+import weka.classifiers.AbstractClassifierTest;
+import weka.classifiers.Classifier;
+import weka.core.CheckOptionHandler;
+
 import junit.framework.Test;
-import junit.framework.TestCase;
 import junit.framework.TestSuite;
-import weka.core.Instances;
 
 /**
  * Tests AttributeSelectedClassifier. Run from the command line with:<p>
- * java weka.classifiers.AttributeSelectedClassifierTest
+ * java weka.classifiers.meta.AttributeSelectedClassifierTest
  *
  * @author <a href="mailto:eibe@cs.waikato.ac.nz">Eibe Frank</a>
- * @version $Revision: 1.1 $
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ * @version $Revision: 1.2 $
  */
-public class AttributeSelectedClassifierTest extends AbstractClassifierTest {
+public class AttributeSelectedClassifierTest
+  extends AbstractClassifierTest {
 
-  public AttributeSelectedClassifierTest(String name) { super(name);  }
+  public AttributeSelectedClassifierTest(String name) { 
+    super(name);
+  }
 
   /** Creates a default AttributeSelectedClassifier */
   public Classifier getClassifier() {
     return new AttributeSelectedClassifier();
+  }
+  
+  /**
+   * Configures the CheckOptionHandler uses for testing the optionhandling.
+   * Sets the classifier return from the getClassifier() method.
+   * 
+   * @return	the fully configured CheckOptionHandler
+   * @see	#getClassifier()
+   */
+  protected CheckOptionHandler getOptionTester() {
+    CheckOptionHandler		result;
+    
+    result = super.getOptionTester();
+    result.setUserOptions(new String[]{
+	"-E",
+	"weka.attributeSelection.CfsSubsetEval",
+	"-S",
+	"weka.attributeSelection.BestFirst"});
+    
+    return result;
   }
 
   public static Test suite() {
@@ -33,5 +74,4 @@ public class AttributeSelectedClassifierTest extends AbstractClassifierTest {
   public static void main(String[] args){
     junit.textui.TestRunner.run(suite());
   }
-
 }
