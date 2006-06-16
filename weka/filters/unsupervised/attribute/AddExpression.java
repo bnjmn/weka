c874c4e4f85f58bccacfd0b8105f9693a5280b6d
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2006-06-16 02:24:59 +0000

    Moved the expression parsing and processing code out to a separate class (weka.core.AttributeExpression).
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@3259 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/filters/unsupervised/attribute/AddExpression.java
+++ weka/filters/unsupervised/attribute/AddExpression.java
@@ -22,6 +22,7 @@
 
 package weka.filters.unsupervised.attribute;
 
+import weka.core.AttributeExpression;
 import weka.core.Attribute;
 import weka.core.Instance;
 import weka.core.Instances;
@@ -62,7 +63,7 @@ import java.util.Vector;
  <!-- options-end -->
  *
  * @author Mark Hall (mhall@cs.waikato.ac.nz)
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  */
 public class AddExpression 
   extends Filter 
@@ -71,189 +72,8 @@ public class AddExpression
   /** for serialization */
   static final long serialVersionUID = 402130384261736245L;
   
-  /**
-   * Inner class handling an attribute index as an operand
-   */
-  private class AttributeOperand 
-    implements Serializable {
-    
-    /** for serialization */
-    static final long serialVersionUID = -7674280127286031105L;
-
-    /** the index of the attribute */
-    protected int m_attributeIndex;
-
-    /** true if the value of the attribute are to be multiplied by -1 */
-    protected boolean m_negative;
-
-    /**
-     * Constructor
-     * 
-     * @param operand
-     * @param sign
-     * @throws Exception
-     */
-    public AttributeOperand(String operand, boolean sign) throws Exception {
-      // strip the leading 'a' and set the index
-      m_attributeIndex = (Integer.parseInt(operand.substring(1)))-1;
-      m_negative = sign;
-    }
-
-    /**
-     * Return a string describing this object
-     * @return a string descibing the attribute operand
-     */
-    public String toString() {
-      String result = "";
-      if (m_negative) {
-	result += '-';
-      }
-      return result+"a"+(m_attributeIndex+1);
-    }
-  }
-
-  /**
-   * Inner class for storing numeric constant opperands
-   */
-  private class NumericOperand 
-    implements Serializable {
-    
-    /** for serialization */
-    static final long serialVersionUID = 9037007836243662859L;
-
-    /** numeric constant */
-    protected double m_numericConst;
-
-    /**
-     * Constructor
-     * 
-     * @param operand
-     * @param sign
-     * @throws Exception
-     */
-    public NumericOperand(String operand, boolean sign) throws Exception {
-      m_numericConst = Double.valueOf(operand).doubleValue();
-      if (sign) {
-	m_numericConst *= -1.0;
-      }
-    }
-    
-    /**
-     * Return a string describing this object
-     * @return a string descibing the numeric operand
-     */
-    public String toString() {
-      return ""+m_numericConst;
-    }
-  }
-
-  /**
-   * Inner class for storing operators
-   */
-  private class Operator 
-    implements Serializable {
-    
-    /** for serialization */
-    static final long serialVersionUID = -2760353522666004638L;
-
-    /** the operator */
-    protected char m_operator;
-
-    /**
-     * Constructor
-     * 
-     * @param opp the operator
-     */
-    public Operator(char opp) {
-      if (!isOperator(opp)) {
-	throw new IllegalArgumentException("Unrecognized operator:" + opp);
-      }
-      m_operator = opp;
-    }
-
-    /**
-     * Apply this operator to the supplied arguments
-     * @param first the first argument
-     * @param second the second argument
-     * @return the result
-     */
-    protected double applyOperator(double first, double second) {
-      switch (m_operator) {
-	case '+' :
-	  return (first+second);
-	case '-' :
-	  return (first-second);
-	case '*' :
-	  return (first*second);
-	case '/' :
-	  return (first/second);
-	case '^' :
-	  return Math.pow(first,second);
-      }
-      return Double.NaN;
-    }
-
-    /**
-     * Apply this operator (function) to the supplied argument
-     * @param value the argument
-     * @return the result
-     */
-    protected double applyFunction(double value) {
-      switch (m_operator) {
-	case 'l' :
-	  return Math.log(value);
-	case 'b' :
-	  return Math.abs(value);
-	case 'c' :
-	  return Math.cos(value);
-	case 'e' :
-	  return Math.exp(value);
-	case 's' :
-	  return Math.sqrt(value);
-	case 'f' :
-	  return Math.floor(value);
-	case 'h' :
-	  return Math.ceil(value);
-	case 'r' :
-	  return Math.rint(value);
-	case 't' :
-	  return Math.tan(value);
-	case 'n' :
-	  return Math.sin(value);
-      }
-      return Double.NaN;
-    }
-
-    /**
-     * Return a string describing this object
-     * @return a string descibing the operator
-     */
-    public String toString() {
-      return ""+m_operator;
-    }
-  }
-
   /** The infix expression */
   private String m_infixExpression = "a1^2";
-
-  /** Operator stack */
-  private Stack m_operatorStack = new Stack();
-
-  /** Supported operators. l = log, b = abs, c = cos, e = exp, s = sqrt, 
-      f = floor, h = ceil, r = rint, t = tan, n = sin */
-  private static final String OPERATORS = "+-*/()^lbcesfhrtn";
-  /** Unary functions. l = log, b = abs, c = cos, e = exp, s = sqrt, 
-      f = floor, h = ceil, r = rint, t = tan, n = sin */
-  private static final String UNARY_FUNCTIONS = "lbcesfhrtn";
-
-  /** Holds the expression in postfix form */
-  private Vector m_postFixExpVector;
-
-  /** True if the next numeric constant or attribute index is negative */
-  private boolean m_signMod = false;
-
-  /** Holds the previous token */
-  private String m_previousTok = "";
 
   /** Name of the new attribute. "expression"  length string will use the 
       provided expression as the new attribute name */
@@ -263,295 +83,7 @@ public class AddExpression
       expression */
   private boolean m_Debug = false;
 
-  /**
-   * Returns a string describing this filter
-   *
-   * @return a description of the filter suitable for
-   * displaying in the explorer/experimenter gui
-   */
-  public String globalInfo() {
-    return "An instance filter that creates a new attribute by applying a "
-      +"mathematical expression to existing attributes. The expression "
-      +"can contain attribute references and numeric constants. Supported "
-      +"opperators are :  +, -, *, /, ^, log, abs, cos, exp, sqrt, "
-      +"floor, ceil, rint, tan, sin, (, ). Attributes are specified "
-      +"by prefixing with 'a', eg. a7 is attribute number 7 (starting from 1)."
-      +" Example expression : a1^2*a5/log(a7*4.0)."; 
-  }
-
-  /**
-   * Handles the processing of an infix operand to postfix
-   * @param tok the infix operand
-   * @throws Exception if there is difficulty parsing the operand
-   */
-  private void handleOperand(String tok) throws Exception {
-    // if it contains an 'a' then its an attribute index
-    if (tok.indexOf('a') != -1) {
-      m_postFixExpVector.addElement(new AttributeOperand(tok,m_signMod));
-    } else {
-      try {
-	// should be a numeric constant
-	m_postFixExpVector.addElement(new NumericOperand(tok, m_signMod));
-      } catch (NumberFormatException ne) {
-	throw new Exception("Trouble parsing numeric constant");
-      }
-    }
-    m_signMod = false;
-  }
-
-  /**
-   * Handles the processing of an infix operator to postfix
-   * @param tok the infix operator
-   * @throws Exception if there is difficulty parsing the operator
-   */
-  private void handleOperator(String tok) throws Exception {
-    boolean push = true;
-
-    char tokchar = tok.charAt(0);
-    if (tokchar == ')') {
-      String popop = " ";
-      do {
-	popop = (String)(m_operatorStack.pop());
-	if (popop.charAt(0) != '(') {
-	  m_postFixExpVector.addElement(new Operator(popop.charAt(0)));
-	}
-      } while (popop.charAt(0) != '(');
-    } else {
-      int infixToc = infixPriority(tok.charAt(0));
-      while (!m_operatorStack.empty() && 
-	     stackPriority(((String)(m_operatorStack.peek())).charAt(0)) 
-	     >= infixToc) {
-	
-	// try an catch double operators and see if the current one can
-	// be interpreted as the sign of an upcoming number
-	if (m_previousTok.length() == 1 && 
-	    isOperator(m_previousTok.charAt(0)) &&
-	    m_previousTok.charAt(0) != ')') {
-	  if (tok.charAt(0) == '-') {
-	    m_signMod = true;
-	  } else {
-	    m_signMod = false;
-	  }
-	  push = false;
-	  break;
-	} else {
-	  String popop = (String)(m_operatorStack.pop());
-	  m_postFixExpVector.addElement(new Operator(popop.charAt(0)));
-	}
-      }
-      if (m_postFixExpVector.size() == 0) {
-	if (tok.charAt(0) == '-') {
-	  m_signMod = true;
-	  push = false;
-	}
-      }
-
-      if (push) {
-	m_operatorStack.push(tok);
-      }
-    }
-  }
-
-  /**
-   * Converts a string containing a mathematical expression in infix form
-   * to postfix form. The result is stored in the vector m_postfixExpVector
-   * @param infixExp the infix expression to convert
-   * @throws Exception if something goes wrong during the conversion
-   */
-  private void convertInfixToPostfix(String infixExp) throws Exception {
-    infixExp = Utils.removeSubstring(infixExp, " ");
-    infixExp = Utils.replaceSubstring(infixExp,"log","l");
-    infixExp = Utils.replaceSubstring(infixExp,"abs","b");
-    infixExp = Utils.replaceSubstring(infixExp,"cos","c");
-    infixExp = Utils.replaceSubstring(infixExp,"exp","e");
-    infixExp = Utils.replaceSubstring(infixExp,"sqrt","s");
-    infixExp = Utils.replaceSubstring(infixExp,"floor","f");
-    infixExp = Utils.replaceSubstring(infixExp,"ceil","h");
-    infixExp = Utils.replaceSubstring(infixExp,"rint","r");
-    infixExp = Utils.replaceSubstring(infixExp,"tan","t");
-    infixExp = Utils.replaceSubstring(infixExp,"sin","n");
-
-    StringTokenizer tokenizer = new StringTokenizer(infixExp, OPERATORS, true);
-    m_postFixExpVector = new Vector();
-
-    while (tokenizer.hasMoreTokens()) {
-      String tok = tokenizer.nextToken();
-      
-      if (tok.length() > 1) {
-	handleOperand(tok);
-      } else {
-	// probably an operator, but could be a single char operand
-	if (isOperator(tok.charAt(0))) {
-	  handleOperator(tok);
-	} else {
-	  // should be a numeric constant
-	  handleOperand(tok);
-	}
-      }
-      m_previousTok = tok;
-    }
-    while (!m_operatorStack.empty()) {
-      String popop = (String)(m_operatorStack.pop());
-      if (popop.charAt(0) == '(' || popop.charAt(0) == ')') {
-	throw new Exception("Mis-matched parenthesis!");
-      }
-      m_postFixExpVector.addElement(new Operator(popop.charAt(0)));
-    }
-  }
-
-  /**
-   * Evaluate the expression using the supplied array of attribute values.
-   * The result is stored in the last element of the array. Assumes that
-   * the infix expression has been converted to postfix and stored in
-   * m_postFixExpVector
-   * @param vals the values to apply the expression to
-   * @throws Exception if something goes wrong
-   */
-  private void evaluateExpression(double [] vals) throws Exception {
-    Stack operands = new Stack();
-
-    for (int i=0;i<m_postFixExpVector.size();i++) {
-      Object nextob = m_postFixExpVector.elementAt(i);
-      if (nextob instanceof NumericOperand) {
-	operands.push(new Double(((NumericOperand)nextob).m_numericConst));
-      } else if (nextob instanceof AttributeOperand) {
-	double value = vals[((AttributeOperand)nextob).m_attributeIndex];
-	if (value == Instance.missingValue()) {
-	  vals[vals.length-1] = Instance.missingValue();
-	  break;
-	}
-	if (((AttributeOperand)nextob).m_negative) {
-	  value = -value;
-	}
-	operands.push(new Double(value));
-      } else if (nextob instanceof Operator) {
-	char op = ((Operator)nextob).m_operator;
-	if (isUnaryFunction(op)) {
-	  double operand = ((Double)operands.pop()).doubleValue();
-	  double result = ((Operator)nextob).applyFunction(operand);
-	  operands.push(new Double(result));
-	} else {
-	  double second = ((Double)operands.pop()).doubleValue();
-	  double first = ((Double)operands.pop()).doubleValue();
-	  double result = ((Operator)nextob).applyOperator(first,second);
-	  operands.push(new Double(result));
-	}
-      } else {
-	throw new Exception("Unknown object in postfix vector!");
-      }
-    }
-
-    if (operands.size() != 1) {
-      throw new Exception("Problem applying function");
-    }
-
-    Double result = ((Double)operands.pop());
-    if (result.isNaN() || result.isInfinite()) {
-      vals[vals.length-1] = Instance.missingValue();
-    } else {
-      vals[vals.length-1] = result.doubleValue();
-    }
-  }
-
-  /**
-   * Returns true if a token is an operator
-   * @param tok the token to check
-   * @return true if the supplied token is an operator
-   */
-  private boolean isOperator(char tok) {
-    if (OPERATORS.indexOf(tok) == -1) {
-      return false;
-    }
-
-    return true;
-  }
-
-  /**
-   * Returns true if a token is a unary function
-   * @param tok the token to check
-   * @return true if the supplied token is a unary function
-   */
-  private boolean isUnaryFunction(char tok) {
-    if (UNARY_FUNCTIONS.indexOf(tok) == -1) {
-      return false;
-    }
-
-    return true;
-  }
-
-  /**
-   * Return the infix priority of an operator
-   * @param opp the operator
-   * @return the infix priority
-   */
-  private int infixPriority(char opp) {
-    switch (opp) {
-      case 'l' : 
-      case 'b' :
-      case 'c' :
-      case 'e' :
-      case 's' :
-      case 'f' :
-      case 'h' :
-      case 'r' :
-      case 't' :
-      case 'n' :
-	return 3;
-      case '^' :
-	return 2;
-      case '*' : 
-	return 2;
-      case '/' : 
-	return 2;
-      case '+' :
-	return 1;
-      case '-' :
-	return 1;
-      case '(' :
-	return 4;
-      case ')' :
-	return 0;
-      default :
-	throw new IllegalArgumentException("Unrecognized operator:" + opp);
-    }
-  }
-
-  /**
-   * Return the stack priority of an operator
-   * @param opp the operator
-   * @return the stack priority
-   */
-  private int stackPriority(char opp) {
-     switch (opp) {
-       case 'l' :
-       case 'b' :
-       case 'c' :
-       case 'e' :
-       case 's' :
-       case 'f' :
-       case 'h' :
-       case 'r' :
-       case 't' :
-       case 'n' :
-	 return 3;
-       case '^' :
-	 return 2;
-       case '*' : 
-	 return 2;
-       case '/' : 
-	 return 2;
-       case '+' :
-	 return 1;
-       case '-' :
-	 return 1;
-       case '(' :
-	 return 0;
-       case ')' :
-	 return -1;
-       default :
-	 throw new IllegalArgumentException("Unrecognized operator:" + opp);
-    }
-  }
+  private AttributeExpression m_attributeExpression = null;
 
   /**
    * Returns an enumeration describing the available options.
@@ -734,14 +266,17 @@ public class AddExpression
    */
   public boolean setInputFormat(Instances instanceInfo) throws Exception {
 
-    convertInfixToPostfix(new String(m_infixExpression));
+    m_attributeExpression = new AttributeExpression();
+    m_attributeExpression.
+      convertInfixToPostfix(new String(m_infixExpression));
 
     super.setInputFormat(instanceInfo);
 
     Instances outputFormat = new Instances(instanceInfo, 0);
     Attribute newAttribute;
     if (m_Debug) {
-      newAttribute = new Attribute(m_postFixExpVector.toString());
+      newAttribute = 
+        new Attribute(m_attributeExpression.getPostFixExpression());
     } else if (m_attributeName.compareTo("expression") != 0) {
       newAttribute = new Attribute(m_attributeName);
     } else {
@@ -783,7 +318,7 @@ public class AddExpression
       }
     }
 
-    evaluateExpression(vals);
+    m_attributeExpression.evaluateExpression(vals);
 
     Instance inst = null;
     if (instance instanceof SparseInstance) {
