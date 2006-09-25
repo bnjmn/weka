362ab6fd343838e4731cafb89950ad9753f3b938
fracpete <fracpete@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2006-09-25 01:22:20 +0000

    added GPL preamble and copyright
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@3567 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/core/MathematicalExpression.java
+++ weka/core/MathematicalExpression.java
@@ -1,3 +1,25 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ *    MathematicalExpression.java
+ *    Copyright (C) 2004 Prados Julien
+ *    Copyright (C) 2002 Eibe Frank
+ */
+
 package weka.core;
 
 import java.io.IOException;
@@ -58,7 +80,7 @@ import java.util.Vector;
  *
  * @author Eibe Frank (eibe@cs.waikato.ac.nz) 
  * @author Prados Julien (julien.prados@cui.unige.ch) 
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class MathematicalExpression {
   
@@ -67,7 +89,7 @@ public class MathematicalExpression {
    *
    * @author Eibe Frank (eibe@cs.waikato.ac.nz) 
    * @author Prados Julien (julien.prados@cui.unige.ch) 
-   * @version $Revision: 1.1 $
+   * @version $Revision: 1.2 $
    */
   static public class Tokenizer 
     extends StreamTokenizer {
@@ -122,7 +144,7 @@ public class MathematicalExpression {
    *
    * @author Eibe Frank (eibe@cs.waikato.ac.nz) 
    * @author Prados Julien (julien.prados@cui.unige.ch) 
-   * @version $Revision: 1.1 $
+   * @version $Revision: 1.2 $
    */
   static public class TreeNode 
     implements Serializable {
