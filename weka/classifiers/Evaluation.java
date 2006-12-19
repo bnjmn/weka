9ff968044e9c6ad28a4409a770b1714af65d2168
fracpete <fracpete@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2006-12-19 20:17:49 +0000

    printClassifications didn't reset the dataset before printing the classifications (resulted in an exception)
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@3701 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/classifiers/Evaluation.java
+++ weka/classifiers/Evaluation.java
@@ -159,7 +159,7 @@ import java.util.zip.GZIPOutputStream;
  *
  * @author   Eibe Frank (eibe@cs.waikato.ac.nz)
  * @author   Len Trigg (trigg@cs.waikato.ac.nz)
- * @version  $Revision: 1.70 $
+ * @version  $Revision: 1.71 $
  */
 public class Evaluation
 implements Summarizable {
@@ -2635,6 +2635,7 @@ implements Summarizable {
 
       // print predictions
       int i = 0;
+      testSource.reset();
       while (testSource.hasMoreElements()) {
 	Instance inst = testSource.nextElement(test);
 	text.append(
