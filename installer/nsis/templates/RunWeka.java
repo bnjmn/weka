773dd0d5037e1459160d65b4db74c06ddeaa7842
fracpete <fracpete@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2007-01-22 21:44:45 +0000

    added GPL preamble and copyrights
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@3738 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- installer/nsis/templates/RunWeka.java
+++ installer/nsis/templates/RunWeka.java
@@ -1,3 +1,25 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ * RunWeka.java
+ * Copyright (C) 2006 University of Waikato, Hamilton, New Zealand
+ *
+ */
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.Collections;
@@ -34,7 +56,7 @@ import java.util.Vector;
  * Additional parameters will be appended to the generated java call.
  *
  * @author  FracPete (fracpete at waikato dot ac dot nz)
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public class RunWeka {
 
