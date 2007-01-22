6442c9baba8a00d58c81551c7dcf02b390c843bd
fracpete <fracpete@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2007-01-22 21:05:06 +0000

    added "-classpath ." in the java call (ran into problems on some machines?)
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@3736 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- installer/nsis/templates/RunWeka.bat
+++ installer/nsis/templates/RunWeka.bat
@@ -5,11 +5,11 @@ REM   RunWeka.bat <command>
 REM Run with option "-h" to see available commands
 REM
 REM Author:  FracPete (fracpete at waikato dot ac dot nz)
-REM Version: $Revision: 1.4 $
+REM Version: $Revision: 1.5 $
 
 set _cmd=%1
 set _java=javaw
 if "%_cmd%"=="" set _cmd=default
 if "%_cmd%"=="-h" set _java=java
-%_java% RunWeka -i .\RunWeka.ini -w .\weka.jar -c %_cmd% "%2"
+%_java% -classpath . RunWeka -i .\RunWeka.ini -w .\weka.jar -c %_cmd% "%2"
 
