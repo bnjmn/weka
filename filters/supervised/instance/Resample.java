b0b573cee2b087d24e91ae58202af42e9be87afd
fracpete <fracpete@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2007-04-17 03:20:00 +0000

    added options -no-replacement and -V (only in combination with -no-replacement)
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk/weka@3873 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- filters/supervised/instance/Resample.java
+++ filters/supervised/instance/Resample.java
@@ -20,7 +20,6 @@
  *
  */
 
-
 package weka.filters.supervised.instance;
 
 import weka.core.Capabilities;
@@ -33,13 +32,15 @@ import weka.core.Capabilities.Capability;
 import weka.filters.Filter;
 import weka.filters.SupervisedFilter;
 
+import java.util.Collections;
 import java.util.Enumeration;
 import java.util.Random;
 import java.util.Vector;
 
 /** 
  <!-- globalinfo-start -->
- * Produces a random subsample of a dataset using sampling with replacement.The original dataset must fit entirely in memory. The number of instances in the generated dataset may be specified. The dataset must have a nominal class attribute. If not, use the unsupervised version. The filter can be made to maintain the class distribution in the subsample, or to bias the class distribution toward a uniform distribution. When used in batch mode (i.e. in the FilteredClassifier), subsequent batches are NOTE resampled.
+ * Produces a random subsample of a dataset using either sampling with replacement or without replacement.<br/>
+ * The original dataset must fit entirely in memory. The number of instances in the generated dataset may be specified. The dataset must have a nominal class attribute. If not, use the unsupervised version. The filter can be made to maintain the class distribution in the subsample, or to bias the class distribution toward a uniform distribution. When used in batch mode (i.e. in the FilteredClassifier), subsequent batches are NOTE resampled.
  * <p/>
  <!-- globalinfo-end -->
  * 
@@ -58,10 +59,18 @@ import java.util.Vector;
  *  0 = distribution in input data -- 1 = uniform distribution.
  *  (default 0)</pre>
  * 
+ * <pre> -no-replacement
+ *  Disables replacement of instances
+ *  (default: with replacement)</pre>
+ * 
+ * <pre> -V
+ *  Inverts the selection - only available with '-no-replacement'.</pre>
+ * 
  <!-- options-end -->
  *
  * @author Len Trigg (len@reeltwo.com)
- * @version $Revision: 1.7 $ 
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ * @version $Revision: 1.8 $ 
  */
 public class Resample
   extends Filter 
@@ -71,13 +80,21 @@ public class Resample
   static final long serialVersionUID = 7079064953548300681L;
 
   /** The subsample size, percent of original set, default 100% */
-  private double m_SampleSizePercent = 100;
+  protected double m_SampleSizePercent = 100;
   
   /** The random number generator seed */
-  private int m_RandomSeed = 1;
+  protected int m_RandomSeed = 1;
   
   /** The degree of bias towards uniform (nominal) class distribution */
-  private double m_BiasToUniformClass = 0;
+  protected double m_BiasToUniformClass = 0;
+
+  /** Whether to perform sampling with replacement or without */
+  protected boolean m_NoReplacement = false;
+
+  /** Whether to invert the selection (only if instances are drawn WITHOUT 
+   * replacement)
+   * @see #m_NoReplacement */
+  protected boolean m_InvertSelection = false;
 
   /**
    * Returns a string describing this filter
@@ -86,8 +103,9 @@ public class Resample
    * displaying in the explorer/experimenter gui
    */
   public String globalInfo() {
-
-    return "Produces a random subsample of a dataset using sampling with replacement."
+    return 
+        "Produces a random subsample of a dataset using either sampling "
+      + "with replacement or without replacement.\n"
       + "The original dataset must "
       + "fit entirely in memory. The number of instances in the generated "
       + "dataset may be specified. The dataset must have a nominal class "
@@ -103,23 +121,33 @@ public class Resample
    * @return an enumeration of all the available options.
    */
   public Enumeration listOptions() {
+    Vector result = new Vector();
 
-    Vector newVector = new Vector(1);
+    result.addElement(new Option(
+	"\tSpecify the random number seed (default 1)",
+	"S", 1, "-S <num>"));
 
-    newVector.addElement(new Option(
-              "\tSpecify the random number seed (default 1)",
-              "S", 1, "-S <num>"));
-    newVector.addElement(new Option(
-              "\tThe size of the output dataset, as a percentage of\n"
-              +"\tthe input dataset (default 100)",
-              "Z", 1, "-Z <num>"));
-    newVector.addElement(new Option(
-              "\tBias factor towards uniform class distribution.\n"
-              +"\t0 = distribution in input data -- 1 = uniform distribution.\n"
-              +"\t(default 0)",
-              "B", 1, "-B <num>"));
+    result.addElement(new Option(
+	"\tThe size of the output dataset, as a percentage of\n"
+	+"\tthe input dataset (default 100)",
+	"Z", 1, "-Z <num>"));
 
-    return newVector.elements();
+    result.addElement(new Option(
+	"\tBias factor towards uniform class distribution.\n"
+	+"\t0 = distribution in input data -- 1 = uniform distribution.\n"
+	+"\t(default 0)",
+	"B", 1, "-B <num>"));
+
+    result.addElement(new Option(
+	"\tDisables replacement of instances\n"
+	+"\t(default: with replacement)",
+	"no-replacement", 0, "-no-replacement"));
+
+    result.addElement(new Option(
+	"\tInverts the selection - only available with '-no-replacement'.",
+	"V", 0, "-V"));
+
+    return result.elements();
   }
 
 
@@ -141,33 +169,43 @@ public class Resample
    *  0 = distribution in input data -- 1 = uniform distribution.
    *  (default 0)</pre>
    * 
+   * <pre> -no-replacement
+   *  Disables replacement of instances
+   *  (default: with replacement)</pre>
+   * 
+   * <pre> -V
+   *  Inverts the selection - only available with '-no-replacement'.</pre>
+   * 
    <!-- options-end -->
    *
    * @param options the list of options as an array of strings
    * @throws Exception if an option is not supported
    */
   public void setOptions(String[] options) throws Exception {
+    String	tmpStr;
     
-    String seedString = Utils.getOption('S', options);
-    if (seedString.length() != 0) {
-      setRandomSeed(Integer.parseInt(seedString));
-    } else {
+    tmpStr = Utils.getOption('S', options);
+    if (tmpStr.length() != 0)
+      setRandomSeed(Integer.parseInt(tmpStr));
+    else
       setRandomSeed(1);
-    }
 
-    String biasString = Utils.getOption('B', options);
-    if (biasString.length() != 0) {
-      setBiasToUniformClass(Double.valueOf(biasString).doubleValue());
-    } else {
+    tmpStr = Utils.getOption('B', options);
+    if (tmpStr.length() != 0)
+      setBiasToUniformClass(Double.parseDouble(tmpStr));
+    else
       setBiasToUniformClass(0);
-    }
 
-    String sizeString = Utils.getOption('Z', options);
-    if (sizeString.length() != 0) {
-      setSampleSizePercent(Double.valueOf(sizeString).doubleValue());
-    } else {
+    tmpStr = Utils.getOption('Z', options);
+    if (tmpStr.length() != 0)
+      setSampleSizePercent(Double.parseDouble(tmpStr));
+    else
       setSampleSizePercent(100);
-    }
+
+    setNoReplacement(Utils.getFlag("no-replacement", options));
+
+    if (getNoReplacement())
+      setInvertSelection(Utils.getFlag('V', options));
 
     if (getInputFormat() != null) {
       setInputFormat(getInputFormat());
@@ -180,21 +218,26 @@ public class Resample
    * @return an array of strings suitable for passing to setOptions
    */
   public String [] getOptions() {
+    Vector<String>	result;
 
-    String [] options = new String [6];
-    int current = 0;
+    result = new Vector<String>();
 
-    options[current++] = "-B"; 
-    options[current++] = "" + getBiasToUniformClass();
+    result.add("-B");
+    result.add("" + getBiasToUniformClass());
 
-    options[current++] = "-S"; options[current++] = "" + getRandomSeed();
+    result.add("-S");
+    result.add("" + getRandomSeed());
 
-    options[current++] = "-Z"; options[current++] = "" + getSampleSizePercent();
+    result.add("-Z");
+    result.add("" + getSampleSizePercent());
 
-    while (current < options.length) {
-      options[current++] = "";
+    if (getNoReplacement()) {
+      result.add("-no-replacement");
+      if (getInvertSelection())
+	result.add("-V");
     }
-    return options;
+    
+    return result.toArray(new String[result.size()]);
   }
     
   /**
@@ -217,7 +260,6 @@ public class Resample
    * @return the current bias
    */
   public double getBiasToUniformClass() {
-
     return m_BiasToUniformClass;
   }
   
@@ -229,7 +271,6 @@ public class Resample
    * @param newBiasToUniformClass the new bias value, between 0 and 1.
    */
   public void setBiasToUniformClass(double newBiasToUniformClass) {
-
     m_BiasToUniformClass = newBiasToUniformClass;
   }
     
@@ -249,7 +290,6 @@ public class Resample
    * @return the random number seed.
    */
   public int getRandomSeed() {
-
     return m_RandomSeed;
   }
   
@@ -259,7 +299,6 @@ public class Resample
    * @param newSeed the new random number seed.
    */
   public void setRandomSeed(int newSeed) {
-
     m_RandomSeed = newSeed;
   }
     
@@ -279,7 +318,6 @@ public class Resample
    * @return the subsample size
    */
   public double getSampleSizePercent() {
-
     return m_SampleSizePercent;
   }
   
@@ -289,8 +327,66 @@ public class Resample
    * @param newSampleSizePercent the subsample set size, between 0 and 100.
    */
   public void setSampleSizePercent(double newSampleSizePercent) {
-
     m_SampleSizePercent = newSampleSizePercent;
+  }
+  
+  /**
+   * Returns the tip text for this property
+   * 
+   * @return tip text for this property suitable for
+   * displaying in the explorer/experimenter gui
+   */
+  public String noReplacementTipText() {
+    return "Disables the replacement of instances.";
+  }
+
+  /**
+   * Gets whether instances are drawn with or without replacement.
+   * 
+   * @return true if the replacement is disabled
+   */
+  public boolean getNoReplacement() {
+    return m_NoReplacement;
+  }
+  
+  /**
+   * Sets whether instances are drawn with or with out replacement.
+   * 
+   * @param value if true then the replacement of instances is disabled
+   */
+  public void setNoReplacement(boolean value) {
+    m_NoReplacement = value;
+  }
+  
+  /**
+   * Returns the tip text for this property
+   * 
+   * @return tip text for this property suitable for
+   * displaying in the explorer/experimenter gui
+   */
+  public String invertSelectionTipText() {
+    return "Inverts the selection (only if instances are drawn WITHOUT replacement).";
+  }
+
+  /**
+   * Gets whether selection is inverted (only if instances are drawn WIHTOUT 
+   * replacement).
+   * 
+   * @return true if the replacement is disabled
+   * @see #m_NoReplacement
+   */
+  public boolean getInvertSelection() {
+    return m_InvertSelection;
+  }
+  
+  /**
+   * Sets whether the selection is inverted (only if instances are drawn WIHTOUT 
+   * replacement).
+   * 
+   * @param value if true then selection is inverted
+   */
+  public void setInvertSelection(boolean value) {
+    m_InvertSelection = value;
   }
 
   /** 
@@ -382,13 +478,133 @@ public class Resample
     return (numPendingOutput() != 0);
   }
 
+  /**
+   * creates the subsample with replacement
+   * 
+   * @param random		the random number generator to use
+   * @param origSize		the original size of the dataset
+   * @param sampleSize		the size to generate
+   * @param actualClasses	the number of classes found in the data
+   * @param classIndices	the indices where classes start
+   */
+  public void createSubsampleWithReplacement(Random random, int origSize, 
+      int sampleSize, int actualClasses, int[] classIndices) {
+    
+    for (int i = 0; i < sampleSize; i++) {
+      int index = 0;
+      if (random.nextDouble() < m_BiasToUniformClass) {
+	// Pick a random class (of those classes that actually appear)
+	int cIndex = random.nextInt(actualClasses);
+	for (int j = 0, k = 0; j < classIndices.length - 1; j++) {
+	  if ((classIndices[j] != classIndices[j + 1]) 
+	      && (k++ >= cIndex)) {
+	    // Pick a random instance of the designated class
+	    index =   classIndices[j] 
+	                           + random.nextInt(classIndices[j + 1] - classIndices[j]);
+	    break;
+	  }
+	}
+      }
+      else {
+	index = random.nextInt(origSize);
+      }
+      push((Instance) getInputFormat().instance(index).copy());
+    }
+  }
+
+  /**
+   * creates the subsample without replacement
+   * 
+   * @param random		the random number generator to use
+   * @param origSize		the original size of the dataset
+   * @param sampleSize		the size to generate
+   * @param actualClasses	the number of classes found in the data
+   * @param classIndices	the indices where classes start
+   */
+  public void createSubsampleWithoutReplacement(Random random, int origSize, 
+      int sampleSize, int actualClasses, int[] classIndices) {
+    
+    if (sampleSize > origSize) {
+      sampleSize = origSize;
+      System.err.println(
+	  "Resampling with replacement can only use percentage <=100% - "
+	  + "Using full dataset!");
+    }
+
+    Vector<Integer>[] indices = new Vector[actualClasses];
+    Vector<Integer>[] indicesNew = new Vector[actualClasses];
+
+    // generate list of all indices to draw from
+    for (int i = 0; i < actualClasses; i++) {
+      indices[i] = new Vector<Integer>(classIndices[i + 1] - classIndices[i]);
+      indicesNew[i] = new Vector<Integer>(indices[i].capacity());
+      for (int n = classIndices[i]; n < classIndices[i + 1]; n++)
+	indices[i].add(n);
+    }
+
+    // draw X samples
+    int currentSize = origSize;
+    for (int i = 0; i < sampleSize; i++) {
+      int index = 0;
+      if (random.nextDouble() < m_BiasToUniformClass) {
+	// Pick a random class (of those classes that actually appear)
+	int cIndex = random.nextInt(actualClasses);
+	for (int j = 0, k = 0; j < classIndices.length - 1; j++) {
+	  if ((classIndices[j] != classIndices[j + 1]) 
+	      && (k++ >= cIndex)) {
+	    // Pick a random instance of the designated class
+	    index = random.nextInt(indices[j].size());
+	    indicesNew[j].add(indices[j].get(index));
+	    indices[j].remove(index);
+	    break;
+	  }
+	}
+      }
+      else {
+	index = random.nextInt(currentSize);
+	for (int n = 0; n < actualClasses; n++) {
+	  if (index < indices[n].size()) {
+	    indicesNew[n].add(indices[n].get(index));
+	    indices[n].remove(index);
+	    break;
+	  }
+	  else {
+	    index -= indices[n].size();
+	  }
+	}
+	currentSize--;
+      }
+    }
+
+    // sort indices
+    if (getInvertSelection()) {
+      indicesNew = indices;
+    }
+    else {
+      for (int i = 0; i < indicesNew.length; i++)
+	Collections.sort(indicesNew[i]);
+    }
+
+    // add to ouput
+    for (int i = 0; i < indicesNew.length; i++) {
+      for (int n = 0; n < indicesNew[i].size(); n++)
+	push((Instance) getInputFormat().instance(indicesNew[i].get(n)).copy());
+    }
+
+    // clean up
+    for (int i = 0; i < indices.length; i++) {
+      indices[i].clear();
+      indicesNew[i].clear();
+    }
+    indices = null;
+    indicesNew = null;
+  }
 
   /**
    * Creates a subsample of the current set of input instances. The output
    * instances are pushed onto the output queue for collection.
    */
-  private void createSubsample() {
-
+  protected void createSubsample() {
     int origSize = getInputFormat().numInstances();
     int sampleSize = (int) (origSize * m_SampleSizePercent / 100);
 
@@ -427,31 +643,18 @@ public class Resample
 	actualClasses++;
       }
     }
+
     // Create the new sample
-    
     Random random = new Random(m_RandomSeed);
+
     // Convert pending input instances
-    for(int i = 0; i < sampleSize; i++) {
-      int index = 0;
-      if (random.nextDouble() < m_BiasToUniformClass) {
-	// Pick a random class (of those classes that actually appear)
-	int cIndex = random.nextInt(actualClasses);
-	for (int j = 0, k = 0; j < classIndices.length - 1; j++) {
-	  if ((classIndices[j] != classIndices[j + 1]) 
-	      && (k++ >= cIndex)) {
-	    // Pick a random instance of the designated class
-	    index = classIndices[j] 
-	      + random.nextInt(classIndices[j + 1] - classIndices[j]);
-	    break;
-	  }
-	}
-      } else {
-	index = random.nextInt(origSize);
-      }
-      push((Instance)getInputFormat().instance(index).copy());
-    }
+    if (getNoReplacement())
+      createSubsampleWithoutReplacement(
+	  random, origSize, sampleSize, actualClasses, classIndices);
+    else
+      createSubsampleWithReplacement(
+	  random, origSize, sampleSize, actualClasses, classIndices);
   }
-  
   
   /**
    * Main method for testing this class.
