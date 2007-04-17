54596b17758dde68e050812299b795a9065e573f
fracpete <fracpete@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2007-04-17 03:21:03 +0000

    added tests for -no-replacement and -V
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@3874 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- tests/weka/filters/supervised/instance/ResampleTest.java
+++ tests/weka/filters/supervised/instance/ResampleTest.java
@@ -1,32 +1,47 @@
 /*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
  * Copyright (C) 2002 University of Waikato 
  */
 
 package weka.filters.supervised.instance;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-import weka.core.Instance;
-import weka.core.Instances;
 import weka.core.AttributeStats;
-import weka.filters.Filter;
+import weka.core.Instances;
 import weka.filters.AbstractFilterTest;
+import weka.filters.Filter;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
 
 /**
  * Tests Resample. Run from the command line with:<p>
- * java weka.filters.ResampleTest
+ * java weka.filters.supervised.instance.ResampleTest
  *
  * @author <a href="mailto:len@reeltwo.com">Len Trigg</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
-public class ResampleTest extends AbstractFilterTest {
+public class ResampleTest
+  extends AbstractFilterTest {
   
   public ResampleTest(String name) { super(name);  }
 
   /** Need to set the class index */
   protected void setUp() throws Exception {
-
     super.setUp();
     m_Instances.setClassIndex(1);
   }
@@ -49,6 +64,26 @@ public class ResampleTest extends AbstractFilterTest {
     assertEquals(m_Instances.numAttributes(), result.numAttributes());
     assertEquals("Expecting output to be 200% of input",
                  m_Instances.numInstances() * 2,  result.numInstances());
+  }
+
+  public void testSampleSizePercentNoReplacement() {
+    ((Resample) m_Filter).setSampleSizePercent(20);
+    ((Resample) m_Filter).setNoReplacement(true);
+    Instances result = useFilter();
+    assertEquals(m_Instances.numAttributes(), result.numAttributes());
+    assertEquals("Expecting output to be 20% of input",
+                 (int) (m_Instances.numInstances() * 20.0 / 100),  result.numInstances());
+  }
+
+  public void testSampleSizePercentNoReplacementInverted() {
+    ((Resample) m_Filter).setSampleSizePercent(20);
+    ((Resample) m_Filter).setNoReplacement(true);
+    ((Resample) m_Filter).setInvertSelection(true);
+    Instances result = useFilter();
+    assertEquals(m_Instances.numAttributes(), result.numAttributes());
+    assertEquals("Expecting output to be 80% of input (20% inverted)",
+                 m_Instances.numInstances() 
+                 - (int) (m_Instances.numInstances() * 20.0 / 100),  result.numInstances());
   }
 
   public void testNoBias() throws Exception {
