dbbb6a1e90c18b8e0ef4dc20b531f1e32913325f
(no author) <(no author)@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2007-06-01 05:28:14 +0000

    This commit was manufactured by cvs2svn to create tag 'dev-3-5-6'.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/tags@3989 e0a1b77d-ad91-4216-81b1-defd5f83fa92

---
+++ dev-3-5-6/weka/classifiers/meta/ensembleSelection/EnsembleModelMismatchException.java
@@ -0,0 +1,45 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ *    EnsembleModelMismatchException.java
+ *    Copyright (C) 2006 Robert Jung
+ *
+ */
+
+package weka.classifiers.meta.ensembleSelection;
+
+/**
+ * This excpetion gets thrown when a models don't match.
+ * 
+ * @author  Robert Jung (mrbobjung@gmail.com)
+ * @version $Revision: 1.1 $
+ */
+public class EnsembleModelMismatchException
+  extends Exception {
+  
+  /** for serialization */
+  private static final long serialVersionUID = 4660917211181280739L;
+
+  /**
+   * constructor of the exception
+   * 
+   * @param s		the message for the exception
+   */
+  public EnsembleModelMismatchException(String s) {
+    super(s);
+  }
+}
