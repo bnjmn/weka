5658569f1987c77d08d26120d034525d408dff0e
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2007-06-24 21:41:34 +0000

    Alex has ok'd copyright for the UoW.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/branches/waikato/weka@4051 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- classifiers/functions/supportVector/StringKernel.java
+++ classifiers/functions/supportVector/StringKernel.java
@@ -1,3 +1,25 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ *    StringKernel.java
+ *    Copyright (C) 2006 University of Waikato, Hamilton, New Zealand
+ *
+ */
+
 package weka.classifiers.functions.supportVector;
 
 import weka.core.Attribute;
@@ -252,7 +274,7 @@ import java.util.Vector;
  *
  * @author Florian Kleedorfer (kleedorfer@austria.fm)
  * @author Alexander K. Seewald (alex@seewald.at)
- * @version $Revision: 1.5 $
+ * @version $Revision: 1.5.2.1 $
  */
 public class StringKernel 
   extends Kernel
