6c68fb2f459fb9dd1744c5fa71d878454e165b74
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2007-08-22 02:07:45 +0000

    Initial import
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@4120 e0a1b77d-ad91-4216-81b1-defd5f83fa92

---
+++ tests/weka/classifiers/trees/J48graftTest.java
@@ -0,0 +1,53 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ * Copyright 2007 University of Waikato
+ */
+
+package weka.classifiers.trees;
+
+import weka.classifiers.AbstractClassifierTest;
+import weka.classifiers.Classifier;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Tests J48graft. Run from the command line with:<p>
+ * java weka.classifiers.trees.J48graftTest
+ *
+ * @author <a href="mailto:eibe@cs.waikato.ac.nz">Eibe Frank</a>
+ * @version $Revision: 1.1 $
+ */
+public class J48graftTest extends AbstractClassifierTest {
+
+  public J48graftTest(String name) { super(name);  }
+
+  /** Creates a default J48 */
+  public Classifier getClassifier() {
+    return new J48();
+  }
+
+  public static Test suite() {
+    return new TestSuite(J48graftTest.class);
+  }
+
+  public static void main(String[] args){
+    junit.textui.TestRunner.run(suite());
+  }
+
+}
