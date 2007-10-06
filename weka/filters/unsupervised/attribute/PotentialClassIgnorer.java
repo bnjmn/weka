e8804e76b530e5d96da7c7a80c2e6b8344a27a9f
fracpete <fracpete@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2007-10-06 22:44:59 +0000

    removed "final" from getOutputFormat method declaration since that caused VerifyError exceptions in derived filters on Mac Books: https://list.scms.waikato.ac.nz/mailman/htdig/wekalist/2007-October/011339.html
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/branches/book2ndEd-branch@4136 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/filters/unsupervised/attribute/PotentialClassIgnorer.java
+++ weka/filters/unsupervised/attribute/PotentialClassIgnorer.java
@@ -34,7 +34,7 @@ import weka.core.Instances;
  * attribute that is originally the class attribute !
  *
  * @author Eibe Frank (eibe@cs.waikato.ac.nz), Mark Hall (mhall@cs.waikato.ac.nz)
- * @version $Revision: 1.2 $ 
+ * @version $Revision: 1.2.2.1 $ 
  */
 public abstract class PotentialClassIgnorer extends Filter {
 
@@ -78,7 +78,7 @@ public abstract class PotentialClassIgnorer extends Filter {
    * @exception NullPointerException if no input structure has been
    * defined (or the output format hasn't been determined yet) 
    */
-  public final Instances getOutputFormat() {
+  public Instances getOutputFormat() {
 
     if (m_IgnoreClass) {
       outputFormatPeek().setClassIndex(m_ClassIndex);
