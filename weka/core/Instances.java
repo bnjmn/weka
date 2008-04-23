a55e84e394cd0739c487973105d45d5d30e1b1c2
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2008-04-23 04:55:16 +0000

    Added Arne Muller's methods to shuffle attribute values
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@4376 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/core/Instances.java
+++ weka/core/Instances.java
@@ -62,7 +62,7 @@ import java.util.Random;
  * @author Eibe Frank (eibe@cs.waikato.ac.nz)
  * @author Len Trigg (trigg@cs.waikato.ac.nz)
  * @author FracPete (fracpete at waikato dot ac dot nz)
- * @version $Revision: 1.75 $ 
+ * @version $Revision: 1.76 $ 
  */
 public class Instances 
   implements Serializable, RevisionHandler {
@@ -103,6 +103,12 @@ public class Instances
    * we have to keep track of the number of lines read so far. 
    * @see #readInstance(Reader) */
   protected int m_Lines = 0;
+  
+  /** used in randomizeAttribute and undoRandomizeAttribute to store/restore
+   * the index of attribute that was last shuffled, and it's original values 
+   */
+  private int attIdx4Randomization = -1;
+  private double[] attIdxOrigValues;
   
   /**
    * Reads an ARFF file from a reader, and assigns a weight of
@@ -878,6 +884,71 @@ public class Instances
       swap(j, random.nextInt(j+1));
   }
 
+  /**
+   * Does an undo of a previous call to randomizeAttribute, so that the
+   * original values of the attribute are restored. Only the original values
+   * before the last call to randomizeAttribute can be restored. Note, the
+   * original Instances object is modified. 
+   * 
+   * @throws Exception if there was no call to randomizeAttribute or if
+   * attributes were added or removed since the last call to
+   * <code>randomizeAttribute</code>
+   * @see randomizeAttribute
+   * @author Arne Muller (arne.muller@gmail.com)
+   */
+  public void undoRandomizeAttribute() throws Exception {
+    
+    if ( attIdx4Randomization < 0 )
+      throw new Exception("no randomization to undo!");
+
+    if ( attIdxOrigValues.length !=  this.numInstances() )
+      throw new Exception(
+	  "meanwhile number of attributes has changed, can't undo!");
+
+    for ( int i=0; i<attIdxOrigValues.length; i++ ) {
+      Instance instance = (Instance)(m_Instances.elementAt(i));
+      instance.modifyValue(attIdx4Randomization, attIdxOrigValues[i]);
+    }
+  
+    return;
+  }
+  
+  /**
+   * Shuffles the values of a given attribute in all instances. Note,
+   * the original Instances object is modified (i.e. no copying), and the
+   * method is not thread save. To avoid undefined behavior of an Instances
+   * object you should not perform other Instances modifying operations between
+   * a call to <code>randomizeAttribute</code> and
+   * <code>undoRandomizeAttribute</code>.
+   * 
+   * @param attIdx the index of the attribute to shuffle
+   * @param random a random number generator
+   * @param rounds how many rounds of shuffling, minimum must be 1. As more
+   * rounds of shuffling the more random your attribute value distribution
+   * (e.g. choose 3, but note that the time needed for shuffling is proportional
+   * to the number of rounds).
+   * @see undoRandomizeAttribute
+   * @author Arne Muller (arne.muller@gmail.com)
+   */
+  public void randomizeAttribute(int attIdx, Random random, int rounds) {
+    
+    attIdx4Randomization = attIdx;
+    attIdxOrigValues = this.attributeToDoubleArray(attIdx);
+    int n = numInstances();
+    for ( int j=0; j<rounds; j++ ) {
+      for ( int i=0; i<n; i++ ) {
+	int r = random.nextInt(n);
+	Instance iOne = (Instance)(m_Instances.elementAt(i));
+	Instance iTwo = (Instance)(m_Instances.elementAt(r));
+	double helper = iOne.value(attIdx);
+	iOne.modifyValue(attIdx, iTwo.value(attIdx));
+	iTwo.modifyValue(attIdx, helper);
+      }
+    }
+    
+    return;
+  }
+  
   /**
    * Reads a single instance from the reader and appends it
    * to the dataset.  Automatically expands the dataset if it
@@ -2063,6 +2134,6 @@ public class Instances
    * @return		the revision
    */
   public String getRevision() {
-    return RevisionUtils.extract("$Revision: 1.75 $");
+    return RevisionUtils.extract("$Revision: 1.76 $");
   }
 }
