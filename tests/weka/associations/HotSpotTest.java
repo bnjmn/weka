91c41bfc22b42916acb3dd94544e238f3c629521
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2008-05-05 04:23:00 +0000

    Initial import
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/branches/waikato@4383 e0a1b77d-ad91-4216-81b1-defd5f83fa92

---
+++ tests/weka/associations/HotSpotTest.java
@@ -0,0 +1,55 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ * Copyright (C) 2008 Pentaho Corporation.
+ */
+
+package weka.associations;
+
+import weka.associations.AbstractAssociatorTest;
+import weka.associations.Associator;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Tests HotSpot. Run from the command line with:<p/>
+ * java weka.associations.HotSpotTest
+ *
+ * @author Mark Hall (mhall{[at]}pentaho{[dot]}org)
+ * @version $Revision: 1.1.2.2 $
+ */
+public class HotSpotTest 
+  extends AbstractAssociatorTest {
+
+  public HotSpotTest(String name) { 
+    super(name);  
+  }
+
+  /** Creates a default HotSpot */
+  public Associator getAssociator() {
+    return new HotSpot();
+  }
+
+  public static Test suite() {
+    return new TestSuite(HotSpotTest.class);
+  }
+
+  public static void main(String[] args){
+    junit.textui.TestRunner.run(suite());
+  }
+}
