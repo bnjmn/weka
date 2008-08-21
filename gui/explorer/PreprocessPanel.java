f248e951a5da8390185d365419b33b7f3f8da317
fracpete <fracpete@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2008-08-21 05:59:16 +0000

    m_RemoveButton is now protected
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/branches/waikato/weka@4496 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- gui/explorer/PreprocessPanel.java
+++ gui/explorer/PreprocessPanel.java
@@ -102,7 +102,7 @@ import javax.swing.filechooser.FileFilter;
  *
  * @author Richard Kirkby (rkirkby@cs.waikato.ac.nz)
  * @author Len Trigg (trigg@cs.waikato.ac.nz)
- * @version $Revision: 1.71.2.1 $
+ * @version $Revision: 1.71.2.2 $
  */
 public class PreprocessPanel
   extends JPanel 
@@ -140,7 +140,7 @@ public class PreprocessPanel
   protected AttributeSelectionPanel m_AttPanel = new AttributeSelectionPanel();
 
   /** Button for removing attributes */
-  JButton m_RemoveButton = new JButton("Remove");
+  protected JButton m_RemoveButton = new JButton("Remove");
 
   /** Displays summary stats on the selected attribute */
   protected AttributeSummaryPanel m_AttSummaryPanel =
