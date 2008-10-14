6b54adf40c3de876a9c5c1bec5b18fd3961ec303
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2008-10-14 15:06:52 +0000

    Missing GPL header and copyright
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@4547 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/classifiers/functions/supportVector/StringKernel.java
+++ weka/classifiers/functions/supportVector/StringKernel.java
@@ -1,3 +1,25 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ * StringKernel.java
+ * 
+ * Copyright (C) 2006 University of Waikato, Hamilton, New Zealand
+ */
+
 package weka.classifiers.functions.supportVector;
 
 import weka.core.Attribute;
@@ -253,7 +275,7 @@ import java.util.Vector;
  *
  * @author Florian Kleedorfer (kleedorfer@austria.fm)
  * @author Alexander K. Seewald (alex@seewald.at)
- * @version $Revision: 1.9 $
+ * @version $Revision$
  */
 public class StringKernel 
   extends Kernel
@@ -1483,7 +1505,7 @@ public class StringKernel
    * @return		the revision
    */
   public String getRevision() {
-    return RevisionUtils.extract("$Revision: 1.9 $");
+    return RevisionUtils.extract("$Revision$");
   }
 }
 
