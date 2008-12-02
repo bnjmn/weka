1f18895edf900564ab524f8d5d9c12af3fd9f668
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2008-12-02 02:11:14 +0000

    Initial import.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/branches/waikato@4824 e0a1b77d-ad91-4216-81b1-defd5f83fa92

---
+++ tests/weka/classifiers/trees/LADTreeTest.java
@@ -0,0 +1,52 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ * Copyright 2008 Pentaho Corporation
+ */
+
+package weka.classifiers.trees;
+
+import weka.classifiers.AbstractClassifierTest;
+import weka.classifiers.Classifier;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Tests LADTree. Run from the command line with:<p>
+ * java weka.classifiers.trees.LADTree
+ *
+ * @author Mark Hall (mhall{[at]}pentaho{[dot]}com)
+ * @version $Revision$
+ */
+public class LADTreeTest extends AbstractClassifierTest {
+
+  public LADTreeTest(String name) { super(name);  }
+
+  /** Creates a default LADTree */
+  public Classifier getClassifier() {
+    return new LADTree();
+  }
+
+  public static Test suite() {
+    return new TestSuite(LADTreeTest.class);
+  }
+
+  public static void main(String[] args){
+    junit.textui.TestRunner.run(suite());
+  }
+}
