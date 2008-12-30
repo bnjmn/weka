e2aa3e6acb8bc006bc07b8bdc9f00922b705f94f
fracpete <fracpete@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2008-12-30 02:11:14 +0000

    fixed saving to stream
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/branches/stable-3-6/weka@4907 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- src/main/java/weka/core/converters/SerializedInstancesSaver.java
+++ src/main/java/weka/core/converters/SerializedInstancesSaver.java
@@ -26,10 +26,9 @@ import weka.core.Capabilities;
 import weka.core.RevisionUtils;
 import weka.core.Capabilities.Capability;
 
-import java.io.BufferedOutputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 
 /**
  <!-- globalinfo-start -->
@@ -49,23 +48,26 @@ import java.io.ObjectOutputStream;
  <!-- options-end -->
  *
  * @author Stefan Mutter (mutter@cs.waikato.ac.nz)
- * @version $Revision: 1.7 $
+ * @version $Revision$
  * @see Saver
  */
 public class SerializedInstancesSaver 
   extends AbstractFileSaver 
   implements BatchConverter {
 
-  /** for serialization */
+  /** for serialization. */
   static final long serialVersionUID = -7717010648500658872L;
   
-  /** Constructor */  
+  /** the output stream. */
+  protected ObjectOutputStream m_objectstream;
+  
+  /** Constructor. */  
   public SerializedInstancesSaver(){
       resetOptions();
   }
     
   /**
-   * Returns a string describing this Saver
+   * Returns a string describing this Saver.
    * 
    * @return a description of the Saver suitable for
    * displaying in the explorer/experimenter gui
@@ -84,7 +86,7 @@ public class SerializedInstancesSaver
   }
 
   /**
-   * Resets the Saver 
+   * Resets the Saver.
    */
   public void resetOptions() {
 
@@ -113,36 +115,51 @@ public class SerializedInstancesSaver
     return result;
   }
   
+  /**
+   * Resets the writer, setting writer and objectstream to null.
+   */  
+  public void resetWriter() {
+    super.resetWriter();
+    
+    m_objectstream = null;
+  }
+  
+  /**
+   * Sets the destination output stream.
+   * 
+   * @param output the output stream.
+   * @throws IOException throws an IOException if destination cannot be set
+   */
+  public void setDestination(OutputStream output) throws IOException {
+    super.setDestination(output);
+    
+    m_objectstream = new ObjectOutputStream(output);
+  }
+  
   /** 
-   * Writes a Batch of instances
+   * Writes a Batch of instances.
    * 
    * @throws IOException throws IOException if saving in batch mode is not possible
    */
   public void writeBatch() throws IOException {
-      
-      resetWriter();
-      
-      if(getRetrieval() == INCREMENTAL)
-          throw new IOException("Batch and incremental saving cannot be mixed.");
-      if(getInstances() == null)
-          throw new IOException("No instances to save");
-      setRetrieval(BATCH);
-      if(retrieveFile() == null){
-        throw new IOException("No output to standard out for serialization.");
-      }
-      setWriteMode(WRITE);
-      ObjectOutputStream outW = 
-		  new ObjectOutputStream(
-		  new BufferedOutputStream(
-		  new FileOutputStream(retrieveFile())));
+    if(getRetrieval() == INCREMENTAL)
+      throw new IOException("Batch and incremental saving cannot be mixed.");
+    
+    if(getInstances() == null)
+      throw new IOException("No instances to save");
+    
+    setRetrieval(BATCH);
+    
+    if (m_objectstream == null)
+      throw new IOException("No output for serialization.");
 
-      outW.writeObject(getInstances());
-      outW.flush();
-      outW.close();
-      setWriteMode(WAIT);
-      outW = null;
-      resetWriter();
-      setWriteMode(CANCEL);
+    setWriteMode(WRITE);
+    m_objectstream.writeObject(getInstances());
+    m_objectstream.flush();
+    m_objectstream.close();
+    setWriteMode(WAIT);
+    resetWriter();
+    setWriteMode(CANCEL);
   }
   
   /**
@@ -151,7 +168,7 @@ public class SerializedInstancesSaver
    * @return		the revision
    */
   public String getRevision() {
-    return RevisionUtils.extract("$Revision: 1.7 $");
+    return RevisionUtils.extract("$Revision$");
   }
 
   /**
