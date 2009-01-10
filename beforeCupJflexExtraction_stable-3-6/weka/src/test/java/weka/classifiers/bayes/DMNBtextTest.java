01e12087ce5282008185d10ad2321deb47e9123b
fracpete <fracpete@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2009-01-10 21:41:09 +0000

    before extraction of CUP and JFlex sources (weka.core.parser) and placing them as jars in the "lib" directory
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/tags@4938 e0a1b77d-ad91-4216-81b1-defd5f83fa92

---
+++ beforeCupJflexExtraction_stable-3-6/weka/src/test/java/weka/classifiers/bayes/DMNBtextTest.java
@@ -0,0 +1,52 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ * Copyright 2008 Pentaho Corporation
+ */
+
+package weka.classifiers.bayes;
+
+import weka.classifiers.AbstractClassifierTest;
+import weka.classifiers.Classifier;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Tests DMNBtext. Run from the command line with:<p>
+ * java weka.classifiers.bayes.DMNBtextTest
+ *
+ * @author Mark Hall (mhall{[at]}pentaho{[dot]}com)
+ * @version $Revision: 1.1 $
+ */
+public class DMNBtextTest extends AbstractClassifierTest {
+
+  public DMNBtextTest(String name) { super(name);  }
+
+  /** Creates a default DMNBtext */
+  public Classifier getClassifier() {
+    return new DMNBtext();
+  }
+
+  public static Test suite() {
+    return new TestSuite(DMNBtextTest.class);
+  }
+
+  public static void main(String[] args){
+    junit.textui.TestRunner.run(suite());
+  }
+}
