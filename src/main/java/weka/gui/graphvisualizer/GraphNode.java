ad139cd8a88b2f05b9651816ec0c6e8b7bde7b01
fracpete <fracpete@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2009-01-25 22:42:14 +0000

    made some members public (otherwise there's no way of accessing them outside the package)
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk/weka@4995 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- src/main/java/weka/gui/graphvisualizer/GraphNode.java
+++ src/main/java/weka/gui/graphvisualizer/GraphNode.java
@@ -26,23 +26,23 @@ package weka.gui.graphvisualizer;
  * This class represents a node in the Graph.
  *
  * @author Ashraf M. Kibriya (amk14@cs.waikato.ac.nz)
- * @version $Revision: 1.5 $ - 23 Apr 2003 - Initial version (Ashraf M. Kibriya)
+ * @version $Revision$ - 23 Apr 2003 - Initial version (Ashraf M. Kibriya)
  */
 
 public class GraphNode extends Object implements GraphConstants {
   /** ID and label for the node */
-  String ID, lbl;
+  public String ID, lbl;
   /** The outcomes for the given node */
-  String [] outcomes;
+  public String [] outcomes;
   /** probability table for each outcome given outcomes of parents, if any */
-  double [][] probs;   //probabilities
+  public double [][] probs;   //probabilities
   /** The x and y position of the node */
   public int x=0, y=0;
   /** The indices of parent nodes */
-  int [] prnts;       //parent nodes
+  public int [] prnts;       //parent nodes
   /** The indices of nodes to which there are edges from this
    * node, plus the type of edge */
-  int [][] edges;
+  public int [][] edges;
   /**  Type of node. Default is Normal node type */
   public int nodeType=NORMAL;
   
