ff470f744c8af725c605a4119883bdd34cc9d307
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2009-02-26 02:35:52 +0000

    Added TreeModel
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk/weka@5107 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- src/test/java/weka/classifiers/pmml/consumer/AllTests.java
+++ src/test/java/weka/classifiers/pmml/consumer/AllTests.java
@@ -38,6 +38,7 @@ public class AllTests extends TestSuite {
     suite.addTest(RegressionTest.suite());
     suite.addTest(GeneralRegressionTest.suite());
     suite.addTest(NeuralNetworkTest.suite());
+    suite.addTest(TreeModelTest.suite());
     
     return suite;
   }
