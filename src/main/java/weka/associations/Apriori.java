d445dfbaa6a4a772bc3201f83d13818ec75ec350
fracpete <fracpete@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2009-03-24 07:57:16 +0000

    fixed typo in GPL preamble
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk/weka@5192 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- src/main/java/weka/associations/Apriori.java
+++ src/main/java/weka/associations/Apriori.java
@@ -10,7 +10,7 @@
  *    GNU General Public License for more details.
  *
  *    You should have received a copy of the GNU General Public License
- *    alo0ng with this program; if not, write to the Free Software
+ *    along with this program; if not, write to the Free Software
  *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
