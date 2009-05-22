2337fa01b3433d8fd62e43bb361cdc0b82381a03
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2009-05-22 02:00:11 +0000

    Fixed a bug where the enabled/disabled state of the start button was not being updated when a new data set was set on this panel.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk/weka@5382 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- src/main/java/weka/gui/explorer/ClassifierPanel.java
+++ src/main/java/weka/gui/explorer/ClassifierPanel.java
@@ -2452,6 +2452,21 @@ public class ClassifierPanel
     
     // set new filter
     m_ClassifierEditor.setCapabilitiesFilter(filterClass);
+    
+    // Check capabilities
+    m_StartBut.setEnabled(true);
+    Capabilities currentFilter = m_ClassifierEditor.getCapabilitiesFilter();
+    Classifier classifier = (Classifier) m_ClassifierEditor.getValue();
+    Capabilities currentSchemeCapabilities =  null;
+    if (classifier != null && currentFilter != null && 
+        (classifier instanceof CapabilitiesHandler)) {
+      currentSchemeCapabilities = ((CapabilitiesHandler)classifier).getCapabilities();
+      
+      if (!currentSchemeCapabilities.supportsMaybe(currentFilter) &&
+          !currentSchemeCapabilities.supports(currentFilter)) {
+        m_StartBut.setEnabled(false);
+      }
+    }
   }
   
   /**
