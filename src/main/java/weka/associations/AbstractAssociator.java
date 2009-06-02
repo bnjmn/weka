65572fd1e888d293cca1c662a36e2c1aafbbf3e7
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2009-06-02 22:06:26 +0000

    Doc update.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk/weka@5505 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- src/main/java/weka/associations/AbstractAssociator.java
+++ src/main/java/weka/associations/AbstractAssociator.java
@@ -103,8 +103,10 @@ public abstract class AbstractAssociator
   }
 
   /** 
-   * Returns the Capabilities of this associator. Derived associators have to
-   * override this method to enable capabilities.
+   * Returns the Capabilities of this associator. Maximally permissive
+   * capabilities are allowed by default. Derived associators should
+   * override this method and first disable all capabilities and then
+   * enable just those capabilities that make sense for the scheme.
    *
    * @return            the capabilities of this object
    * @see               Capabilities
