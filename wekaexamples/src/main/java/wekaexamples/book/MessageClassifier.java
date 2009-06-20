93868e9ee8c58551f7d90e902251d3b230b1e029
fracpete <fracpete@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2009-06-20 22:46:50 +0000

    uses SerializationHelper now fixed Javadoc
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@5645 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- wekaexamples/src/main/java/wekaexamples/book/MessageClassifier.java
+++ wekaexamples/src/main/java/wekaexamples/book/MessageClassifier.java
@@ -28,50 +28,50 @@ import weka.core.Attribute;
 import weka.core.FastVector;
 import weka.core.Instance;
 import weka.core.Instances;
+import weka.core.SerializationHelper;
 import weka.core.Utils;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.StringToWordVector;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 /**
- * Java program for classifying short text messages into two classes.
+ * Java program for classifying short text messages into two classes 'miss'
+ * and 'hit'.
  * <p/>
  * See also wiki article <a href="http://weka.wiki.sourceforge.net/MessageClassifier">MessageClassifier</a>.
  */
 public class MessageClassifier
   implements Serializable {
 
-  /* The training data gathered so far. */
+  /** for serialization. */
+  private static final long serialVersionUID = -123455813150452885L;
+
+  /** The training data gathered so far. */
   private Instances m_Data = null;
 
-  /* The filter used to generate the word counts. */
+  /** The filter used to generate the word counts. */
   private StringToWordVector m_Filter = new StringToWordVector();
 
-  /* The actual classifier. */
+  /** The actual classifier. */
   private Classifier m_Classifier = new J48();
 
-  /* Whether the model is up to date. */
+  /** Whether the model is up to date. */
   private boolean m_UpToDate;
 
   /**
    * Constructs empty training dataset.
    */
-  public MessageClassifier() throws Exception {
-
+  public MessageClassifier() {
     String nameOfDataset = "MessageClassificationProblem";
 
     // Create vector of attributes.
     FastVector attributes = new FastVector(2);
 
     // Add attribute for holding messages.
-    attributes.addElement(new Attribute("Message", (FastVector)null));
+    attributes.addElement(new Attribute("Message", (FastVector) null));
 
     // Add class attribute.
     FastVector classValues = new FastVector(2);
@@ -86,9 +86,11 @@ public class MessageClassifier
 
   /**
    * Updates model using the given training message.
+   * 
+   * @param message	the message content
+   * @param classValue	the class label
    */
-  public void updateData(String message, String classValue) throws Exception {
-
+  public void updateData(String message, String classValue) {
     // Make message into instance.
     Instance instance = makeInstance(message, m_Data);
 
@@ -97,22 +99,23 @@ public class MessageClassifier
 
     // Add instance to training data.
     m_Data.add(instance);
+    
     m_UpToDate = false;
   }
 
   /**
    * Classifies a given message.
+   * 
+   * @param message	the message content
+   * @throws Exception 	if classification fails
    */
   public void classifyMessage(String message) throws Exception {
-
     // Check whether classifier has been built.
-    if (m_Data.numInstances() == 0) {
+    if (m_Data.numInstances() == 0)
       throw new Exception("No classifier available.");
-    }
 
     // Check whether classifier and filter are up to date.
     if (!m_UpToDate) {
-
       // Initialize filter and tell it about the input format.
       m_Filter.setInputFormat(m_Data);
 
@@ -121,6 +124,7 @@ public class MessageClassifier
 
       // Rebuild classifier.
       m_Classifier.buildClassifier(filteredData);
+      
       m_UpToDate = true;
     }
 
@@ -140,14 +144,17 @@ public class MessageClassifier
 
     // Output class value.
     System.err.println("Message classified as : " +
-		       m_Data.classAttribute().value((int)predicted));
+		       m_Data.classAttribute().value((int) predicted));
   }
 
   /**
    * Method that converts a text message into an instance.
+   * 
+   * @param text	the message content to convert
+   * @param data	the header information
+   * @return		the generated Instance
    */
   private Instance makeInstance(String text, Instances data) {
-
     // Create instance of length two.
     Instance instance = new Instance(2);
 
@@ -157,62 +164,74 @@ public class MessageClassifier
 
     // Give instance access to attribute information from the dataset.
     instance.setDataset(data);
+    
     return instance;
   }
 
   /**
-   * Main method.
+   * Main method. The following parameters are recognized:
+   * <ul>
+   *   <li>
+   *      <code>-m messagefile</code><br/>
+   *      Points to the file containing the message to classify or use for
+   *      updating the model.
+   *   </li>
+   *   <li>
+   *      <code>-c classlabel</code><br/>
+   *      The class label of the message if model is to be updated. Omit for
+   *      classification of a message.
+   *   </li>
+   *   <li>
+   *      <code>-t modelfile</code><br/>
+   *      The file containing the model. If it doesn't exist, it will be
+   *      created automatically.
+   *   </li>
+   * </ul>
+   * 
+   * @param args	the commandline options
    */
-  public static void main(String[] options) {
-
+  public static void main(String[] args) {
     try {
-
       // Read message file into string.
-      String messageName = Utils.getOption('m', options);
-      if (messageName.length() == 0) {
-        throw new Exception("Must provide name of message file.");
-      }
+      String messageName = Utils.getOption('m', args);
+      if (messageName.length() == 0)
+        throw new Exception("Must provide name of message file ('-m <file>').");
       FileReader m = new FileReader(messageName);
-      StringBuffer message = new StringBuffer(); int l;
-      while ((l = m.read()) != -1) {
-	message.append((char)l);
-      }
+      StringBuffer message = new StringBuffer();
+      int l;
+      while ((l = m.read()) != -1)
+	message.append((char) l);
       m.close();
       
       // Check if class value is given.
-      String classValue = Utils.getOption('c', options);
+      String classValue = Utils.getOption('c', args);
       
       // If model file exists, read it, otherwise create new one.
-      String modelName = Utils.getOption('t', options);
-      if (modelName.length() == 0) {
-	throw new Exception("Must provide name of model file.");
-      }
+      String modelName = Utils.getOption('t', args);
+      if (modelName.length() == 0)
+	throw new Exception("Must provide name of model file ('-t <file>').");
       MessageClassifier messageCl;
       try {
-	ObjectInputStream modelInObjectFile = 
-	  new ObjectInputStream(new FileInputStream(modelName));
-	messageCl = (MessageClassifier) modelInObjectFile.readObject();
-	modelInObjectFile.close();
-      } catch (FileNotFoundException e) {
+	messageCl = (MessageClassifier) SerializationHelper.read(modelName);
+      }
+      catch (FileNotFoundException e) {
 	messageCl = new MessageClassifier();
       }
       
       // Check if there are any options left
-      Utils.checkForRemainingOptions(options);
+      Utils.checkForRemainingOptions(args);
       
       // Process message.
-      if (classValue.length() != 0) {
+      if (classValue.length() != 0)
         messageCl.updateData(message.toString(), classValue);
-      } else {
+      else
         messageCl.classifyMessage(message.toString());
-      }
       
-      // Save message classifier object.
-      ObjectOutputStream modelOutObjectFile = 
-	new ObjectOutputStream(new FileOutputStream(modelName));
-      modelOutObjectFile.writeObject(messageCl);
-      modelOutObjectFile.close();
-    } catch (Exception e) {
+      // Save message classifier object only if it was updated.
+      if (classValue.length() != 0)
+	SerializationHelper.write(modelName, messageCl);
+    }
+    catch (Exception e) {
       e.printStackTrace();
     }
   }
