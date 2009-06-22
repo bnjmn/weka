38baf7e28638158940ced12fded09abb23db9062
fracpete <fracpete@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2009-06-22 01:42:28 +0000

    points of ROC are now connected
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@5663 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- wekaexamples/src/main/java/wekaexamples/gui/visualize/GenerateROC.java
+++ wekaexamples/src/main/java/wekaexamples/gui/visualize/GenerateROC.java
@@ -75,6 +75,12 @@ public class GenerateROC {
     PlotData2D plotdata = new PlotData2D(curve);
     plotdata.setPlotName(curve.relationName());
     plotdata.addInstanceNumberAttribute();
+    // specify which points are connected
+    boolean[] cp = new boolean[curve.numInstances()];
+    for (int n = 1; n < cp.length; n++)
+      cp[n] = true;
+    plotdata.setConnectPoints(cp);
+    // add plot
     tvp.addPlot(plotdata);
 
     // display curve
