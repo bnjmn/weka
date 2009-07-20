265ad72388f45421f6e5fa2cd2fac3510b3766ca
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2009-07-20 23:41:17 +0000

    Was not setting the priors in the evaluation object for each set - fixed.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk/weka@5778 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- src/main/java/weka/gui/beans/ClassifierPerformanceEvaluator.java
+++ src/main/java/weka/gui/beans/ClassifierPerformanceEvaluator.java
@@ -145,6 +145,13 @@ public class ClassifierPerformanceEvaluator
 		  m_PlotInstances.setUp();
 		}
 		if (ce.getSetNumber() <= ce.getMaxSetNumber()) {
+		  
+		  if (ce.getTrainSet().getDataSet() != null &&
+		      ce.getTrainSet().getDataSet().numInstances() > 0) {
+		    // set the priors
+		    m_eval.setPriors(ce.getTrainSet().getDataSet());
+		  }
+		  
 //		  m_visual.setText("Evaluating ("+ce.getSetNumber()+")...");
 		  if (m_logger != null) {
 		    m_logger.statusMessage(statusMessagePrefix()
