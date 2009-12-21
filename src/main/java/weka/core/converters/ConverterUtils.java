892996359af6c449b15597fa61c0dd3d607f2d84
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2009-12-21 20:22:31 +0000

    DataSource.reset() was resetting the loader in the case where the source is neither a file or URL (i.e an input stream). A loader can't really be reset for re-reading in this case and doing so causes the source to be set to the default file location (user.dir) in the case of file based loaders. This was causing a failure in the case of DataSources constructed directly with input streams since DataSource.reset() gets called in DataSource.getDataSet() before the data is actually read.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk/weka@6155 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- src/main/java/weka/core/converters/ConverterUtils.java
+++ src/main/java/weka/core/converters/ConverterUtils.java
@@ -306,7 +306,7 @@ public class ConverterUtils
       else if (m_URL != null)
 	((URLSourcedLoader) m_Loader).setURL(m_URL.toString());
       else if (m_Loader != null)
-	m_Loader.reset();
+	// m_Loader.reset(); we can't really reset the loader if the source has been set as an input stream
       
       m_BatchCounter      = 0;
       m_IncrementalBuffer = null;
@@ -739,6 +739,7 @@ public class ConverterUtils
 	  		new String[]{FileSourcedConverter.class.getName()});
     }
     catch (Exception e) {
+      e.printStackTrace();
       // ignore
     }
     finally {
