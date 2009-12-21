b83ca1e4f999ff1dadd46b3c1a06650d7bf426ed
fracpete <fracpete@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2009-12-21 00:46:49 +0000

    initial import
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@6150 e0a1b77d-ad91-4216-81b1-defd5f83fa92

---
+++ tools/snapshots/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# a script to simplify the snapshot generation
+#
+# FracPete, 2007-01-11
+# mhall, 2008-10-05
+
+/research/ml/programs/snapshots/svn/weka_snapshots.sh -o /home/ml/public_html/weka/snapshots/ -a "mhall@cs.waikato.ac.nz" -H /research/ml/programs/snapshots/svn/weka_snapshots.html $@
+
