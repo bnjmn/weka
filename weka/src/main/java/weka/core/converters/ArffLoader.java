4b87b2cf1b1439b112ca97bd33f6df5add24ac90
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2010-01-24 19:43:03 +0000

    When the source is a URL, a call to reset was defaulting back to a file as the source - fixed.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/branches/stable-3-6@6243 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/src/main/java/weka/core/converters/ArffLoader.java
+++ weka/src/main/java/weka/core/converters/ArffLoader.java
@@ -904,6 +904,9 @@ public class ArffLoader
     setSource(url.openStream());
 
     m_URL = url.toString();
+    // make sure that the file is null so that any calls to
+    // reset() work properly
+    m_File = null;
   }
   
 
