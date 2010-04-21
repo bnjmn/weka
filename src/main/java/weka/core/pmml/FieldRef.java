44d1a0b405415ac4b1a1f9080eafa0e718e013e3
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2010-04-21 03:10:10 +0000

    Added a serial version UID and missing GPL header
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/branches/waikato/weka@6461 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- src/main/java/weka/core/pmml/FieldRef.java
+++ src/main/java/weka/core/pmml/FieldRef.java
@@ -1,3 +1,25 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ *    FieldRef.java
+ *    Copyright (C) 2008 University of Waikato, Hamilton, New Zealand
+ *
+ */
+
 package weka.core.pmml;
 
 import java.util.ArrayList;
@@ -13,6 +35,11 @@ import weka.core.Attribute;
  * @version $Revision 1.0 $
  */
 public class FieldRef extends Expression {
+  
+  /**
+   * For serialization
+   */
+  private static final long serialVersionUID = -8009605897876168409L;
   
   /** The name of the field to reference */
   protected String m_fieldName = null;
