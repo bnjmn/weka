12f8f0d5e804501c67b4943a6430112197555438
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2010-05-05 02:42:40 +0000

    Now includes support in the structure of batchAssociationRules.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk/weka@6523 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- src/main/java/weka/gui/beans/Associator.java
+++ src/main/java/weka/gui/beans/Associator.java
@@ -287,7 +287,8 @@ public class Associator
                     if ((m_Associator instanceof AssociationRulesProducer) &&
                         m_rulesListeners.size() > 0) {
                       AssociationRules rules = 
-                        ((AssociationRulesProducer)m_Associator).getAssociationRules();
+                        ((AssociationRulesProducer)m_Associator).getAssociationRules();                      
+
                       BatchAssociationRulesEvent bre = 
                         new BatchAssociationRulesEvent(Associator.this, rules);
                       notifyRulesListeners(bre);
@@ -691,6 +692,7 @@ public class Associator
         ArrayList<Attribute> attInfo = new ArrayList<Attribute>();
         attInfo.add(new Attribute("LHS", (ArrayList<String>)null));
         attInfo.add(new Attribute("RHS", (ArrayList<String>)null));
+        attInfo.add(new Attribute("Support"));
         for (int i = 0; i < metricNames.length; i++) {
           attInfo.add(new Attribute(metricNames[i]));
         }
