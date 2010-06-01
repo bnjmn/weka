2faad6ed844e02b68d16bb0145d10859b832fa9c
fracpete <fracpete@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2010-06-01 21:56:19 +0000

    PropertyDialog now adjusts the height of the dialog if more than 95% of the screen height position of the dialog gets adjusted to be fully displayed on screen (unfortunately, the buttons might still get hidden by the Windows taskbar)
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/branches/stable-3-6/weka@6595 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- src/main/java/weka/gui/PropertyDialog.java
+++ src/main/java/weka/gui/PropertyDialog.java
@@ -33,12 +33,12 @@ import java.beans.PropertyEditor;
 
 import javax.swing.JDialog;
 
-/** 
+/**
  * Support for PropertyEditors with custom editors: puts the editor into
  * a separate frame.
  *
  * @author Len Trigg (trigg@cs.waikato.ac.nz)
- * @version $Revision: 1.9 $
+ * @version $Revision$
  */
 public class PropertyDialog
   extends JDialog {
@@ -51,7 +51,7 @@ public class PropertyDialog
 
   /** The custom editor component. */
   private Component m_EditorComponent;
-  
+
   /**
    * Creates the editor frame - only kept for backward-compatibility.
    *
@@ -59,8 +59,8 @@ public class PropertyDialog
    * @param x 		initial x coord for the frame
    * @param y 		initial y coord for the frame
    * @deprecated 	instead of this constructor, one should use the constructors
-   * 			with an explicit owner (either derived from 
-   * 			<code>java.awt.Dialog</code> or from 
+   * 			with an explicit owner (either derived from
+   * 			<code>java.awt.Dialog</code> or from
    * 			<code>java.awt.Frame</code>) or, if none available,
    * 			using <code>(Frame) null</code> as owner.
    */
@@ -68,7 +68,7 @@ public class PropertyDialog
     this((Frame) null, pe, x, y);
     setVisible(true);
   }
-  
+
   /**
    * Creates the (screen-centered) editor dialog. The dialog is automatically
    * modal in case the owner is non-null.
@@ -79,7 +79,7 @@ public class PropertyDialog
   public PropertyDialog(Dialog owner, PropertyEditor pe) {
     this(owner, pe, -1, -1);
   }
-  
+
   /**
    * Creates the editor dialog at the given position. The dialog is automatically
    * modal in case the owner is non-null.
@@ -93,7 +93,7 @@ public class PropertyDialog
     super(owner, pe.getClass().getName(), true);
     initialize(pe, x, y);
   }
-  
+
   /**
    * Creates the (screen-centered) editor dialog. The dialog is automatically
    * modal in case the owner is non-null.
@@ -104,7 +104,7 @@ public class PropertyDialog
   public PropertyDialog(Frame owner, PropertyEditor pe) {
     this(owner, pe, -1, -1);
   }
-  
+
   /**
    * Creates the editor dialog at the given position. The dialog is automatically
    * modal in case the owner is non-null.
@@ -116,10 +116,10 @@ public class PropertyDialog
    */
   public PropertyDialog(Frame owner, PropertyEditor pe, int x, int y) {
     super(owner, pe.getClass().getName(), true);
-    
+
     initialize(pe, x, y);
   }
-  
+
   /**
    * Initializes the dialog.
    *
@@ -140,11 +140,25 @@ public class PropertyDialog
     getContentPane().add(m_EditorComponent, BorderLayout.CENTER);
 
     pack();
-    
-    if ((x == -1) && (y == -1))
+
+    int screenWidth = getGraphicsConfiguration().getBounds().width;
+    int screenHeight = getGraphicsConfiguration().getBounds().height;
+
+    // adjust height to a maximum of 95% of screen height
+    if (getHeight() > (double) screenHeight * 0.95)
+      setSize(getWidth(), (int) ((double) screenHeight * 0.95));
+
+    if ((x == -1) && (y == -1)) {
       setLocationRelativeTo(null);
-    else
+    }
+    else {
+      // adjust position if necessary
+      if (x + getWidth() > screenWidth)
+	x = screenWidth - getWidth();
+      if (y + getHeight() > screenHeight)
+	y = screenHeight - getHeight();
       setLocation(x, y);
+    }
   }
 
   /**
@@ -158,16 +172,16 @@ public class PropertyDialog
 
   /**
    * Tries to determine the frame this panel is part of.
-   * 
+   *
    * @param c		the container to start with
    * @return		the parent frame if one exists or null if not
    */
   public static Frame getParentFrame(Container c) {
     Frame	result;
     Container	parent;
-    
+
     result = null;
-    
+
     parent = c;
     while (parent != null) {
       if (parent instanceof Frame) {
@@ -178,22 +192,22 @@ public class PropertyDialog
 	parent = parent.getParent();
       }
     }
-    
+
     return result;
   }
 
   /**
    * Tries to determine the dialog this panel is part of.
-   * 
+   *
    * @param c		the container to start with
    * @return		the parent dialog if one exists or null if not
    */
   public static Dialog getParentDialog(Container c) {
     Dialog	result;
     Container	parent;
-    
+
     result = null;
-    
+
     parent = c;
     while (parent != null) {
       if (parent instanceof Dialog) {
@@ -204,7 +218,7 @@ public class PropertyDialog
 	parent = parent.getParent();
       }
     }
-    
+
     return result;
   }
 }
