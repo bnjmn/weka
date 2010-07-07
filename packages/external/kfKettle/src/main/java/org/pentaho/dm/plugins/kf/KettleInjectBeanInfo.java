01d4b17a204aa213ae9052162e11c2d0b3e9c1c6
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2010-07-07 08:02:54 +0000

    Initial import of the kfKettle package
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@6645 e0a1b77d-ad91-4216-81b1-defd5f83fa92

---
+++ packages/external/kfKettle/src/main/java/org/pentaho/dm/plugins/kf/KettleInjectBeanInfo.java
@@ -0,0 +1,35 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ *    KettleInjectBeanInfo.java
+ *    Copyright (C) 2008 Pentaho Corporation
+ *
+ */
+
+package org.pentaho.dm.plugins.kf;
+
+import java.beans.*;
+
+import weka.gui.beans.AbstractDataSourceBeanInfo;
+
+/**
+ * Bean info class for the KettleInject bean
+ *
+ * @author Mark Hall (mhall@{[at]}pentaho{[dot]}com)
+ * @version $Revision$
+ */
+public class KettleInjectBeanInfo extends AbstractDataSourceBeanInfo { }
