fdc330035e901c0e39b51dc3938f56f4787a4c2d
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2010-08-03 03:42:27 +0000

    Changed some characters that were unmappable for UTF8
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@6740 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- packages/external/associationRulesVisualizer/src/main/java/ar/ARPanel.java
+++ packages/external/associationRulesVisualizer/src/main/java/ar/ARPanel.java
@@ -201,7 +201,7 @@ public class ARPanel
 	}
 
 	private void showAbout(java.awt.event.ActionEvent evt) {
-		String message = "ARViewer 1.0 \n \n Sébastien Demay \n Damien Delautre \n \n LIFL - OPAC";
+		String message = "ARViewer 1.0 \n \n Sebastien Demay \n Damien Delautre \n \n LIFL - OPAC";
 		JOptionPane.showMessageDialog(this, message, "About ARViewer", JOptionPane.INFORMATION_MESSAGE);
 
 	}
