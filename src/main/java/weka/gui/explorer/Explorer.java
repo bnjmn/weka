d27b2ad224814f46820282de13649c6334d03220
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2010-08-13 03:10:32 +0000

    Main method now calls GenericObjectEditor.determineClasses() to ensure that the GenericPropertiesCreator populates the lists fully.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk/weka@6778 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- src/main/java/weka/gui/explorer/Explorer.java
+++ src/main/java/weka/gui/explorer/Explorer.java
@@ -354,6 +354,9 @@ public class Explorer
     weka.core.logging.Logger.log(weka.core.logging.Logger.Level.INFO, "Logging started");
     
     LookAndFeel.setLookAndFeel();
+    // make sure that packages are loaded and the GenericPropertiesCreator
+    // executes to populate the lists correctly
+    weka.gui.GenericObjectEditor.determineClasses();
     
     try {
       // uncomment to disable the memory management:
