fbbcde033ac67676964225945055ff9e09b2b579
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2010-12-01 01:55:56 +0000

    Moved complement naive bayes out to a package
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@6981 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- packages/internal/complementNaiveBayes/src/test/java/weka/classifiers/bayes/ComplementNaiveBayesTest.java
+++ packages/internal/complementNaiveBayes/src/test/java/weka/classifiers/bayes/ComplementNaiveBayesTest.java
@@ -32,7 +32,7 @@ import junit.framework.TestSuite;
  * java weka.classifiers.bayes.ComplementNaiveBayesTest
  *
  * @author FracPete (fracpete at waikato dot ac dot nz)
- * @version $Revision: 1.3 $
+ * @version $Revision$
  */
 public class ComplementNaiveBayesTest 
   extends AbstractClassifierTest {
