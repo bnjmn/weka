f32cd1b5f4e1b47457dce4bac4918cc4967b1786
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2011-02-10 06:55:03 +0000

    Iterative routine for finding the requested number of rules now always starts at 1.0 for the support (instead of upper bound on min support). Upper bound is still used to prune the rule set in each iteration.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk/weka@7060 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- src/main/java/weka/associations/FPGrowth.java
+++ src/main/java/weka/associations/FPGrowth.java
@@ -2249,7 +2249,8 @@ public class FPGrowth extends AbstractAssociator
       ? m_delta / m_numInstances
       : m_delta;
       
-    double currentSupport = upperBoundMinSuppAsFraction;      
+    //double currentSupport = upperBoundMinSuppAsFraction;
+    double currentSupport = 1.0;
              
     if (m_findAllRulesForSupportLevel) {
       currentSupport = lowerBoundMinSuppAsFraction;
