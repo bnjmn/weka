fc56501f3f15bd8b7e3873867ef6d3033d6d810a
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2011-07-25 03:50:16 +0000

    Now doesn't try to setup the panel (via activePerspective()) if there is no data!!
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk/weka@7402 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- src/main/java/weka/gui/beans/AttributeSummarizer.java
+++ src/main/java/weka/gui/beans/AttributeSummarizer.java
@@ -190,52 +190,56 @@ public class AttributeSummarizer
   protected void setUpFinal() {
     removeAll();
     
+    if (m_visualizeDataSet == null) {
+      return;
+    }
+
     if (!m_runningAsPerspective || m_activePerspective) {
-    final JScrollPane hp = makePanel();
-    add(hp, BorderLayout.CENTER);
-    
-    if (m_showClassCombo) {
-      Vector<String> atts = new Vector<String>();
-      for (int i = 0; i < m_visualizeDataSet.numAttributes(); i++) {
-        atts.add("(" + Attribute.typeToStringShort(m_visualizeDataSet.attribute(i)) + ") "
-            + m_visualizeDataSet.attribute(i).name());
-      }
-      
-      final JComboBox classCombo = new JComboBox();
-      classCombo.setModel(new DefaultComboBoxModel(atts));      
-      
-      if (atts.size() > 0) {
-        if (m_visualizeDataSet.classIndex() < 0) {
-          classCombo.setSelectedIndex(atts.size() - 1);
-        } else {
-          classCombo.setSelectedIndex(m_visualizeDataSet.classIndex());
+      final JScrollPane hp = makePanel();
+      add(hp, BorderLayout.CENTER);
+
+      if (m_showClassCombo) {
+        Vector<String> atts = new Vector<String>();
+        for (int i = 0; i < m_visualizeDataSet.numAttributes(); i++) {
+          atts.add("(" + Attribute.typeToStringShort(m_visualizeDataSet.attribute(i)) + ") "
+              + m_visualizeDataSet.attribute(i).name());
         }
-        classCombo.setEnabled(true);
-        for (int i = 0; i < m_plots.size(); i++) {
-          m_plots.get(i).setColoringIndex(classCombo.getSelectedIndex());
-        }
-      }
-      
-      JPanel comboHolder = new JPanel();
-      comboHolder.setLayout(new BorderLayout());
-      JPanel tempHolder = new JPanel();
-      tempHolder.setLayout(new BorderLayout());
-      tempHolder.add(new JLabel("Class: "), BorderLayout.WEST);
-      tempHolder.add(classCombo, BorderLayout.EAST);
-      comboHolder.add(tempHolder, BorderLayout.WEST);
-      add(comboHolder, BorderLayout.NORTH);
-            
-      classCombo.addActionListener(new ActionListener() {
-        public void actionPerformed(ActionEvent e) {
-          int selected = classCombo.getSelectedIndex();
-          if (selected >= 0) {
-            for (int i = 0; i < m_plots.size(); i++) {
-              m_plots.get(i).setColoringIndex(selected);
-            }
+
+        final JComboBox classCombo = new JComboBox();
+        classCombo.setModel(new DefaultComboBoxModel(atts));      
+
+        if (atts.size() > 0) {
+          if (m_visualizeDataSet.classIndex() < 0) {
+            classCombo.setSelectedIndex(atts.size() - 1);
+          } else {
+            classCombo.setSelectedIndex(m_visualizeDataSet.classIndex());
+          }
+          classCombo.setEnabled(true);
+          for (int i = 0; i < m_plots.size(); i++) {
+            m_plots.get(i).setColoringIndex(classCombo.getSelectedIndex());
           }
         }
-      });
-    }
+
+        JPanel comboHolder = new JPanel();
+        comboHolder.setLayout(new BorderLayout());
+        JPanel tempHolder = new JPanel();
+        tempHolder.setLayout(new BorderLayout());
+        tempHolder.add(new JLabel("Class: "), BorderLayout.WEST);
+        tempHolder.add(classCombo, BorderLayout.EAST);
+        comboHolder.add(tempHolder, BorderLayout.WEST);
+        add(comboHolder, BorderLayout.NORTH);
+
+        classCombo.addActionListener(new ActionListener() {
+          public void actionPerformed(ActionEvent e) {
+            int selected = classCombo.getSelectedIndex();
+            if (selected >= 0) {
+              for (int i = 0; i < m_plots.size(); i++) {
+                m_plots.get(i).setColoringIndex(selected);
+              }
+            }
+          }
+        });
+      }
     }
   }
 
