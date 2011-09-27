d99c32483d9d95ebc9bcdeab27fe5e4b0c529a5f
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2011-09-27 00:28:28 +0000

    Updated for the new customizer.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk/weka@7606 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- src/main/java/weka/gui/beans/ClassifierPerformanceEvaluatorBeanInfo.java
+++ src/main/java/weka/gui/beans/ClassifierPerformanceEvaluatorBeanInfo.java
@@ -28,7 +28,7 @@ import java.beans.*;
  * Bean info class for the classifier performance evaluator
  *
  * @author <a href="mailto:mhall@cs.waikato.ac.nz">Mark Hall</a>
- * @version $Revision: 1.5 $
+ * @version $Revision$
  */
 public class ClassifierPerformanceEvaluatorBeanInfo extends SimpleBeanInfo {
   
@@ -52,5 +52,32 @@ public class ClassifierPerformanceEvaluatorBeanInfo extends SimpleBeanInfo {
       ex.printStackTrace();
     }
     return null;
+  }
+  
+  /**
+   * Get the property descriptors for this bean
+   *
+   * @return a <code>PropertyDescriptor[]</code> value
+   */
+  public PropertyDescriptor[] getPropertyDescriptors() {
+    try {
+      PropertyDescriptor p1;
+      p1 = new PropertyDescriptor("executionSlots", ClassifierPerformanceEvaluator.class);
+      PropertyDescriptor [] pds = { p1 };
+      return pds;
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+    return null;
+  }
+  
+  /**
+   * Get the bean descriptor for this bean
+   *
+   * @return a <code>BeanDescriptor</code> value
+   */
+  public BeanDescriptor getBeanDescriptor() {
+    return new BeanDescriptor(weka.gui.beans.ClassifierPerformanceEvaluator.class,
+                              ClassifierPerformanceEvaluatorCustomizer.class);
   }
 }
