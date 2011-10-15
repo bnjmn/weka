80b0650b86e479601268bf788f85e8bf0eeb793e
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2011-10-15 08:00:03 +0000

    Now supports saving with gzip compression
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/branches/waikato/weka@7705 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- src/main/java/weka/core/converters/ArffSaver.java
+++ src/main/java/weka/core/converters/ArffSaver.java
@@ -25,44 +25,164 @@ package weka.core.converters;
 import weka.core.Capabilities;
 import weka.core.Instance;
 import weka.core.Instances;
+import weka.core.Option;
 import weka.core.RevisionUtils;
+import weka.core.Utils;
 import weka.core.Capabilities.Capability;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.util.Enumeration;
+import java.util.Vector;
+import java.util.zip.GZIPOutputStream;
 
 /**
  * Writes to a destination in arff text format. <p/>
  *
  <!-- options-start -->
  * Valid options are: <p/>
- *
+ * 
  * <pre> -i &lt;the input file&gt;
  * The input file</pre>
- *
+ * 
  * <pre> -o &lt;the output file&gt;
  * The output file</pre>
- *
+ * 
  <!-- options-end -->
  *
  * @author Stefan Mutter (mutter@cs.waikato.ac.nz)
  * @version $Revision$
  * @see Saver
  */
-public class ArffSaver
-  extends AbstractFileSaver
+public class ArffSaver 
+  extends AbstractFileSaver 
   implements BatchConverter, IncrementalConverter {
 
   /** for serialization */
   static final long serialVersionUID = 2223634248900042228L;
-
-  /** Constructor */
+  
+  /** whether to compress the output */
+  protected boolean m_CompressOutput = false;
+  
+  /** Constructor */  
   public ArffSaver(){
-
+  
       resetOptions();
   }
+  
+  /**
+   * Returns an enumeration describing the available options.
+   * 
+   * @return an enumeration of all the available options.
+   */
+  public Enumeration listOptions() {
+    Vector<Option>      result;
+    
+    result = new Vector<Option>();
+    
+    Enumeration en = super.listOptions();
+    while (en.hasMoreElements())
+      result.addElement((Option)en.nextElement());
+    
+    result.addElement(
+        new Option(
+            "\tThe class index (first and last are valid as well).\n"
+            + "\t(default: last)",
+            "C", 1, "-C <class index>"));
+    
+    result.addElement(
+        new Option(
+            "\tCompresses the data (uses '" 
+            + XRFFLoader.FILE_EXTENSION_COMPRESSED 
+            + "' as extension instead of '" 
+            + XRFFLoader.FILE_EXTENSION + "')\n"
+            + "\t(default: off)",
+            "compress", 0, "-compress"));
+    
+    return result.elements();
+  }
+  
+  /**
+   * returns the options of the current setup
+   *
+   * @return            the current options
+   */
+  public String[] getOptions(){
+    int         i;
+    Vector<String>      result;
+    String[]    options;
 
+    result = new Vector<String>();
 
+    if (getCompressOutput())
+      result.add("-compress");
+    
+    options = super.getOptions();
+    for (i = 0; i < options.length; i++)
+      result.add(options[i]);
+
+    return (String[]) result.toArray(new String[result.size()]);          
+  }
+
+  /**
+   * Parses the options for this object. <p/>
+   *
+   <!-- options-start -->
+   * Valid options are: <p/>
+   * 
+   * <pre> -i &lt;the input file&gt;
+   *  The input file</pre>
+   * 
+   * <pre> -o &lt;the output file&gt;
+   *  The output file</pre>
+   *  
+   * <pre> -compress
+   *  Compresses the data (uses '.xrff.gz' as extension instead of '.xrff')
+   *  (default: off)</pre>
+   * 
+   <!-- options-end -->
+   *
+   * @param options     the options to use
+   * @throws Exception  if setting of options fails
+   */
+  public void setOptions(String[] options) throws Exception {
+
+    setCompressOutput(Utils.getFlag("compress", options));
+    
+    super.setOptions(options);
+  }
+  
+  /**
+   * Returns the tip text for this property
+   * 
+   * @return            tip text for this property suitable for
+   *                    displaying in the explorer/experimenter gui
+   */
+  public String compressOutputTipText() {
+    return "Optional compression of the output data";
+  }
+  
+  /**
+   * Gets whether the output data is compressed.
+   *
+   * @return            true if the output data is compressed
+   */
+  public boolean getCompressOutput() {
+    return m_CompressOutput;
+  }
+
+  /**
+   * Sets whether to compress the output.
+   *
+   * @param value       if truee the output will be compressed
+   */
+  public void setCompressOutput(boolean value) {
+    m_CompressOutput = value;
+  }
+   
+   
   /**
    * Returns a string describing this Saver
    * @return a description of the Saver suitable for
@@ -70,10 +190,10 @@ public class ArffSaver
    */
   public String globalInfo() {
     return "Writes to a destination that is in arff (attribute relation file format) "
-      +"format. ";
+      +"format. The data can be compressed with gzip in order to save space.";
   }
 
-
+  
   /**
    * Returns a description of the file type.
    *
@@ -82,9 +202,44 @@ public class ArffSaver
   public String getFileDescription() {
     return "Arff data files";
   }
+  
+  /**
+   * Gets all the file extensions used for this type of file
+   *
+   * @return the file extensions
+   */
+  public String[] getFileExtensions() {
+    return new String[]{ArffLoader.FILE_EXTENSION, ArffLoader.FILE_EXTENSION_COMPRESSED};
+  }
+  
+  /** 
+   * Sets the destination file.
+   * 
+   * @param outputFile the destination file.
+   * @throws IOException throws an IOException if file cannot be set
+   */
+  public void setFile(File outputFile) throws IOException  {
+    if (outputFile.getAbsolutePath().endsWith(ArffLoader.FILE_EXTENSION_COMPRESSED))
+      setCompressOutput(true);
+    
+    super.setFile(outputFile);
+  }
+  
+  /** 
+   * Sets the destination output stream.
+   * 
+   * @param output              the output stream.
+   * @throws IOException        throws an IOException if destination cannot be set
+   */
+  public void setDestination(OutputStream output) throws IOException {
+    if (getCompressOutput())
+      super.setDestination(new GZIPOutputStream(output));
+    else
+      super.setDestination(output);
+  }
 
   /**
-   * Resets the Saver
+   * Resets the Saver 
    */
   public void resetOptions() {
 
@@ -92,7 +247,7 @@ public class ArffSaver
     setFileExtension(".arff");
   }
 
-  /**
+  /** 
    * Returns the Capabilities of this saver.
    *
    * @return            the capabilities of this object
@@ -100,35 +255,35 @@ public class ArffSaver
    */
   public Capabilities getCapabilities() {
     Capabilities result = super.getCapabilities();
-
+    
     // attributes
     result.enableAllAttributes();
     result.enable(Capability.MISSING_VALUES);
-
+    
     // class
     result.enableAllClasses();
     result.enable(Capability.MISSING_CLASS_VALUES);
     result.enable(Capability.NO_CLASS);
-
+    
     return result;
   }
-
+  
   /** Saves an instances incrementally. Structure has to be set by using the
    * setStructure() method or setInstances() method.
    * @param inst the instance to save
    * @throws IOException throws IOEXception if an instance cannot be saved incrementally.
-   */
+   */  
   public void writeIncremental(Instance inst) throws IOException{
-
+  
       int writeMode = getWriteMode();
       Instances structure = getInstances();
       PrintWriter outW = null;
-
+      
       if(getRetrieval() == BATCH || getRetrieval() == NONE)
           throw new IOException("Batch and incremental saving cannot be mixed.");
       if(getWriter() != null)
           outW = new PrintWriter(getWriter());
-
+          
       if(writeMode == WAIT){
         if(structure == null){
             setWriteMode(CANCEL);
@@ -161,7 +316,7 @@ public class ArffSaver
           if(structure == null)
               throw new IOException("No instances information available.");
           if(inst != null){
-          //write instance
+          //write instance 
               if(retrieveFile() == null || outW == null)
                 System.out.println(inst);
               else{
@@ -187,12 +342,12 @@ public class ArffSaver
           }
       }
   }
-
+  
   /** Writes a Batch of instances
    * @throws IOException throws IOException if saving in batch mode is not possible
    */
   public void writeBatch() throws IOException {
-
+  
       if(getInstances() == null)
           throw new IOException("No instances to save");
       if(getRetrieval() == INCREMENTAL)
@@ -204,14 +359,14 @@ public class ArffSaver
           setWriteMode(WAIT);
           return;
       }
-
+      
       PrintWriter outW = new PrintWriter(getWriter());
       Instances data = getInstances();
-
+      
       // header
       Instances header = new Instances(data, 0);
       outW.print(header.toString());
-
+      
       // data
       for (int i = 0; i < data.numInstances(); i++) {
 	if (i % 1000 == 0)
@@ -220,16 +375,16 @@ public class ArffSaver
       }
       outW.flush();
       outW.close();
-
+      
       setWriteMode(WAIT);
       outW = null;
       resetWriter();
       setWriteMode(CANCEL);
   }
-
+  
   /**
    * Returns the revision string.
-   *
+   * 
    * @return		the revision
    */
   public String getRevision() {
