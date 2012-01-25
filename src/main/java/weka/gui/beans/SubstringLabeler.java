04b08e14c1b00b7ed430c643053160102415364d
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2012-01-25 21:45:10 +0000

    Now checks for missing values in the String attributes.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk/weka@8003 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- src/main/java/weka/gui/beans/SubstringLabeler.java
+++ src/main/java/weka/gui/beans/SubstringLabeler.java
@@ -341,12 +341,14 @@ public class SubstringLabeler extends JPanel implements BeanCommon, Visible,
      */
     public String apply(Instance inst) {
       for (int i = 0; i < m_selectedAtts.length; i++) {
-        String value  = inst.stringValue(m_selectedAtts[i]);
-        
-        String result = apply(value);
-        if (result != null) {
-          // first match is good enough
-          return result;
+        if (!inst.isMissing(m_selectedAtts[i])) {
+          String value  = inst.stringValue(m_selectedAtts[i]);
+
+          String result = apply(value);
+          if (result != null) {
+            // first match is good enough
+            return result;
+          }
         }
       }
       
