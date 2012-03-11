2723de082daf2e90b17e83ab1da288b5f84a43d3
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2012-03-11 21:30:15 +0000

    Added the new RBFRegressor method.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk/packages/internal/RBFNetwork@8102 e0a1b77d-ad91-4216-81b1-defd5f83fa92

---
+++ src/test/java/weka/classifiers/functions/RBFRegressorTest.java
@@ -0,0 +1,70 @@
+/*
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/*
+ * Copyright (C) 2012 University of Waikato, Hamilton, New Zealand
+ */
+
+package weka.classifiers.functions;
+
+import weka.classifiers.AbstractClassifierTest;
+import weka.classifiers.CheckClassifier;
+import weka.classifiers.Classifier;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Tests RBFRegressor. Run from the command line with:<p/>
+ * java weka.classifiers.functions.RBFRegressorTest
+ *
+ * @author Mark Hall (mhall{[at]}pentaho{[dot]}com)
+ * @version $Revision$
+ */
+public class RBFRegressorTest 
+  extends AbstractClassifierTest {
+
+  public RBFRegressorTest(String name) { 
+    super(name);  
+  }
+
+  /**
+   * configures the CheckClassifier instance used throughout the tests
+   * 
+   * @return	the fully configured CheckClassifier instance used for testing
+   */
+  protected CheckClassifier getTester() {
+    CheckClassifier 	result;
+    
+    result = super.getTester();
+    result.setNumInstances(40);
+    
+    return result;
+  }
+
+  /** Creates a default RBFRegressor */
+  public Classifier getClassifier() {
+    return new RBFRegressor();
+  }
+
+  public static Test suite() {
+    return new TestSuite(RBFRegressorTest.class);
+  }
+
+  public static void main(String[] args){
+    junit.textui.TestRunner.run(suite());
+  }
+}
