d6b1c8852b7ad84db6816b54a8cb423832095c0c
fracpete <fracpete@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2012-03-15 22:44:07 +0000

    replaced license preamble with GPL3 one
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@8108 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- packages/external/optics_dbScan/src/main/java/weka/clusterers/forOPTICSAndDBScan/DataObjects/DataObject.java
+++ packages/external/optics_dbScan/src/main/java/weka/clusterers/forOPTICSAndDBScan/DataObjects/DataObject.java
@@ -1,17 +1,16 @@
 /*
- *    This program is free software; you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation; either version 2 of the License, or
- *    (at your option) any later version.
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
  *
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
  *
- *    You should have received a copy of the GNU General Public License
- *    along with this program; if not, write to the Free Software
- *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /*
@@ -37,7 +36,7 @@ import weka.core.Instance;
  * @author Matthias Schubert (schubert@dbs.ifi.lmu.de)
  * @author Zhanna Melnikova-Albrecht (melnikov@cip.ifi.lmu.de)
  * @author Rainer Holzmann (holzmann@cip.ifi.lmu.de)
- * @version $Revision: 1.2 $
+ * @version $Revision$
  */
 public interface DataObject {
 
