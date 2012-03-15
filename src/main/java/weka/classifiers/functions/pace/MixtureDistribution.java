eec8eeff79e4b35b0a52e16f8936701a7edaed65
fracpete <fracpete@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2012-03-15 22:48:36 +0000

    replaced license preamble with GPL3 one
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk/packages/internal/paceRegression@8109 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- src/main/java/weka/classifiers/functions/pace/MixtureDistribution.java
+++ src/main/java/weka/classifiers/functions/pace/MixtureDistribution.java
@@ -1,17 +1,17 @@
 /*
- *    This program is free software; you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation; either version 2 of the License, or (at
- *    your option) any later version.
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
  *
- *    This program is distributed in the hope that it will be useful, but
- *    WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    General Public License for more details.
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
  *
- *    You should have received a copy of the GNU General Public License
- *    along with this program; if not, write to the Free Software
- *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /*
  *    MixtureDistribution.java
@@ -42,7 +42,7 @@ import weka.core.matrix.IntVector;
  * prediction." Proceedings of ICML'2002. Sydney. <p>
  *
  * @author Yong Wang (yongwang@cs.waikato.ac.nz)
- * @version $Revision: 1.5 $ */
+ * @version $Revision$ */
 
 public abstract class MixtureDistribution
   implements TechnicalInformationHandler, RevisionHandler {
