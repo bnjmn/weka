cba57f25f277611e246905964c3d68ffc03ae170
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2012-04-23 10:33:59 +0000

    Now preserves attribute weight information from the original instances.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk/weka@8288 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- src/main/java/weka/filters/unsupervised/attribute/MergeTwoValues.java
+++ src/main/java/weka/filters/unsupervised/attribute/MergeTwoValues.java
@@ -428,7 +428,11 @@ public class MergeTwoValues
 	    newVals.addElement(att.value(i));
 	  }
 	}
-	newAtts.addElement(new Attribute(att.name(), newVals));
+	
+	Attribute newAtt = new Attribute(att.name(), newVals);
+	newAtt.setWeight(getInputFormat().attribute(j).weight());
+	
+	newAtts.addElement(newAtt);
       }
     }
       
