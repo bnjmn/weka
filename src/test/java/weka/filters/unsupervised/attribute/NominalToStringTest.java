f5cdab33ab407a5487ccf9e4208ef8e86077ecef
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2012-10-11 02:43:56 +0000

    Updated to reflect the change to string attributes to enable string values to always be present in sparse instances
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/branches/stable-3-6/weka@9180 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- src/test/java/weka/filters/unsupervised/attribute/NominalToStringTest.java
+++ src/test/java/weka/filters/unsupervised/attribute/NominalToStringTest.java
@@ -1,17 +1,16 @@
 /*
- *    This program is free software; you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation; either version 2 of the License, or
- *    (at your option) any later version.
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
  *
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
  *
- *    You should have received a copy of the GNU General Public License
- *    along with this program; if not, write to the Free Software
- *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /*
@@ -36,7 +35,7 @@ import junit.framework.TestSuite;
  * java weka.filters.unsupervised.attribute.NominalToStringTest
  *
  * @author FracPete (fracpete at waikato dot ac dot nz)
- * @version $Revision: 1.2 $
+ * @version $Revision$
  */
 public class NominalToStringTest
   extends AbstractFilterTest {
@@ -95,7 +94,7 @@ public class NominalToStringTest
     assertEquals("Attribute type should now be STRING",
                  Attribute.STRING, result.attribute(1).type());
 
-    assertEquals(3, result.attribute(1).numValues());
+    assertEquals(4, result.attribute(1).numValues());
   }
 
   public void testMissing() {
@@ -107,7 +106,7 @@ public class NominalToStringTest
     assertEquals("Attribute type should now be STRING",
                  Attribute.STRING, result.attribute(4).type());
 
-    assertEquals(4, result.attribute(4).numValues());
+    assertEquals(5, result.attribute(4).numValues());
     for (int i = 0; i < result.numInstances(); i++) {
       assertTrue("Missing values should be preserved",
              m_Instances.instance(i).isMissing(4) ==
