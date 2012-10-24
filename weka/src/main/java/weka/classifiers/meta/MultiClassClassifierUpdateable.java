cb7b7ab4611f504d7a51acd9967238a2980c6171
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2012-10-24 01:59:26 +0000

    1-vs-1 with pairwise coupling was failing on the first instance (due to an unormalizable probs array) in interleaved test and train mode in the Knowledge Flow - fixed.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@9245 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/src/main/java/weka/classifiers/meta/MultiClassClassifierUpdateable.java
+++ weka/src/main/java/weka/classifiers/meta/MultiClassClassifierUpdateable.java
@@ -31,52 +31,71 @@ import weka.core.Utils;
 import weka.filters.unsupervised.instance.RemoveWithValues;
 
 /**
- <!-- globalinfo-start -->
- * A metaclassifier for handling multi-class datasets with 2-class classifiers. This classifier is also capable of applying error correcting output codes for increased accuracy. The base classifier must be an updateable classifier
+ * <!-- globalinfo-start --> A metaclassifier for handling multi-class datasets
+ * with 2-class classifiers. This classifier is also capable of applying error
+ * correcting output codes for increased accuracy. The base classifier must be
+ * an updateable classifier
  * <p/>
- <!-- globalinfo-end -->
- *
- <!-- options-start -->
- * Valid options are: <p/>
+ * <!-- globalinfo-end -->
  * 
- * <pre> -M &lt;num&gt;
+ * <!-- options-start --> Valid options are:
+ * <p/>
+ * 
+ * <pre>
+ * -M &lt;num&gt;
  *  Sets the method to use. Valid values are 0 (1-against-all),
  *  1 (random codes), 2 (exhaustive code), and 3 (1-against-1). (default 0)
  * </pre>
  * 
- * <pre> -R &lt;num&gt;
- *  Sets the multiplier when using random codes. (default 2.0)</pre>
+ * <pre>
+ * -R &lt;num&gt;
+ *  Sets the multiplier when using random codes. (default 2.0)
+ * </pre>
  * 
- * <pre> -P
- *  Use pairwise coupling (only has an effect for 1-against1)</pre>
+ * <pre>
+ * -P
+ *  Use pairwise coupling (only has an effect for 1-against1)
+ * </pre>
  * 
- * <pre> -S &lt;num&gt;
+ * <pre>
+ * -S &lt;num&gt;
  *  Random number seed.
- *  (default 1)</pre>
+ *  (default 1)
+ * </pre>
  * 
- * <pre> -D
+ * <pre>
+ * -D
  *  If set, classifier is run in debug mode and
- *  may output additional info to the console</pre>
+ *  may output additional info to the console
+ * </pre>
  * 
- * <pre> -W
+ * <pre>
+ * -W
  *  Full name of base classifier.
- *  (default: weka.classifiers.functions.Logistic)</pre>
+ *  (default: weka.classifiers.functions.Logistic)
+ * </pre>
  * 
- * <pre> 
+ * <pre>
  * Options specific to classifier weka.classifiers.functions.Logistic:
  * </pre>
  * 
- * <pre> -D
- *  Turn on debugging output.</pre>
+ * <pre>
+ * -D
+ *  Turn on debugging output.
+ * </pre>
  * 
- * <pre> -R &lt;ridge&gt;
- *  Set the ridge in the log-likelihood.</pre>
+ * <pre>
+ * -R &lt;ridge&gt;
+ *  Set the ridge in the log-likelihood.
+ * </pre>
  * 
- * <pre> -M &lt;number&gt;
- *  Set the maximum number of iterations (default -1, until convergence).</pre>
+ * <pre>
+ * -M &lt;number&gt;
+ *  Set the maximum number of iterations (default -1, until convergence).
+ * </pre>
  * 
- <!-- options-end -->
- *
+ * <!-- options-end -->
+ * 
  * @author Eibe Frank (eibe@cs.waikato.ac.nz)
  * @author Len Trigg (len@reeltwo.com)
  * @author Richard Kirkby (rkirkby@cs.waikato.ac.nz)
@@ -84,49 +103,51 @@ import weka.filters.unsupervised.instance.RemoveWithValues;
  * 
  * @version $Revision$
  */
-public class MultiClassClassifierUpdateable 
-  extends MultiClassClassifier 
-  implements OptionHandler, UpdateableClassifier {
-  
+public class MultiClassClassifierUpdateable extends MultiClassClassifier
+    implements OptionHandler, UpdateableClassifier {
+
   /** For serialization */
-  private static final long serialVersionUID = -1619685269774366430L;  
+  private static final long serialVersionUID = -1619685269774366430L;
 
   /**
-   * @return a description of the classifier suitable for
-   * displaying in the explorer/experimenter gui
+   * @return a description of the classifier suitable for displaying in the
+   *         explorer/experimenter gui
    */
+  @Override
   public String globalInfo() {
 
     return "A metaclassifier for handling multi-class datasets with 2-class "
-      + "classifiers. This classifier is also capable of "
-      + "applying error correcting output codes for increased accuracy. "
-      + "The base classifier must be an updateable classifier";
+        + "classifiers. This classifier is also capable of "
+        + "applying error correcting output codes for increased accuracy. "
+        + "The base classifier must be an updateable classifier";
   }
 
+  @Override
   public void buildClassifier(Instances insts) throws Exception {
     if (m_Classifier == null) {
       throw new Exception("No base classifier has been set!");
     }
-    
+
     if (!(m_Classifier instanceof UpdateableClassifier)) {
       throw new Exception("Base classifier must be updateable!");
     }
-    
-    super.buildClassifier(insts);                
+
+    super.buildClassifier(insts);
   }
-  
+
   /**
    * Updates the classifier with the given instance.
-   *
-   * @param instance the new training instance to include in the model 
-   * @exception Exception if the instance could not be incorporated in
-   * the model.
+   * 
+   * @param instance the new training instance to include in the model
+   * @exception Exception if the instance could not be incorporated in the
+   *              model.
    */
+  @Override
   public void updateClassifier(Instance instance) throws Exception {
     if (!instance.classIsMissing()) {
 
       if (m_Classifiers.length == 1) {
-        ((UpdateableClassifier)m_Classifiers[0]).updateClassifier(instance);
+        ((UpdateableClassifier) m_Classifiers[0]).updateClassifier(instance);
         return;
       }
 
@@ -136,9 +157,9 @@ public class MultiClassClassifierUpdateable
           Instance converted = m_ClassFilters[i].output();
           if (converted != null) {
             converted.dataset().setClassIndex(m_ClassAttribute.index());
-            ((UpdateableClassifier)m_Classifiers[i]).
-            updateClassifier(converted);
-            
+            ((UpdateableClassifier) m_Classifiers[i])
+                .updateClassifier(converted);
+
             if (m_Method == METHOD_1_AGAINST_1) {
               m_SumOfWeights[i] += converted.weight();
             }
@@ -147,28 +168,33 @@ public class MultiClassClassifierUpdateable
       }
     }
   }
-  
+
   /**
    * Returns the distribution for an instance.
-   *
+   * 
    * @param inst the instance to get the distribution for
    * @return the distribution
    * @throws Exception if the distribution can't be computed successfully
    */
+  @Override
   public double[] distributionForInstance(Instance inst) throws Exception {
-    
+
+    if (m_Classifiers.length == 1) {
+      return m_Classifiers[0].distributionForInstance(inst);
+    }
+
     double[] probs = new double[inst.numClasses()];
     if (m_Method == METHOD_1_AGAINST_1) {
       double[][] r = new double[inst.numClasses()][inst.numClasses()];
       double[][] n = new double[inst.numClasses()][inst.numClasses()];
 
-      for(int i = 0; i < m_ClassFilters.length; i++) {
+      for (int i = 0; i < m_ClassFilters.length; i++) {
         if (m_Classifiers[i] != null && m_SumOfWeights[i] > 0) {
-          Instance tempInst = (Instance)inst.copy(); 
+          Instance tempInst = (Instance) inst.copy();
           tempInst.setDataset(m_TwoClassDataset);
-          double [] current = m_Classifiers[i].distributionForInstance(tempInst);  
-          Range range = new Range(((RemoveWithValues)m_ClassFilters[i])
-                                  .getNominalIndices());
+          double[] current = m_Classifiers[i].distributionForInstance(tempInst);
+          Range range = new Range(
+              ((RemoveWithValues) m_ClassFilters[i]).getNominalIndices());
           range.setUpper(m_ClassAttribute.numValues());
           int[] pair = range.getSelection();
           if (m_pairwiseCoupling && inst.numClasses() > 2) {
@@ -184,37 +210,43 @@ public class MultiClassClassifierUpdateable
         }
       }
       if (m_pairwiseCoupling && inst.numClasses() > 2) {
-        return pairwiseCoupling(n, r);
+        try {
+          return pairwiseCoupling(n, r);
+        } catch (IllegalArgumentException ex) {
+        }
       }
-      Utils.normalize(probs);
-    } else {    
+      if (Utils.gr(Utils.sum(probs), 0)) {
+        Utils.normalize(probs);
+      }
+      return probs;
+    } else {
       probs = super.distributionForInstance(inst);
     }
-    
-    if (probs.length == 1) {
-      // ZeroR made the prediction
-      return new double[m_ClassAttribute.numValues()];
-    }
-    
+
+    /*
+     * if (probs.length == 1) { // ZeroR made the prediction return new
+     * double[m_ClassAttribute.numValues()]; }
+     */
+
     return probs;
   }
-  
+
   /**
    * Returns the revision string.
    * 
-   * @return		the revision
+   * @return the revision
    */
+  @Override
   public String getRevision() {
     return RevisionUtils.extract("$Revision$");
   }
 
   /**
    * Main method for testing this class.
-   *
+   * 
    * @param argv the options
    */
-  public static void main(String [] argv) {
+  public static void main(String[] argv) {
     runClassifier(new MultiClassClassifierUpdateable(), argv);
   }
 }
-
