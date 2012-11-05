347608d8edba0023075236589ad733ef92891f0d
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2012-11-05 01:59:39 +0000

    Moved implementations of the stream tokenizer helper methods out to StreamTokenizerUtils. The methods in this class call the implementations in StreamTokenizerUtils to avoid breaking existing code. Clients that only need stream tokenizer helpers should use StreamTokenizerUtils directly as it does not drag in the GOE-related baggage that ConverterUtils does.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk/weka@9285 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- src/main/java/weka/core/converters/ConverterUtils.java
+++ src/main/java/weka/core/converters/ConverterUtils.java
@@ -881,15 +881,7 @@ public class ConverterUtils
    */
   public static void getFirstToken(StreamTokenizer tokenizer) 
     throws IOException {
-    
-    while (tokenizer.nextToken() == StreamTokenizer.TT_EOL){};
-    if ((tokenizer.ttype == '\'') ||
-	(tokenizer.ttype == '"')) {
-      tokenizer.ttype = StreamTokenizer.TT_WORD;
-    } else if ((tokenizer.ttype == StreamTokenizer.TT_WORD) &&
-	       (tokenizer.sval.equals("?"))) {
-      tokenizer.ttype = '?';
-    }
+    StreamTokenizerUtils.getFirstToken(tokenizer);
   }
 
   /**
@@ -898,20 +890,8 @@ public class ConverterUtils
    * @param tokenizer 		the stream tokenizer
    * @throws IOException 	if reading the next token fails
    */
-  public static void getToken(StreamTokenizer tokenizer) throws IOException {
-    
-    tokenizer.nextToken();
-    if (tokenizer.ttype== StreamTokenizer.TT_EOL) {
-      return;
-    }
-
-    if ((tokenizer.ttype == '\'') ||
-	(tokenizer.ttype == '"')) {
-      tokenizer.ttype = StreamTokenizer.TT_WORD;
-    } else if ((tokenizer.ttype == StreamTokenizer.TT_WORD) &&
-	       (tokenizer.sval.equals("?"))) {
-      tokenizer.ttype = '?';
-    }
+  public static void getToken(StreamTokenizer tokenizer) throws IOException {    
+    StreamTokenizerUtils.getToken(tokenizer);
   }
 
   /**
