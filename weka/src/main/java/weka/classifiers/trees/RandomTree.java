191ffd2628746aa31217abf451184ce013656b41
eibe <eibe@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2012-11-27 03:08:53 +0000

    Fixed infinite recursion due to failure to split in RandomTree and REPTree.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@9360 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/src/main/java/weka/classifiers/trees/RandomTree.java
+++ weka/src/main/java/weka/classifiers/trees/RandomTree.java
@@ -1268,6 +1268,11 @@ public class RandomTree
             // Save split point
             splitPoint = (inst.value(att) + currSplit) / 2.0;
 
+            // Check for numeric precision problems
+            if (splitPoint <= currSplit) {
+              splitPoint = inst.value(att);
+            }
+
             // Save distribution
             for (int j = 0; j < currDist.length; j++) {
               System.arraycopy(currDist[j], 0, dist[j], 0, dist[j].length);
