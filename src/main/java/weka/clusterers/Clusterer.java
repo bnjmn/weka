38a3662d8a02c2c8643e468351dc9ff5f3e687f3
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2012-12-02 07:02:31 +0000

    Removed an unnecessary public modifier.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/branches/stable-3-6/weka@9381 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- src/main/java/weka/clusterers/Clusterer.java
+++ src/main/java/weka/clusterers/Clusterer.java
@@ -29,62 +29,57 @@ import weka.core.Instances;
 /**
  * Interface for clusterers. Clients will typically extend either
  * AbstractClusterer or AbstractDensityBasedClusterer.
- *
+ * 
  * @author Mark Hall (mhall@cs.waikato.ac.nz)
  * @revision $Revision: 1.18 $
  */
 public interface Clusterer {
 
   /**
-   * Generates a clusterer. Has to initialize all fields of the clusterer
-   * that are not being set via options.
-   *
-   * @param data set of instances serving as training data 
-   * @exception Exception if the clusterer has not been 
-   * generated successfully
+   * Generates a clusterer. Has to initialize all fields of the clusterer that
+   * are not being set via options.
+   * 
+   * @param data set of instances serving as training data
+   * @exception Exception if the clusterer has not been generated successfully
    */
   void buildClusterer(Instances data) throws Exception;
 
   /**
-   * Classifies a given instance. Either this or distributionForInstance()
-   * needs to be implemented by subclasses.
-   *
+   * Classifies a given instance. Either this or distributionForInstance() needs
+   * to be implemented by subclasses.
+   * 
    * @param instance the instance to be assigned to a cluster
    * @return the number of the assigned cluster as an integer
-   * @exception Exception if instance could not be clustered
-   * successfully
+   * @exception Exception if instance could not be clustered successfully
    */
   int clusterInstance(Instance instance) throws Exception;
 
   /**
-   * Predicts the cluster memberships for a given instance.  Either
-   * this or clusterInstance() needs to be implemented by subclasses.
-   *
+   * Predicts the cluster memberships for a given instance. Either this or
+   * clusterInstance() needs to be implemented by subclasses.
+   * 
    * @param instance the instance to be assigned a cluster.
-   * @return an array containing the estimated membership 
-   * probabilities of the test instance in each cluster (this 
-   * should sum to at most 1)
-   * @exception Exception if distribution could not be 
-   * computed successfully 
+   * @return an array containing the estimated membership probabilities of the
+   *         test instance in each cluster (this should sum to at most 1)
+   * @exception Exception if distribution could not be computed successfully
    */
-  public double[] distributionForInstance(Instance instance) throws Exception;
+  double[] distributionForInstance(Instance instance) throws Exception;
 
   /**
    * Returns the number of clusters.
-   *
+   * 
    * @return the number of clusters generated for a training dataset.
    * @exception Exception if number of clusters could not be returned
-   * successfully
+   *              successfully
    */
   int numberOfClusters() throws Exception;
 
-  /** 
+  /**
    * Returns the Capabilities of this clusterer. Derived classifiers have to
    * override this method to enable capabilities.
-   *
-   * @return            the capabilities of this object
-   * @see               Capabilities
+   * 
+   * @return the capabilities of this object
+   * @see Capabilities
    */
   public Capabilities getCapabilities();
-  
 }
