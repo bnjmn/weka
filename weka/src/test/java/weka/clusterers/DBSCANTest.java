23a5e3ae9d43f8ee71e92cc5dc7b1f64e9cbe660
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2013-01-10 09:07:19 +0000

    Fixed some typos. Added warning that implementations should not be used as reference implementations for runtime comparisons. Improved runtime by eliminating redundant data consistency checks from distance calculations, which can make a dramatic difference if the dataset has a large header, and replacing pow(x,2) by x*x.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/branches/stable-3-6@9435 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/src/test/java/weka/clusterers/DBSCANTest.java
+++ weka/src/test/java/weka/clusterers/DBSCANTest.java
@@ -1,17 +1,16 @@
 /*
- *    This program is free software; you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation; either version 2 of the License, or
- *    (at your option) any later version.
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
  *
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
  *
- *    You should have received a copy of the GNU General Public License
- *    along with this program; if not, write to the Free Software
- *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /*
@@ -27,26 +26,26 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 /**
- * Tests DBScan. Run from the command line with:<p/>
- * java weka.clusterers.DBScanTest
+ * Tests DBSCAN. Run from the command line with:<p/>
+ * java weka.clusterers.DBSCANTest
  *
  * @author FracPete (fracpete at waikato dot ac dot nz)
- * @version $Revision: 1.1 $
+ * @version $Revision: 8108 $
  */
-public class DBScanTest 
+public class DBSCANTest 
   extends AbstractClustererTest {
 
-  public DBScanTest(String name) { 
+  public DBSCANTest(String name) { 
     super(name);  
   }
 
-  /** Creates a default DBScan */
+  /** Creates a default DBSCAN */
   public Clusterer getClusterer() {
-    return new DBScan();
+    return new DBSCAN();
   }
 
   public static Test suite() {
-    return new TestSuite(DBScanTest.class);
+    return new TestSuite(DBSCANTest.class);
   }
 
   public static void main(String[] args){
