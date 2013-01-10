7d2fc2f0cf12b507d1c397181d37ab34b90c33e4
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2013-01-10 08:11:06 +0000

    [maven-release-plugin]  copy for tag optics_dbScan-1.0.3
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/tags/optics_dbScan-1.0.3@9427 e0a1b77d-ad91-4216-81b1-defd5f83fa92

---
+++ optics_dbScan/src/test/java/weka/clusterers/OPTICSTest.java
@@ -0,0 +1,56 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Copyright (C) 2005 University of Waikato, Hamilton, New Zealand
+ */
+
+package weka.clusterers;
+
+import weka.clusterers.AbstractClustererTest;
+import weka.clusterers.Clusterer;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Tests OPTICS. Run from the command line with:<p/>
+ * java weka.clusterers.OPTICSTest
+ *
+ * @author FracPete (fracpete at waikato dot ac dot nz)
+ * @version $Revision$
+ */
+public class OPTICSTest 
+  extends AbstractClustererTest {
+
+  public OPTICSTest(String name) { 
+    super(name);  
+  }
+
+  /** Creates a default OPTICS */
+  public Clusterer getClusterer() {
+    OPTICS ops = new OPTICS();
+    ops.setShowGUI(false);
+    return ops;
+  }
+
+  public static Test suite() {
+    return new TestSuite(OPTICSTest.class);
+  }
+
+  public static void main(String[] args){
+    junit.textui.TestRunner.run(suite());
+  }
+}
