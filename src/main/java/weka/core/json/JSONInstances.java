21f1cd72b666605a1ee36c52eb6003c6930166e1
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2013-04-04 07:03:03 +0000

    Fixed two bugs related to date attributes - 1) was not reading the date format string correctly from the header section and 2) was attempting to parse dates as numeric values.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk/weka@9664 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- src/main/java/weka/core/json/JSONInstances.java
+++ src/main/java/weka/core/json/JSONInstances.java
@@ -119,7 +119,7 @@ public class JSONInstances {
       result = new Attribute(name, values);
     }
     else if (type.equals(Attribute.typeToString(Attribute.DATE))) {
-      dateformat = (String) att.getChild(TYPE).getValue("yyyy-MM-dd'T'HH:mm:ss");
+      dateformat = (String) att.getChild(DATEFORMAT).getValue("yyyy-MM-dd'T'HH:mm:ss");
       result     = new Attribute(name, dateformat);
     }
     else if (type.equals(Attribute.typeToString(Attribute.STRING))) {
@@ -176,7 +176,7 @@ public class JSONInstances {
 	  // unescape '?' labels 
 	  if (value.equals("'" + MISSING_VALUE + "'"))
 	    value = MISSING_VALUE;
-	  if (data.attribute(index).isNumeric()) {
+	  if (data.attribute(index).isNumeric() && !data.attribute(index).isDate()) {
 	    vals[index] = Double.parseDouble(value);
 	  }
 	  else if (data.attribute(index).isNominal()) {
