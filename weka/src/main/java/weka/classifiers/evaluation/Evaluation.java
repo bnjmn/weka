96c201050ffd9d608a02ab7c20be2dd3093cd23e
eibe <eibe@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2013-06-07 02:47:28 +0000

    Added command-line option to force training in batch mode for updateable classifiers.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@9744 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/src/main/java/weka/classifiers/evaluation/Evaluation.java
+++ weka/src/main/java/weka/classifiers/evaluation/Evaluation.java
@@ -1096,6 +1096,7 @@ public class Evaluation implements Summarizable, RevisionHandler, Serializable {
     String splitPercentageString = "";
     double splitPercentage = -1;
     boolean preserveOrder = false;
+    boolean forceBatchTraining = false; // set to true if updateable classifier should not be trained using updateClassifier()
     boolean trainSetPresent = false;
     boolean testSetPresent = false;
     boolean discardPredictions = false;
@@ -1320,6 +1321,7 @@ public class Evaluation implements Summarizable, RevisionHandler, Serializable {
       printSource = (sourceClass.length() != 0);
       thresholdFile = Utils.getOption("threshold-file", options);
       thresholdLabel = Utils.getOption("threshold-label", options);
+      forceBatchTraining = Utils.getFlag("force-batch-training", options);
 
       String classifications = Utils.getOption("classifications", options);
       String classificationsOld = Utils.getOption("p", options);
@@ -1443,7 +1445,7 @@ public class Evaluation implements Summarizable, RevisionHandler, Serializable {
     // Build the classifier if no object file provided
     if ((classifier instanceof UpdateableClassifier)
         && (testSetPresent || noCrossValidation) && (costMatrix == null)
-        && (trainSetPresent)) {
+        && (trainSetPresent) && !forceBatchTraining) {
       // Build classifier incrementally
       trainingEvaluation.setPriors(train);
       testingEvaluation.setPriors(train);
@@ -3924,6 +3926,8 @@ public class Evaluation implements Summarizable, RevisionHandler, Serializable {
         .append("\tSets number of folds for cross-validation (default: 10).\n");
     optionsText.append("-no-cv\n");
     optionsText.append("\tDo not perform any cross validation.\n");
+    optionsText.append("-force-batch-training\n");
+    optionsText.append("\tAlways train classifier in batch mode, never incrementally.\n");
     optionsText.append("-split-percentage <percentage>\n");
     optionsText
         .append("\tSets the percentage for the train/test set split, e.g., 66.\n");
