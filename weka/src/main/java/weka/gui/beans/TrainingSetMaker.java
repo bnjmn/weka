859e049ee115ce82bcd26860f5d7d9763706ffc4
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2013-06-10 08:26:33 +0000

    Now accepts testSet connections.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/branches/stable-3-6@9751 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/src/main/java/weka/gui/beans/TrainingSetMaker.java
+++ weka/src/main/java/weka/gui/beans/TrainingSetMaker.java
@@ -27,13 +27,12 @@ import java.util.Vector;
 
 /**
  * Bean that accepts a data sets and produces a training set
- *
+ * 
  * @author <a href="mailto:mhall@cs.waikato.ac.nz">Mark Hall</a>
  * @version $Revision$
  */
-public class TrainingSetMaker 
-  extends AbstractTrainingSetProducer 
-  implements DataSourceListener, EventConstraints, Serializable {
+public class TrainingSetMaker extends AbstractTrainingSetProducer implements
+    DataSourceListener, TestSetListener, EventConstraints, Serializable {
 
   /** for serialization */
   private static final long serialVersionUID = -6152577265471535786L;
@@ -41,10 +40,8 @@ public class TrainingSetMaker
   protected boolean m_receivedStopNotification = false;
 
   public TrainingSetMaker() {
-    m_visual.loadIcons(BeanVisual.ICON_PATH
-		       +"TrainingSetMaker.gif",
-		       BeanVisual.ICON_PATH
-		       +"TrainingSetMaker_animated.gif");
+    m_visual.loadIcons(BeanVisual.ICON_PATH + "TrainingSetMaker.gif",
+        BeanVisual.ICON_PATH + "TrainingSetMaker_animated.gif");
     m_visual.setText("TrainingSetMaker");
   }
 
@@ -53,6 +50,7 @@ public class TrainingSetMaker
    * 
    * @param name the name to use
    */
+  @Override
   public void setCustomName(String name) {
     m_visual.setText(name);
   }
@@ -62,13 +60,14 @@ public class TrainingSetMaker
    * 
    * @return the custom name (or the default name)
    */
+  @Override
   public String getCustomName() {
     return m_visual.getText();
   }
 
   /**
    * Global info for this bean
-   *
+   * 
    * @return a <code>String</code> value
    */
   public String globalInfo() {
@@ -77,13 +76,22 @@ public class TrainingSetMaker
 
   /**
    * Accept a data set
-   *
+   * 
    * @param e a <code>DataSetEvent</code> value
    */
+  @Override
   public void acceptDataSet(DataSetEvent e) {
     m_receivedStopNotification = false;
-    System.err.println(Messages.getInstance().getString("TrainingSetMaker_AcceptDataSet_Error_Text"));
     TrainingSetEvent tse = new TrainingSetEvent(this, e.getDataSet());
+    tse.m_setNumber = 1;
+    tse.m_maxSetNumber = 1;
+    notifyTrainingSetProduced(tse);
+  }
+
+  @Override
+  public void acceptTestSet(TestSetEvent e) {
+    m_receivedStopNotification = false;
+    TrainingSetEvent tse = new TrainingSetEvent(this, e.getTestSet());
     tse.m_setNumber = 1;
     tse.m_maxSetNumber = 1;
     notifyTrainingSetProduced(tse);
@@ -91,28 +99,41 @@ public class TrainingSetMaker
 
   /**
    * Inform training set listeners that a training set is available
-   *
+   * 
    * @param tse a <code>TrainingSetEvent</code> value
    */
   protected void notifyTrainingSetProduced(TrainingSetEvent tse) {
     Vector l;
     synchronized (this) {
-      l = (Vector)m_listeners.clone();
+      l = (Vector) m_listeners.clone();
     }
     if (l.size() > 0) {
-      for(int i = 0; i < l.size(); i++) {
+      for (int i = 0; i < l.size(); i++) {
         if (m_receivedStopNotification) {
           if (m_logger != null) {
-            m_logger.logMessage(Messages.getInstance().getString("TrainingSetMaker_NotifyTrainingSetProduced_LogMessage_Text_First")
-                +statusMessagePrefix() + Messages.getInstance().getString("TrainingSetMaker_NotifyTrainingSetProduced_LogMessage_Text_Second"));
-            m_logger.statusMessage(statusMessagePrefix()
-                + Messages.getInstance().getString("TrainingSetMaker_NotifyTrainingSetProduced_LogMessage_Text_Third"));
+            m_logger
+                .logMessage(Messages
+                    .getInstance()
+                    .getString(
+                        "TrainingSetMaker_NotifyTrainingSetProduced_LogMessage_Text_First")
+                    + statusMessagePrefix()
+                    + Messages
+                        .getInstance()
+                        .getString(
+                            "TrainingSetMaker_NotifyTrainingSetProduced_LogMessage_Text_Second"));
+            m_logger
+                .statusMessage(statusMessagePrefix()
+                    + Messages
+                        .getInstance()
+                        .getString(
+                            "TrainingSetMaker_NotifyTrainingSetProduced_LogMessage_Text_Third"));
           }
           m_receivedStopNotification = false;
           break;
         }
-	System.err.println(Messages.getInstance().getString("TrainingSetMaker_NotifyTrainingSetProduced_Error_Text"));
-	((TrainingSetListener)l.elementAt(i)).acceptTrainingSet(tse);
+        System.err.println(Messages.getInstance().getString(
+            "TrainingSetMaker_NotifyTrainingSetProduced_Error_Text"));
+        ((TrainingSetListener) l.elementAt(i)).acceptTrainingSet(tse);
       }
     }
   }
@@ -120,50 +141,51 @@ public class TrainingSetMaker
   /**
    * Stop any action
    */
+  @Override
   public void stop() {
     m_receivedStopNotification = true;
 
     // tell the listenee (upstream bean) to stop
     if (m_listenee instanceof BeanCommon) {
-      //      System.err.println("Listener is BeanCommon");
-      ((BeanCommon)m_listenee).stop();
+      // System.err.println("Listener is BeanCommon");
+      ((BeanCommon) m_listenee).stop();
     }
   }
-  
+
   /**
-   * Returns true if. at this time, the bean is busy with some
-   * (i.e. perhaps a worker thread is performing some calculation).
+   * Returns true if. at this time, the bean is busy with some (i.e. perhaps a
+   * worker thread is performing some calculation).
    * 
    * @return true if the bean is busy.
    */
+  @Override
   public boolean isBusy() {
     return false;
   }
 
   /**
-   * Returns true, if at the current time, the named event could
-   * be generated. Assumes that supplied event names are names of
-   * events that could be generated by this bean.
-   *
+   * Returns true, if at the current time, the named event could be generated.
+   * Assumes that supplied event names are names of events that could be
+   * generated by this bean.
+   * 
    * @param eventName the name of the event in question
-   * @return true if the named event could be generated at this point in
-   * time
+   * @return true if the named event could be generated at this point in time
    */
+  @Override
   public boolean eventGeneratable(String eventName) {
     if (m_listenee == null) {
       return false;
     }
 
     if (m_listenee instanceof EventConstraints) {
-      if (!((EventConstraints)m_listenee).eventGeneratable("dataSet")) {
-	return false;
+      if (!((EventConstraints) m_listenee).eventGeneratable("dataSet")) {
+        return false;
       }
     }
     return true;
   }
-  
+
   private String statusMessagePrefix() {
     return getCustomName() + "$" + hashCode() + "|";
   }
 }
-
