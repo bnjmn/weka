f8b39f8e0e59f5daad520f503c8039cfdac69bf5
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2013-12-03 07:15:50 +0000

    Cleaned up code.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk/packages/internal/distributedWekaHadoop@10349 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- src/main/java/weka/gui/beans/InteractiveTableModel.java
+++ src/main/java/weka/gui/beans/InteractiveTableModel.java
@@ -1,22 +1,21 @@
 /*
- *    This program is free software; you can redistribute it and/or modify
- *    it under the terms of the GNU General Public License as published by
- *    the Free Software Foundation; either version 2 of the License, or
- *    (at your option) any later version.
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
  *
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
  *
- *    You should have received a copy of the GNU General Public License
- *    along with this program; if not, write to the Free Software
- *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /*
  *    InteractiveTableModel.java
- *    Copyright (C) 2011 University of Waikato, Hamilton, New Zealand
+ *    Copyright (C) 2011-2013 University of Waikato, Hamilton, New Zealand
  */
 
 package weka.gui.beans;
@@ -40,11 +39,20 @@ public class InteractiveTableModel extends AbstractTableModel {
    */
   private static final long serialVersionUID = -5113873323690309667L;
 
-  public int m_hidden_index = 0;
+  /** Index of the hidden column */
+  public int m_hidden_index;
 
+  /** The names of the columns */
   protected String[] m_columnNames;
+
+  /** Holds the data */
   protected List<List<String>> m_dataVector;
 
+  /**
+   * Constructor
+   * 
+   * @param columnNames the names of the columns
+   */
   public InteractiveTableModel(String[] columnNames) {
     m_columnNames = columnNames;
     m_dataVector = new ArrayList<List<String>>();
@@ -65,7 +73,7 @@ public class InteractiveTableModel extends AbstractTableModel {
   }
 
   @Override
-  public Class getColumnClass(int column) {
+  public Class<?> getColumnClass(int column) {
     return String.class;
   }
 
@@ -100,9 +108,15 @@ public class InteractiveTableModel extends AbstractTableModel {
     return m_columnNames.length;
   }
 
+  /**
+   * Returns true if the model has an empty row
+   * 
+   * @return true if the model has an empty row
+   */
   public boolean hasEmptyRow() {
-    if (m_dataVector.size() == 0)
+    if (m_dataVector.size() == 0) {
       return false;
+    }
 
     List<String> dataRow = m_dataVector.get(m_dataVector.size() - 1);
     for (String s : dataRow) {
@@ -114,6 +128,9 @@ public class InteractiveTableModel extends AbstractTableModel {
     return true;
   }
 
+  /**
+   * Adds an empty row to the model
+   */
   public void addEmptyRow() {
     ArrayList<String> empty = new ArrayList<String>();
     for (int i = 0; i < m_columnNames.length; i++) {
