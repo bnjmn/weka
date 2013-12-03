bd409ab15ea4c6eee752654067ceca2941020ae0
eibe <eibe@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2013-12-03 02:37:17 +0000

    Code clean-up: generic type arguments, elimination of FastVector, clean-up of option handling.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@10338 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- packages/internal/dagging/src/main/java/weka/classifiers/meta/Dagging.java
+++ packages/internal/dagging/src/main/java/weka/classifiers/meta/Dagging.java
@@ -21,34 +21,39 @@
 
 package weka.classifiers.meta;
 
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Vector;
+
 import weka.classifiers.Classifier;
-import weka.classifiers.AbstractClassifier;
 import weka.classifiers.RandomizableSingleClassifierEnhancer;
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.Option;
 import weka.core.RevisionUtils;
 import weka.core.TechnicalInformation;
-import weka.core.TechnicalInformationHandler;
-import weka.core.Utils;
 import weka.core.TechnicalInformation.Field;
 import weka.core.TechnicalInformation.Type;
-
-import java.util.Enumeration;
-import java.util.Vector;
+import weka.core.TechnicalInformationHandler;
+import weka.core.Utils;
 
 /**
- <!-- globalinfo-start -->
- * This meta classifier creates a number of disjoint, stratified folds out of the data and feeds each chunk of data to a copy of the supplied base classifier. Predictions are made via majority vote, since all the generated base classifiers are put into the Vote meta classifier. <br/>
- * Useful for base classifiers that are quadratic or worse in time behavior, regarding number of instances in the training data. <br/>
+ * <!-- globalinfo-start --> This meta classifier creates a number of disjoint,
+ * stratified folds out of the data and feeds each chunk of data to a copy of
+ * the supplied base classifier. Predictions are made via majority vote, since
+ * all the generated base classifiers are put into the Vote meta classifier. <br/>
+ * Useful for base classifiers that are quadratic or worse in time behavior,
+ * regarding number of instances in the training data. <br/>
  * <br/>
  * For more information, see: <br/>
- * Ting, K. M., Witten, I. H.: Stacking Bagged and Dagged Models. In: Fourteenth international Conference on Machine Learning, San Francisco, CA, 367-375, 1997.
+ * Ting, K. M., Witten, I. H.: Stacking Bagged and Dagged Models. In: Fourteenth
+ * international Conference on Machine Learning, San Francisco, CA, 367-375,
+ * 1997.
  * <p/>
- <!-- globalinfo-end -->
+ * <!-- globalinfo-end -->
  * 
- <!-- technical-bibtex-start -->
- * BibTeX:
+ * <!-- technical-bibtex-start --> BibTeX:
+ * 
  * <pre>
  * &#64;inproceedings{Ting1997,
  *    address = {San Francisco, CA},
@@ -62,45 +67,56 @@ import java.util.Vector;
  * }
  * </pre>
  * <p/>
- <!-- technical-bibtex-end -->
- *
- <!-- options-start -->
- * Valid options are: <p/>
+ * <!-- technical-bibtex-end -->
  * 
- * <pre> -F &lt;folds&gt;
+ * <!-- options-start --> Valid options are:
+ * <p/>
+ * 
+ * <pre>
+ * -F &lt;folds&gt;
  *  The number of folds for splitting the training set into
  *  smaller chunks for the base classifier.
- *  (default 10)</pre>
+ *  (default 10)
+ * </pre>
  * 
- * <pre> -verbose
+ * <pre>
+ * -verbose
  *  Whether to print some more information during building the
  *  classifier.
- *  (default is off)</pre>
+ *  (default is off)
+ * </pre>
  * 
- * <pre> -S &lt;num&gt;
+ * <pre>
+ * -S &lt;num&gt;
  *  Random number seed.
- *  (default 1)</pre>
+ *  (default 1)
+ * </pre>
  * 
- * <pre> -D
+ * <pre>
+ * -D
  *  If set, classifier is run in debug mode and
- *  may output additional info to the console</pre>
+ *  may output additional info to the console
+ * </pre>
  * 
- * <pre> -W
+ * <pre>
+ * -W
  *  Full name of base classifier.
- *  (default: weka.classifiers.trees.DecisionStump)</pre>
- <!-- options-end -->
- *
- * Options after -- are passed to the designated classifier.<p/>
- *
+ *  (default: weka.classifiers.trees.DecisionStump)
+ * </pre>
+ * 
+ * <!-- options-end -->
+ * 
+ * Options after -- are passed to the designated classifier.
+ * <p/>
+ * 
  * @author Bernhard Pfahringer (bernhard at cs dot waikato dot ac dot nz)
  * @author FracPete (fracpete at waikato dot ac dot nz)
  * @version $Revision$
- * @see       Vote
+ * @see Vote
  */
-public class Dagging
-  extends RandomizableSingleClassifierEnhancer
-  implements TechnicalInformationHandler {
-  
+public class Dagging extends RandomizableSingleClassifierEnhancer implements
+  TechnicalInformationHandler {
+
   /** for serialization */
   static final long serialVersionUID = 4560165876570074309L;
 
@@ -112,48 +128,50 @@ public class Dagging
 
   /** whether to output some progress information during building */
   protected boolean m_Verbose = false;
-    
+
   /**
    * Returns a string describing classifier
-   * @return a description suitable for
-   * displaying in the explorer/experimenter gui
+   * 
+   * @return a description suitable for displaying in the explorer/experimenter
+   *         gui
    */
   public String globalInfo() {
-    return 
-     "This meta classifier creates a number of disjoint, stratified folds out "
-     + "of the data and feeds each chunk of data to a copy of the supplied "
-     + "base classifier. Predictions are made via averaging, since all the "
-     + "generated base classifiers are put into the Vote meta classifier. \n"
-     + "Useful for base classifiers that are quadratic or worse in time "
-     + "behavior, regarding number of instances in the training data. \n"
-     + "\n"
-     + "For more information, see: \n"
-     + getTechnicalInformation().toString();
+    return "This meta classifier creates a number of disjoint, stratified folds out "
+      + "of the data and feeds each chunk of data to a copy of the supplied "
+      + "base classifier. Predictions are made via averaging, since all the "
+      + "generated base classifiers are put into the Vote meta classifier. \n"
+      + "Useful for base classifiers that are quadratic or worse in time "
+      + "behavior, regarding number of instances in the training data. \n"
+      + "\n"
+      + "For more information, see: \n"
+      + getTechnicalInformation().toString();
   }
 
   /**
-   * Returns an instance of a TechnicalInformation object, containing 
-   * detailed information about the technical background of this class,
-   * e.g., paper reference or book this class is based on.
+   * Returns an instance of a TechnicalInformation object, containing detailed
+   * information about the technical background of this class, e.g., paper
+   * reference or book this class is based on.
    * 
    * @return the technical information about this class
    */
+  @Override
   public TechnicalInformation getTechnicalInformation() {
-    TechnicalInformation 	result;
-    
+    TechnicalInformation result;
+
     result = new TechnicalInformation(Type.INPROCEEDINGS);
     result.setValue(Field.AUTHOR, "Ting, K. M. and Witten, I. H.");
     result.setValue(Field.TITLE, "Stacking Bagged and Dagged Models");
-    result.setValue(Field.BOOKTITLE, "Fourteenth international Conference on Machine Learning");
+    result.setValue(Field.BOOKTITLE,
+      "Fourteenth international Conference on Machine Learning");
     result.setValue(Field.EDITOR, "D. H. Fisher");
     result.setValue(Field.YEAR, "1997");
     result.setValue(Field.PAGES, "367-375");
     result.setValue(Field.PUBLISHER, "Morgan Kaufmann Publishers");
     result.setValue(Field.ADDRESS, "San Francisco, CA");
-    
+
     return result;
   }
-    
+
   /**
    * Constructor.
    */
@@ -166,165 +184,177 @@ public class Dagging
    * 
    * @return the default classifier classname
    */
+  @Override
   protected String defaultClassifierString() {
     return weka.classifiers.trees.DecisionStump.class.getName();
   }
 
   /**
    * Returns an enumeration describing the available options.
-   *
+   * 
    * @return an enumeration of all the available options.
    */
-  public Enumeration listOptions() {
-    Vector result = new Vector();
-    
+  @Override
+  public Enumeration<Option> listOptions() {
+    Vector<Option> result = new Vector<Option>();
+
     result.addElement(new Option(
-        "\tThe number of folds for splitting the training set into\n"
-        + "\tsmaller chunks for the base classifier.\n"
-        + "\t(default 10)",
-        "F", 1, "-F <folds>"));
-    
+      "\tThe number of folds for splitting the training set into\n"
+        + "\tsmaller chunks for the base classifier.\n" + "\t(default 10)",
+      "F", 1, "-F <folds>"));
+
     result.addElement(new Option(
-        "\tWhether to print some more information during building the\n"
-        + "\tclassifier.\n"
-        + "\t(default is off)",
-        "verbose", 0, "-verbose"));
-    
-    Enumeration en = super.listOptions();
-    while (en.hasMoreElements())
-      result.addElement(en.nextElement());
-      
+      "\tWhether to print some more information during building the\n"
+        + "\tclassifier.\n" + "\t(default is off)", "verbose", 0, "-verbose"));
+
+    result.addAll(Collections.list(super.listOptions()));
+
     return result.elements();
   }
 
-
   /**
-   * Parses a given list of options. <p/>
-   *
-   <!-- options-start -->
-   * Valid options are: <p/>
+   * Parses a given list of options.
+   * <p/>
    * 
-   * <pre> -F &lt;folds&gt;
+   * <!-- options-start --> Valid options are:
+   * <p/>
+   * 
+   * <pre>
+   * -F &lt;folds&gt;
    *  The number of folds for splitting the training set into
    *  smaller chunks for the base classifier.
-   *  (default 10)</pre>
+   *  (default 10)
+   * </pre>
    * 
-   * <pre> -verbose
+   * <pre>
+   * -verbose
    *  Whether to print some more information during building the
    *  classifier.
-   *  (default is off)</pre>
+   *  (default is off)
+   * </pre>
    * 
-   * <pre> -S &lt;num&gt;
+   * <pre>
+   * -S &lt;num&gt;
    *  Random number seed.
-   *  (default 1)</pre>
+   *  (default 1)
+   * </pre>
    * 
-   * <pre> -D
+   * <pre>
+   * -D
    *  If set, classifier is run in debug mode and
-   *  may output additional info to the console</pre>
+   *  may output additional info to the console
+   * </pre>
    * 
-   * <pre> -W
+   * <pre>
+   * -W
    *  Full name of base classifier.
-   *  (default: weka.classifiers.trees.DecisionStump)</pre>
-   <!-- options-end -->
-   *
-   * Options after -- are passed to the designated classifier.<p>
-   *
+   *  (default: weka.classifiers.trees.DecisionStump)
+   * </pre>
+   * 
+   * <!-- options-end -->
+   * 
+   * Options after -- are passed to the designated classifier.
+   * <p>
+   * 
    * @param options the list of options as an array of strings
    * @throws Exception if an option is not supported
    */
+  @Override
   public void setOptions(String[] options) throws Exception {
-    String        tmpStr;
+    String tmpStr;
 
     tmpStr = Utils.getOption('F', options);
-    if (tmpStr.length() != 0)
+    if (tmpStr.length() != 0) {
       setNumFolds(Integer.parseInt(tmpStr));
-    else
+    } else {
       setNumFolds(10);
-    
+    }
+
     setVerbose(Utils.getFlag("verbose", options));
-    
+
     super.setOptions(options);
+
+    Utils.checkForRemainingOptions(options);
   }
 
   /**
    * Gets the current settings of the Classifier.
-   *
+   * 
    * @return an array of strings suitable for passing to setOptions
    */
+  @Override
   public String[] getOptions() {
-    Vector        result;
-    String[]      options;
-    int           i;
-    
-    result  = new Vector();
+
+    Vector<String> result = new Vector<String>();
 
     result.add("-F");
     result.add("" + getNumFolds());
-    
-    if (getVerbose())
+
+    if (getVerbose()) {
       result.add("-verbose");
-    
-    options = super.getOptions();
-    for (i = 0; i < options.length; i++)
-      result.add(options[i]);
-    
-    return (String[]) result.toArray(new String[result.size()]);
+    }
+
+    Collections.addAll(result, super.getOptions());
+
+    return result.toArray(new String[result.size()]);
   }
 
   /**
    * Gets the number of folds to use for splitting the training set.
-   *
+   * 
    * @return the number of folds
    */
   public int getNumFolds() {
     return m_NumFolds;
   }
-  
+
   /**
    * Sets the number of folds to use for splitting the training set.
-   *
-   * @param value     the new number of folds
+   * 
+   * @param value the new number of folds
    */
   public void setNumFolds(int value) {
-    if (value > 0)
+    if (value > 0) {
       m_NumFolds = value;
-    else
-      System.out.println(
-          "At least 1 fold is necessary (provided: " + value + ")!");
+    } else {
+      System.out.println("At least 1 fold is necessary (provided: " + value
+        + ")!");
+    }
   }
 
   /**
    * Returns the tip text for this property
-   *
-   * @return tip text for this property suitable for
-   *         displaying in the explorer/experimenter gui
+   * 
+   * @return tip text for this property suitable for displaying in the
+   *         explorer/experimenter gui
    */
   public String numFoldsTipText() {
     return "The number of folds to use for splitting the training set into smaller chunks for the base classifier.";
   }
-  
+
   /**
    * Set the verbose state.
-   *
+   * 
    * @param value the verbose state
    */
   public void setVerbose(boolean value) {
     m_Verbose = value;
   }
-  
+
   /**
    * Gets the verbose state
-   *
+   * 
    * @return the verbose state
    */
   public boolean getVerbose() {
     return m_Verbose;
   }
-  
+
   /**
    * Returns the tip text for this property
-   * @return tip text for this property suitable for
-   * displaying in the explorer/experimenter gui
+   * 
+   * @return tip text for this property suitable for displaying in the
+   *         explorer/experimenter gui
    */
   public String verboseTipText() {
     return "Whether to ouput some additional information during building.";
@@ -332,31 +362,33 @@ public class Dagging
 
   /**
    * Bagging method.
-   *
-   * @param data the training data to be used for generating the
-   * bagged classifier.
+   * 
+   * @param data the training data to be used for generating the bagged
+   *          classifier.
    * @throws Exception if the classifier could not be built successfully
    */
+  @Override
   public void buildClassifier(Instances data) throws Exception {
-    Classifier[]        base;
-    int                 i;
-    int                 n;
-    int                 fromIndex;
-    int                 toIndex;
-    Instances           train;
-    double              chunkSize;
-    
+    Classifier[] base;
+    int i;
+    // int n; NOT USED
+    // int fromIndex; NOT USED
+    // int toIndex; NOT USED
+    Instances train;
+    // double chunkSize; NOT USED
+
     // can classifier handle the data?
     getCapabilities().testWithFail(data);
 
     // remove instances with missing class
     data = new Instances(data);
     data.deleteWithMissingClass();
-    
-    m_Vote    = new Vote();
-    base      = new Classifier[getNumFolds()];
-    chunkSize = (double) data.numInstances() / (double) getNumFolds();
-    
+
+    m_Vote = new Vote();
+    base = new Classifier[getNumFolds()];
+    // chunkSize = (double) data.numInstances() / (double) getNumFolds(); NOT
+    // USED
+
     // stratify data
     if (getNumFolds() > 1) {
       data.randomize(data.getRandomNumberGenerator(getSeed()));
@@ -370,63 +402,67 @@ public class Dagging
       // generate training data
       if (getNumFolds() > 1) {
         // some progress information
-        if (getVerbose())
+        if (getVerbose()) {
           System.out.print(".");
-        
-        train     = data.testCV(getNumFolds(), i);
-      }
-      else {
+        }
+
+        train = data.testCV(getNumFolds(), i);
+      } else {
         train = data;
       }
 
       // train classifier
       base[i].buildClassifier(train);
     }
-    
+
     // init vote
     m_Vote.setClassifiers(base);
-    
-    if (getVerbose())
+
+    if (getVerbose()) {
       System.out.println();
+    }
   }
 
   /**
-   * Calculates the class membership probabilities for the given test
-   * instance.
-   *
+   * Calculates the class membership probabilities for the given test instance.
+   * 
    * @param instance the instance to be classified
    * @return preedicted class probability distribution
-   * @throws Exception if distribution can't be computed successfully 
+   * @throws Exception if distribution can't be computed successfully
    */
+  @Override
   public double[] distributionForInstance(Instance instance) throws Exception {
     return m_Vote.distributionForInstance(instance);
   }
 
   /**
    * Returns description of the classifier.
-   *
+   * 
    * @return description of the classifier as a string
    */
+  @Override
   public String toString() {
-    if (m_Vote == null)
-      return this.getClass().getName().replaceAll(".*\\.", "") 
-             + ": No model built yet.";
-    else
+    if (m_Vote == null) {
+      return this.getClass().getName().replaceAll(".*\\.", "")
+        + ": No model built yet.";
+    } else {
       return m_Vote.toString();
+    }
   }
-  
+
   /**
    * Returns the revision string.
    * 
-   * @return		the revision
+   * @return the revision
    */
+  @Override
   public String getRevision() {
     return RevisionUtils.extract("$Revision$");
   }
 
   /**
    * Main method for testing this class.
-   *
+   * 
    * @param args the options
    */
   public static void main(String[] args) {
