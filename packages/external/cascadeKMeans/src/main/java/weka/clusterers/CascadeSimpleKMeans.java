bfb54a334c1bdae9044c51ab3cd1319734e8cab4
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2014-02-21 02:24:03 +0000

    Fixed a small bug introduced with the last change.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@10457 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- packages/external/cascadeKMeans/src/main/java/weka/clusterers/CascadeSimpleKMeans.java
+++ packages/external/cascadeKMeans/src/main/java/weka/clusterers/CascadeSimpleKMeans.java
@@ -148,7 +148,9 @@ public class CascadeSimpleKMeans extends RandomizableClusterer implements Cluste
     kMeans.setDistanceFunction(distanceFunction);
     kMeans.setMaxIterations(maxIterations);
     //    kMeans.setInitializeUsingKMeansPlusPlusMethod(initializeWithKMeansPlusPlus);
-    kMeans.setInitializationMethod(new weka.core.SelectedTag(SimpleKMeans.KMEANS_PLUS_PLUS, SimpleKMeans.TAGS_SELECTION));
+    if (initializeWithKMeansPlusPlus) {
+      kMeans.setInitializationMethod(new weka.core.SelectedTag(SimpleKMeans.KMEANS_PLUS_PLUS, SimpleKMeans.TAGS_SELECTION));
+    }
 
     /**
      * step 1: iterate over all restarts and possible k values, record CH-scores
