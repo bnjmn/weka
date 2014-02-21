6f44cc843a066e96f90dc01de6c69b229276f98e
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2014-02-21 01:53:51 +0000

    Updated for the changes to SimpleKMeans
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@10456 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- packages/external/cascadeKMeans/src/main/java/weka/clusterers/CascadeSimpleKMeans.java
+++ packages/external/cascadeKMeans/src/main/java/weka/clusterers/CascadeSimpleKMeans.java
@@ -147,7 +147,8 @@ public class CascadeSimpleKMeans extends RandomizableClusterer implements Cluste
 
     kMeans.setDistanceFunction(distanceFunction);
     kMeans.setMaxIterations(maxIterations);
-    kMeans.setInitializeUsingKMeansPlusPlusMethod(initializeWithKMeansPlusPlus);
+    //    kMeans.setInitializeUsingKMeansPlusPlusMethod(initializeWithKMeansPlusPlus);
+    kMeans.setInitializationMethod(new weka.core.SelectedTag(SimpleKMeans.KMEANS_PLUS_PLUS, SimpleKMeans.TAGS_SELECTION));
 
     /**
      * step 1: iterate over all restarts and possible k values, record CH-scores
