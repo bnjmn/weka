2a50634868fdf34ac72aa2042bb8ef2999a365a8
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2014-05-06 08:14:35 +0000

    Switched to a split pane to separate the result list from the text output area. Removed the trimming of result list entry names.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@10815 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/src/main/java/weka/gui/beans/TextViewer.java
+++ weka/src/main/java/weka/gui/beans/TextViewer.java
@@ -49,6 +49,7 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
 import javax.swing.JTextArea;
 
 import weka.gui.Logger;
@@ -104,7 +105,8 @@ public class TextViewer extends JPanel implements TextListener,
   /**
    * Objects listening for text events
    */
-  private final Vector<TextListener> m_textListeners = new Vector<TextListener>();
+  private final Vector<TextListener> m_textListeners =
+    new Vector<TextListener>();
 
   public TextViewer() {
     java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment();
@@ -119,8 +121,9 @@ public class TextViewer extends JPanel implements TextListener,
     setUpResultHistory();
     removeAll();
     if (m_visual == null) {
-      m_visual = new BeanVisual("TextViewer", BeanVisual.ICON_PATH
-        + "DefaultText.gif", BeanVisual.ICON_PATH + "DefaultText_animated.gif");
+      m_visual =
+        new BeanVisual("TextViewer", BeanVisual.ICON_PATH + "DefaultText.gif",
+          BeanVisual.ICON_PATH + "DefaultText_animated.gif");
     }
     setLayout(new BorderLayout());
     add(m_visual, BorderLayout.CENTER);
@@ -262,8 +265,9 @@ public class TextViewer extends JPanel implements TextListener,
    */
   @Override
   public synchronized void acceptDataSet(DataSetEvent e) {
-    TextEvent nt = new TextEvent(e.getSource(), e.getDataSet().toString(), e
-      .getDataSet().relationName());
+    TextEvent nt =
+      new TextEvent(e.getSource(), e.getDataSet().toString(), e.getDataSet()
+        .relationName());
     acceptText(nt);
   }
 
@@ -274,8 +278,9 @@ public class TextViewer extends JPanel implements TextListener,
    */
   @Override
   public synchronized void acceptTrainingSet(TrainingSetEvent e) {
-    TextEvent nt = new TextEvent(e.getSource(), e.getTrainingSet().toString(),
-      e.getTrainingSet().relationName());
+    TextEvent nt =
+      new TextEvent(e.getSource(), e.getTrainingSet().toString(), e
+        .getTrainingSet().relationName());
     acceptText(nt);
   }
 
@@ -286,8 +291,9 @@ public class TextViewer extends JPanel implements TextListener,
    */
   @Override
   public synchronized void acceptTestSet(TestSetEvent e) {
-    TextEvent nt = new TextEvent(e.getSource(), e.getTestSet().toString(), e
-      .getTestSet().relationName());
+    TextEvent nt =
+      new TextEvent(e.getSource(), e.getTestSet().toString(), e.getTestSet()
+        .relationName());
     acceptText(nt);
   }
 
@@ -307,10 +313,6 @@ public class TextViewer extends JPanel implements TextListener,
     // m_outText.setText(m_resultsString.toString());
     String name = (new SimpleDateFormat("HH:mm:ss - ")).format(new Date());
     name += e.getTextTitle();
-    // System.err.println(name);
-    if (name.length() > 30) {
-      name = name.substring(0, 30);
-    }
 
     if (m_outText != null) {
       // see if there is an entry with this name already in the list -
@@ -401,8 +403,12 @@ public class TextViewer extends JPanel implements TextListener,
       m_resultsFrame.getContentPane().setLayout(new BorderLayout());
       final JScrollPane js = new JScrollPane(m_outText);
       js.setBorder(BorderFactory.createTitledBorder("Text"));
-      m_resultsFrame.getContentPane().add(js, BorderLayout.CENTER);
-      m_resultsFrame.getContentPane().add(m_history, BorderLayout.WEST);
+
+      JSplitPane p2 =
+        new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, m_history, js);
+      m_resultsFrame.getContentPane().add(p2, BorderLayout.CENTER);
+      // m_resultsFrame.getContentPane().add(js, BorderLayout.CENTER);
+      // m_resultsFrame.getContentPane().add(m_history, BorderLayout.WEST);
       m_resultsFrame.addWindowListener(new java.awt.event.WindowAdapter() {
         @Override
         public void windowClosing(java.awt.event.WindowEvent e) {
