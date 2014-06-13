6d020cd1e42d4bdc9a5a9c0aacfd4b038b21c345
eibe <eibe@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2014-06-13 06:17:26 +0000

    Made some small changes to the IteartiveClassifier inteface, which is not currently used in WEKA.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@10861 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/src/main/java/weka/classifiers/IterativeClassifier.java
+++ weka/src/main/java/weka/classifiers/IterativeClassifier.java
@@ -32,23 +32,23 @@ import weka.core.Instances;
  * @version $Revision$
  */
 
-public interface IterativeClassifier {
+public interface IterativeClassifier extends Classifier {
 
   /**
-   * Inits an iterative classifier.
+   * Initializes an iterative classifier.
    *
    * @param instances the instances to be used in induction
    * @exception Exception if the model cannot be initialized
    */
-  void initClassifier(Instances instances) throws Exception;
+  void initializeClassifier(Instances instances) throws Exception;
 
   /**
    * Performs one iteration.
    *
-   * @param iteration the index of the current iteration (0-based)
-   * @exception Exception if this iteration fails
+   * @return false if no further iterations could be performed, true otherwise
+   * @exception Exception if this iteration fails for unexpected reasons
    */
-  void next(int iteration) throws Exception;
+  boolean next() throws Exception;
 
   /**
    * Signal end of iterating, useful for any house-keeping/cleanup
@@ -56,13 +56,4 @@ public interface IterativeClassifier {
    * @exception Exception if cleanup fails
    */
   void done() throws Exception;
-
-  /**
-    * Performs a deep copy of the classifier, and a reference copy
-    * of the training instances (or a deep copy if required).
-    *
-    * @return a clone of the classifier
-    */
-  Object clone() throws CloneNotSupportedException;
-
 }
