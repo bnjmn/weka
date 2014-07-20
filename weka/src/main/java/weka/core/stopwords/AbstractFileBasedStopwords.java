35028652618d08e130a19a69975a7123df899cd2
fracpete <fracpete@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2014-07-20 22:07:37 +0000

    added GPL preamble
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@10982 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/src/main/java/weka/core/stopwords/AbstractFileBasedStopwords.java
+++ weka/src/main/java/weka/core/stopwords/AbstractFileBasedStopwords.java
@@ -1,12 +1,24 @@
 /*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
  * AbstractFileBasedStopwords.java
  * Copyright (C) 2014 University of Waikato, Hamilton, New Zealand
  */
 
 package weka.core.stopwords;
-
-import weka.core.Option;
-import weka.core.Utils;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -16,6 +28,9 @@ import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.Vector;
+
+import weka.core.Option;
+import weka.core.Utils;
 
 /**
  * Ancestor for file-based stopword schemes.
