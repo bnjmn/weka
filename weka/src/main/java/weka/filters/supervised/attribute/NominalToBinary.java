e2f996fb095d7f0e6a5829c248789f60d57210a5
eibe <eibe@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2015-01-13 20:56:18 +0000

    Now correctly declares that it can deal with missing class values. This was not declared as a capability previously.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@11470 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/src/main/java/weka/filters/supervised/attribute/NominalToBinary.java
+++ weka/src/main/java/weka/filters/supervised/attribute/NominalToBinary.java
@@ -172,6 +172,7 @@ public class NominalToBinary extends Filter implements SupervisedFilter,
     result.enable(Capability.NUMERIC_CLASS);
     result.enable(Capability.DATE_CLASS);
     result.enable(Capability.NOMINAL_CLASS);
+    result.enable(Capability.MISSING_CLASS_VALUES);
 
     return result;
   }
