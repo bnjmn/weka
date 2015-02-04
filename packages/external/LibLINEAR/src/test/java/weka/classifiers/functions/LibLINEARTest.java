d2836c9ed3dd44d7a5e65d4aaad08adae8934122
eibe <eibe@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2015-02-04 04:45:52 +0000

    Updated to 1.95 version of the Java port of LibLINEAR. Enabled application to regression problems.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@11543 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- packages/external/LibLINEAR/src/test/java/weka/classifiers/functions/LibLINEARTest.java
+++ packages/external/LibLINEAR/src/test/java/weka/classifiers/functions/LibLINEARTest.java
@@ -1,16 +1,17 @@
 /*
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
  *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
  *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
 /*
@@ -19,36 +20,34 @@
 
 package weka.classifiers.functions;
 
+import junit.framework.Test;
+import junit.framework.TestSuite;
 import weka.classifiers.AbstractClassifierTest;
 import weka.classifiers.Classifier;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
 
 /**
  * Tests LibLINEAR. Run from the command line with: <p/>
  * java weka.classifiers.functions.LibLINEARTest
  *
- * @author Mark Hall (mhall{[at]}pentaho{[dot]}com)
- * @version $Revision$
+ * @author Benedikt Waldvogel
  */
-public class LibLINEARTest 
-  extends AbstractClassifierTest {
+public class LibLINEARTest extends AbstractClassifierTest {
 
-  public LibLINEARTest(String name) { 
-    super(name);  
-  }
+    public LibLINEARTest( String name ) {
+        super(name);
+    }
 
-  /** Creates a default SMO */
-  public Classifier getClassifier() {
-    return new LibLINEAR();
-  }
+    /** Creates a default SMO */
+    public Classifier getClassifier() {
+        return new LibLINEAR();
+    }
 
-  public static Test suite() {
-    return new TestSuite(LibLINEARTest.class);
-  }
+    public static Test suite() {
+        return new TestSuite(LibLINEARTest.class);
+    }
 
-  public static void main(String[] args){
-    junit.textui.TestRunner.run(suite());
-  }
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
 }
