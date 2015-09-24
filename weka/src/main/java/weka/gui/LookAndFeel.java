493dd6fe57f72381dd3b41b5b3ae4a1f968fd27c
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2015-09-24 04:34:08 +0000

    Now maps meta key to ctrl key under OS X when meta A, C, X, or V are typed so that cut, copy, select-all and paste work with non-osx-native look-and-feels
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@12021 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/src/main/java/weka/gui/LookAndFeel.java
+++ weka/src/main/java/weka/gui/LookAndFeel.java
@@ -19,9 +19,11 @@
  *
  */
 
-
 package weka.gui;
 
+import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
+import java.awt.event.KeyEvent;
 import java.util.Properties;
 
 import javax.swing.JOptionPane;
@@ -32,7 +34,7 @@ import weka.core.Utils;
 
 /**
  * A little helper class for setting the Look and Feel of the user interface.
- * Was necessary, since Java 1.5 sometimes crashed the WEKA GUI (e.g. under 
+ * Was necessary, since Java 1.5 sometimes crashed the WEKA GUI (e.g. under
  * Linux/Gnome). Running this class from the commandline will print all
  * available Look and Feel themes.
  *
@@ -40,7 +42,7 @@ import weka.core.Utils;
  * @version $Revision$
  */
 public class LookAndFeel {
-  
+
   /** The name of the properties file */
   public static String PROPERTY_FILE = "weka/gui/LookAndFeel.props";
 
@@ -50,34 +52,52 @@ public class LookAndFeel {
   static {
     try {
       LOOKANDFEEL_PROPERTIES = Utils.readProperties(PROPERTY_FILE);
-    } 
-    catch (Exception ex) {
+    } catch (Exception ex) {
       JOptionPane.showMessageDialog(null,
-       "LookAndFeel: Could not read a LookAndFeel configuration file.\n"
-       +"An example file is included in the Weka distribution.\n"
-       +"This file should be named \"" + PROPERTY_FILE + "\"  and\n"
-       +"should be placed either in your user home (which is set\n"
-       +"to \"" + System.getProperties().getProperty("user.home") + "\")\n"
-       +"or the directory that java was started from\n",
-       "LookAndFeel",
-       JOptionPane.ERROR_MESSAGE);
+        "LookAndFeel: Could not read a LookAndFeel configuration file.\n"
+          + "An example file is included in the Weka distribution.\n"
+          + "This file should be named \"" + PROPERTY_FILE + "\"  and\n"
+          + "should be placed either in your user home (which is set\n"
+          + "to \"" + System.getProperties().getProperty("user.home") + "\")\n"
+          + "or the directory that java was started from\n", "LookAndFeel",
+        JOptionPane.ERROR_MESSAGE);
     }
   }
 
   /**
    * sets the look and feel to the specified class
    * 
-   * @param classname      the look and feel to use
-   * @return               whether setting was successful   
+   * @param classname the look and feel to use
+   * @return whether setting was successful
    */
   public static boolean setLookAndFeel(String classname) {
-    boolean          result;
+    boolean result;
 
     try {
       UIManager.setLookAndFeel(classname);
       result = true;
-    }
-    catch (Exception e) {
+
+      if (System.getProperty("os.name").toLowerCase().contains("mac os x")
+        && !classname.contains("com.apple.laf")) {
+        KeyboardFocusManager.getCurrentKeyboardFocusManager()
+          .addKeyEventDispatcher(new KeyEventDispatcher() {
+            @Override
+            public boolean dispatchKeyEvent(KeyEvent e) {
+              if (!e.isConsumed()) {
+                if (e.isMetaDown()) {
+                  if (e.getKeyCode() == KeyEvent.VK_V
+                    || e.getKeyCode() == KeyEvent.VK_A
+                    || e.getKeyCode() == KeyEvent.VK_C
+                    || e.getKeyCode() == KeyEvent.VK_X) {
+                    e.setModifiers(KeyEvent.CTRL_DOWN_MASK);
+                  }
+                }
+              }
+              return false;
+            }
+          });
+      }
+    } catch (Exception e) {
       e.printStackTrace();
       result = false;
     }
@@ -86,24 +106,23 @@ public class LookAndFeel {
   }
 
   /**
-   * sets the look and feel to the one in the props-file or if not set the 
+   * sets the look and feel to the one in the props-file or if not set the
    * default one of the system
    * 
-   * @return               whether setting was successful
+   * @return whether setting was successful
    */
   public static boolean setLookAndFeel() {
-    String           classname;
+    String classname;
 
     classname = LOOKANDFEEL_PROPERTIES.getProperty("Theme", "");
     if (classname.equals("")) {
-      // Java 1.5 crashes under Gnome if one sets it to the GTKLookAndFeel 
-      // theme, hence we don't set any theme by default if we're on a Linux 
+      // Java 1.5 crashes under Gnome if one sets it to the GTKLookAndFeel
+      // theme, hence we don't set any theme by default if we're on a Linux
       // box.
       if (System.getProperty("os.name").equalsIgnoreCase("linux")) {
-	return true;
-      }
-      else {
-	classname = getSystemLookAndFeel();
+        return true;
+      } else {
+        classname = getSystemLookAndFeel();
       }
     }
 
@@ -113,7 +132,7 @@ public class LookAndFeel {
   /**
    * returns the system LnF classname
    * 
-   * @return               the name of the System LnF class
+   * @return the name of the System LnF class
    */
   public static String getSystemLookAndFeel() {
     return UIManager.getSystemLookAndFeelClassName();
@@ -122,35 +141,36 @@ public class LookAndFeel {
   /**
    * returns an array with the classnames of all the installed LnFs
    * 
-   * @return               the installed LnFs
+   * @return the installed LnFs
    */
   public static String[] getInstalledLookAndFeels() {
-    String[]               result;
-    LookAndFeelInfo[]      laf;
-    int                    i;
+    String[] result;
+    LookAndFeelInfo[] laf;
+    int i;
 
-    laf    = UIManager.getInstalledLookAndFeels();
+    laf = UIManager.getInstalledLookAndFeels();
     result = new String[laf.length];
     for (i = 0; i < laf.length; i++)
       result[i] = laf[i].getClassName();
 
     return result;
   }
-  
+
   /**
    * prints all the available LnFs to stdout
    * 
-   * @param args	the commandline options
+   * @param args the commandline options
    */
   public static void main(String[] args) {
-    String[]	list;
-    int		i;
-    
+    String[] list;
+    int i;
+
     System.out.println("\nInstalled Look and Feel themes:");
     list = getInstalledLookAndFeels();
     for (i = 0; i < list.length; i++)
-      System.out.println((i+1) + ". " + list[i]);
+      System.out.println((i + 1) + ". " + list[i]);
 
-    System.out.println("\nNote: a theme can be set in '" + PROPERTY_FILE + "'.");
+    System.out
+      .println("\nNote: a theme can be set in '" + PROPERTY_FILE + "'.");
   }
 }
