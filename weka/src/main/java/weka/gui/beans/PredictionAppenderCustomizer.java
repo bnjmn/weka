3e73df06d4b9c77afa37aea1ad78c1bbd062981c
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2015-11-29 20:00:21 +0000

    Clicking cancel always set the append probabilities property to false - fixed.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@12107 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/src/main/java/weka/gui/beans/PredictionAppenderCustomizer.java
+++ weka/src/main/java/weka/gui/beans/PredictionAppenderCustomizer.java
@@ -21,6 +21,10 @@
 
 package weka.gui.beans;
 
+import weka.gui.PropertySheetPanel;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.Window;
@@ -28,11 +32,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
-
-import javax.swing.JButton;
-import javax.swing.JPanel;
-
-import weka.gui.PropertySheetPanel;
 
 /**
  * GUI Customizer for the prediction appender bean
@@ -107,6 +106,7 @@ public class PredictionAppenderCustomizer
   public void setObject(Object object) {
     m_appender = ((PredictionAppender)object);
     m_paEditor.setTarget(m_appender);
+    m_appendProbsBackup = m_appender.getAppendPredictedProbabilities();
   }
 
   /**
@@ -141,6 +141,5 @@ public class PredictionAppenderCustomizer
   public void customizerClosing() {
     // restore the backup value
     m_appender.setAppendPredictedProbabilities(m_appendProbsBackup);
-    
   }
 }
