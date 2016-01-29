0b32cdfc19d39e4a3602cb9bb6410307c4ae933f
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2016-01-29 06:28:55 +0000

    Now uses the @KFIgnore and @GPCIgnore annotations
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@12413 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- packages/internal/distributedWekaBase/src/main/java/weka/classifiers/meta/BatchPredictorVote.java
+++ packages/internal/distributedWekaBase/src/main/java/weka/classifiers/meta/BatchPredictorVote.java
@@ -25,6 +25,8 @@ import weka.core.BatchPredictor;
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.Utils;
+import weka.gui.GPCIgnore;
+import weka.gui.beans.KFIgnore;
 
 /**
  * Class that extends Vote in order to implement BatchPredictor. This is of
@@ -34,6 +36,8 @@ import weka.core.Utils;
  * @author Mark Hall (mhall{[at]}pentaho{[dot]}com)
  * @version $Revision: $
  */
+@KFIgnore
+@GPCIgnore
 public class BatchPredictorVote extends Vote implements BatchPredictor {
 
   /** For serialization */
