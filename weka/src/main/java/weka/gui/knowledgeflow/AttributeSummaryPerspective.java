0145d8a7a81869a228469de98b2125f9e59deb86
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2016-02-22 08:55:13 +0000

    Added javadoc
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@12528 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/src/main/java/weka/gui/knowledgeflow/AttributeSummaryPerspective.java
+++ weka/src/main/java/weka/gui/knowledgeflow/AttributeSummaryPerspective.java
@@ -1,3 +1,24 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ *    AttributeSummaryPerspective.java
+ *    Copyright (C) 2015 University of Waikato, Hamilton, New Zealand
+ *
+ */
+
 package weka.gui.knowledgeflow;
 
 import weka.core.Attribute;
@@ -18,6 +39,9 @@ import java.util.List;
 import java.util.Vector;
 
 /**
+ * Knowledge Flow perspective that provides a matrix of
+ * AttributeVisualizationPanels
+ * 
  * @author Mark Hall (mhall{[at]}pentaho{[dot]}com)
  * @version $Revision: $
  */
@@ -26,31 +50,45 @@ import java.util.Vector;
   iconPath = "weka/gui/knowledgeflow/icons/chart_bar.png")
 public class AttributeSummaryPerspective extends AbstractPerspective {
 
+  private static final long serialVersionUID = 6697308901346612850L;
+
+  /** The dataset being visualized */
   protected Instances m_visualizeDataSet;
+
+  /** Holds the grid of attribute histogram panels */
   protected transient List<AttributeVisualizationPanel> m_plots;
 
   /** Index on which to color the plots */
   protected int m_coloringIndex = -1;
 
+  /**
+   * Constructor
+   */
   public AttributeSummaryPerspective() {
     setLayout(new BorderLayout());
   }
 
+  /**
+   * Setup the panel using the supplied settings
+   *
+   * @param settings the settings to use
+   */
   protected void setup(Settings settings) {
     removeAll();
     if (m_visualizeDataSet == null) {
       return;
     }
 
-    JScrollPane hp = makePanel(
-      settings == null ? m_mainApplication.getApplicationSettings() : settings);
+    JScrollPane hp =
+      makePanel(settings == null ? m_mainApplication.getApplicationSettings()
+        : settings);
     add(hp, BorderLayout.CENTER);
 
     Vector<String> atts = new Vector<String>();
     for (int i = 0; i < m_visualizeDataSet.numAttributes(); i++) {
-      atts
-        .add("(" + Attribute.typeToStringShort(m_visualizeDataSet.attribute(i))
-          + ") " + m_visualizeDataSet.attribute(i).name());
+      atts.add("("
+        + Attribute.typeToStringShort(m_visualizeDataSet.attribute(i)) + ") "
+        + m_visualizeDataSet.attribute(i).name());
     }
 
     final JComboBox<String> classCombo = new JComboBox<String>();
@@ -90,6 +128,13 @@ public class AttributeSummaryPerspective extends AbstractPerspective {
     });
   }
 
+  /**
+   * Makes the scrollable panel containing the grid of attribute
+   * visualizations
+   *
+   * @param settings the settings to use
+   * @return the configured panel
+   */
   private JScrollPane makePanel(Settings settings) {
     String fontFamily = this.getFont().getFamily();
     Font newFont = new Font(fontFamily, Font.PLAIN, 10);
@@ -114,8 +159,8 @@ public class AttributeSummaryPerspective extends AbstractPerspective {
     for (int i = 0; i < numPlots; i++) {
       JPanel temp = new JPanel();
       temp.setLayout(new BorderLayout());
-      temp.setBorder(BorderFactory
-        .createTitledBorder(m_visualizeDataSet.attribute(i).name()));
+      temp.setBorder(BorderFactory.createTitledBorder(m_visualizeDataSet
+        .attribute(i).name()));
 
       AttributeVisualizationPanel ap = new AttributeVisualizationPanel();
       m_plots.add(ap);
@@ -140,32 +185,64 @@ public class AttributeSummaryPerspective extends AbstractPerspective {
     return scroller;
   }
 
+  /**
+   * Get the default settings for this perspective
+   *
+   * @return the default settings for this perspective
+   */
   @Override
   public Defaults getDefaultSettings() {
     return new AttDefaults();
   }
 
+  /**
+   * Set the instances to visualize
+   *
+   * @param instances the instances the instances to visualize
+   */
   @Override
   public void setInstances(Instances instances) {
     m_visualizeDataSet = instances;
     setup(null);
   }
 
+  /**
+   * Set the instances to visualize
+   *
+   * @param instances the instances to visualize
+   * @param settings the settings to use
+   */
   public void setInstances(Instances instances, Settings settings) {
     m_visualizeDataSet = instances;
     setup(settings);
   }
 
+  /**
+   * Returns true if this perspective is OK with being an active perspective -
+   * i.e. the user can click on this perspective at this time in the perspective
+   * toolbar. For example, a Perspective might return false from this method if
+   * it needs a set of instances to operate but none have been supplied yet.
+   *
+   * @return true if this perspective can be active at the current time
+   */
   @Override
   public boolean okToBeActive() {
     return m_visualizeDataSet != null;
   }
 
+  /**
+   * Returns true, as this perspective does accept instances
+   *
+   * @return true
+   */
   @Override
   public boolean acceptsInstances() {
     return true;
   }
 
+  /**
+   * Default settings for the AttributeSummaryPerspective
+   */
   public static class AttDefaults extends Defaults {
 
     public static final String ID = "attributesummary";
--- weka/src/main/java/weka/gui/knowledgeflow/BaseInteractiveViewer.java
+++ weka/src/main/java/weka/gui/knowledgeflow/BaseInteractiveViewer.java
@@ -1,23 +1,53 @@
+/*
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * BaseInteractiveViewer.java
+ * Copyright (C) 2015 University of Waikato, Hamilton, New Zealand
+ */
+
 package weka.gui.knowledgeflow;
 
-import weka.core.Defaults;
-import weka.core.Settings;
-import weka.gui.SettingsEditor;
-import weka.knowledgeflow.steps.Step;
-
-import javax.swing.JButton;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import java.awt.Window;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.IOException;
 
+import javax.swing.*;
+
+import weka.core.Defaults;
+import weka.core.Settings;
+import weka.gui.SettingsEditor;
+import weka.knowledgeflow.steps.Step;
+
 /**
+ * Base class than clients can extend when implementing
+ * {@code StepInteractiveViewer}. Provides a {@code BorderLayout} with a close
+ * button in the south position. Also provides a {@code addButton()} method that
+ * subclasses can call to add additional buttons to the bottom of the window. If
+ * the subclass returns a default settings object from the
+ * {@code getDefaultSettings()} method, then a settings button will also appear
+ * at the bottom of the window - this will pop up a settings editor. In this
+ * case, the subclass should also override the no-op {@code applySettings()}
+ * method in order to apply any settings changes that the user might have made
+ * in the settings editor. There is also a {@code closePressed()} method that is
+ * called when the close button is pressed. Subclasses can override this method
+ * as necessary.
+ * 
  * @author Mark Hall (mhall{[at]}pentaho{[dot]}com)
  * @version $Revision: $
  */
@@ -26,15 +56,24 @@ public abstract class BaseInteractiveViewer extends JPanel implements
 
   private static final long serialVersionUID = -1191494001428785466L;
 
+  /** Holds the step that this interactive viewer relates to */
   protected Step m_step;
 
+  /** The close button, for closing the viewer */
   protected JButton m_closeBut = new JButton("Close");
+
+  /** Holds buttons displayed at the bottom of the window */
   protected JPanel m_buttonHolder = new JPanel(new GridLayout());
 
+  /** The parent window */
   protected Window m_parent;
 
+  /** The main Knowledge Flow perspective */
   protected MainKFPerspective m_mainPerspective;
 
+  /**
+   * Constructor
+   */
   public BaseInteractiveViewer() {
     super();
     setLayout(new BorderLayout());
@@ -74,15 +113,33 @@ public abstract class BaseInteractiveViewer extends JPanel implements
     }
   }
 
+  /**
+   * Get the settings object for the application
+   *
+   * @return the settings object the settings object
+   */
   @Override
   public Settings getSettings() {
     return m_mainPerspective.getMainApplication().getApplicationSettings();
   }
 
+  /**
+   * No-op implementation. Subcasses should override to be notified about, and
+   * apply, any changed settings
+   * 
+   * @param settings the settings object that might (or might not) have been
+   *          altered by the user
+   */
   public void applySettings(Settings settings) {
 
   }
 
+  /**
+   * Set the main knowledge flow perspective. Implementations can then access
+   * application settings if necessary
+   *
+   * @param perspective the main knowledge flow perspective
+   */
   @Override
   public void setMainKFPerspective(MainKFPerspective perspective) {
     m_mainPerspective = perspective;
@@ -91,25 +148,51 @@ public abstract class BaseInteractiveViewer extends JPanel implements
       .applyDefaults(getDefaultSettings());
   }
 
+  /**
+   * Get the main knowledge flow perspective. Implementations can the access
+   * application settings if necessary
+   *
+   * @return
+   */
   @Override
   public MainKFPerspective getMainKFPerspective() {
     return m_mainPerspective;
   }
 
+  /**
+   * Set the step that owns this viewer. Implementations may want to access data
+   * that has been computed by the step in question.
+   *
+   * @param theStep the step that owns this viewer
+   */
   @Override
   public void setStep(Step step) {
     m_step = step;
   }
 
+  /**
+   * Get the step that owns this viewer.
+   *
+   * @return the {@code Step} that owns this viewer
+   */
   public Step getStep() {
     return m_step;
   }
 
+  /**
+   * Called by the KnowledgeFlow application once the enclosing JFrame is
+   * visible
+   */
   @Override
   public void nowVisible() {
     // no-op. Subclasses to override if necessary
   }
 
+  /**
+   * Set the parent window for this viewer
+   *
+   * @param parent the parent window
+   */
   @Override
   public void setParentWindow(Window parent) {
     m_parent = parent;
@@ -122,6 +205,11 @@ public abstract class BaseInteractiveViewer extends JPanel implements
     });
   }
 
+  /**
+   * Adds a button to the bottom of the window.
+   *
+   * @param button the button to add.
+   */
   public void addButton(JButton button) {
     m_buttonHolder.add(button);
   }
@@ -134,11 +222,23 @@ public abstract class BaseInteractiveViewer extends JPanel implements
     }
   }
 
+  /**
+   * Called when the close button is pressed. Subclasses should override if they
+   * need to do something before the window is closed
+   */
   public void closePressed() {
     // subclasses to override if they need to do something before
     // the window is closed
   }
 
+  /**
+   * Get default settings for the viewer. Default implementation returns null,
+   * i.e. no default settings. Subclasses can override if they have settings
+   * (with default values) that the user can edit.
+   *
+   * @return the default settings for this viewer, or null if there are no
+   *         user-editable settings
+   */
   public Defaults getDefaultSettings() {
     // subclasses to override if they have default settings
     return null;
