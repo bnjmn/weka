5144e3d64a37da1ac38769c4422ca3ec00f82abe
eibe <eibe@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2016-08-24 23:30:23 +0000

    New attempt. We do need the hack after all because we do need the TigerJython menu bar, just not as the main Mac menu bar. Now setting the OS to Darwin before TigerJython is started, which seems to do the trick. Darwin is a valid OS, in contrast to pretending_not_to_be_a_mac.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@13196 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/src/main/java/weka/gui/GUIChooserApp.java
+++ weka/src/main/java/weka/gui/GUIChooserApp.java
@@ -996,8 +996,22 @@ public class GUIChooserApp extends JFrame {
               Class.forName("tigerjython.core.TigerJython");
             Object[] args = new Object[1];
             args[0] = new String[0];
-            tigerJythonClass.getMethod("main", String[].class).invoke(null, args);
+            if (System.getProperty("os.name").toLowerCase().startsWith("mac")) {
 
+              // Awful hack to prevent TigerJython from taking over the Mac menu
+              // bar.
+              // Could potentially cause problems due to multi-threading, but
+              // hopefully
+              // not problematic in practice.
+              String realOS = System.getProperty("os.name");
+              System.setProperty("os.name", "Darwin");
+              tigerJythonClass.getMethod("main", String[].class).invoke(null,
+                      args);
+              System.setProperty("os.name", realOS);
+            } else {
+              tigerJythonClass.getMethod("main", String[].class).invoke(null,
+                      args);
+            }
           } catch (Exception ex) {
 
            // Default to built-in console
