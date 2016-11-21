c909585975e19c72764c9e0afdeb680764c854a3
fracpete <fracpete@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2016-11-21 23:58:41 +0000

    now handles string and relation-valued class attributes
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@13618 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/src/main/java/weka/filters/unsupervised/attribute/PartitionedMultiFilter.java
+++ weka/src/main/java/weka/filters/unsupervised/attribute/PartitionedMultiFilter.java
@@ -15,16 +15,11 @@
 
 /*
  * PartitionedMultiFilter.java
- * Copyright (C) 2006-2012 University of Waikato, Hamilton, New Zealand
+ * Copyright (C) 2006-2016 University of Waikato, Hamilton, New Zealand
  *
  */
 
 package weka.filters.unsupervised.attribute;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.Vector;
 
 import weka.core.Attribute;
 import weka.core.DenseInstance;
@@ -39,6 +34,11 @@ import weka.core.Utils;
 import weka.filters.AllFilter;
 import weka.filters.Filter;
 import weka.filters.SimpleBatchFilter;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Vector;
 
 /**
  * <!-- globalinfo-start --> A filter that applies filters on subsets of
@@ -746,7 +746,13 @@ public class PartitionedMultiFilter extends SimpleBatchFilter {
 
       // class
       if (instances.classIndex() > -1) {
-        values[values.length - 1] = inst.value(instances.classIndex());
+	index = values.length - 1;
+	if (result.attribute(index).isString())
+	  values[index] = result.attribute(index).addStringValue(inst.stringValue(instances.classIndex()));
+	else if (result.attribute(index).isRelationValued())
+	  values[index] = result.attribute(index).addRelation(inst.relationalValue(instances.classIndex()));
+	else
+	  values[index] = inst.value(instances.classIndex());
       }
 
       // generate and add instance
