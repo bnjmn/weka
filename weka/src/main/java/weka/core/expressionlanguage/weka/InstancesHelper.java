1c65b41ae79f43417143e942aaedf0d171bd7b66
mhall <mhall@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2017-11-17 00:57:15 +0000

    Single argument constructor now keeps just a copy of the dataset's attribute information in memory rather than a reference to the full dataset. Added a constructor that takes an additional boolean argument to specify whether all the instances or just the attribute information should be retained. setInstance(i) now checks the flag and raises an UnsupportedOperationException if the full dataset is not available.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@14522 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/src/main/java/weka/core/expressionlanguage/weka/InstancesHelper.java
+++ weka/src/main/java/weka/core/expressionlanguage/weka/InstancesHelper.java
@@ -39,18 +39,20 @@ import weka.core.expressionlanguage.common.Primitives.StringExpression;
  * A helper class to expose instance values and macros for instance values to a
  * program</p>
  * 
- * Exposes instance values of the current instance (see {@link #setInstance(Instance)}
- * and {@link #setInstance(int)} methods) as a1, A1 or ATT1 etc where the number
- * is the attribute index (starting with 1).</br>
- * Furthermore exposes the class value as CLASS.</p>
+ * Exposes instance values of the current instance (see
+ * {@link #setInstance(Instance)} and {@link #setInstance(int)} methods) as a1,
+ * A1 or ATT1 etc where the number is the attribute index (starting with
+ * 1).</br> Furthermore exposes the class value as CLASS.</p>
  * 
- * Exposes the '<code>ismissing</code>' macro which can only be applied to 
- * instance values and returns whether the value is set as missing in the current instance.</p>
+ * Exposes the '<code>ismissing</code>' macro which can only be applied to
+ * instance values and returns whether the value is set as missing in the
+ * current instance.</p>
  * 
  * @author Benjamin Weber ( benweber at student dot ethz dot ch )
  * @version $Revision: 1000 $
  */
-public class InstancesHelper implements VariableDeclarations, Macro, MacroDeclarations {
+public class InstancesHelper implements VariableDeclarations, Macro,
+  MacroDeclarations {
 
   // from MathExpression filter and AttributeExpression
   private static final Pattern ATTRIBUTE1 = Pattern.compile("[aA][0-9]+");
@@ -68,21 +70,44 @@ public class InstancesHelper implements VariableDeclarations, Macro, MacroDeclar
 
   /** the dataset whose instance values should be exposed */
   private final Instances dataset;
-  
+
   /** the current instance whose values should be exposed */
   private Instance instance;
-  
+
   /** whether a missing value has been evaluated during computation */
   private boolean missingAccessed = false;
-  
+
   /**
-   * Constructs an {@link InstancesHelper} for the given dataset
+   * True if this instance retains the full dataset rather than just the
+   * attribute information
+   */
+  private final boolean dataRetained;
+
+  /**
+   * Constructs an {@link InstancesHelper} for the given dataset. Only attribute
+   * information is retained in memory.
    * 
-   * @param dataset the dataset whose instance values should be exposed
+   * @param dataset the dataset whose attribute values should be exposed
    */
   public InstancesHelper(Instances dataset) {
+    this(dataset, false);
+  }
+
+  /**
+   * Constructs an {@link InstancesHelper} for the given dataset.
+   * 
+   * @param dataset the dataset
+   * @param retainData true if the full dataset (rather than just the attribute
+   *          information) is to be retained
+   */
+  public InstancesHelper(Instances dataset, boolean retainData) {
     assert dataset != null;
-    this.dataset = dataset;
+    if (retainData) {
+      this.dataset = dataset;
+    } else {
+      this.dataset = new Instances(dataset, 0);
+    }
+    dataRetained = retainData;
   }
 
   /**
@@ -95,7 +120,7 @@ public class InstancesHelper implements VariableDeclarations, Macro, MacroDeclar
   public boolean hasMacro(String name) {
     return IS_MISSING.equals(name);
   }
-  
+
   /**
    * Tries to fetch a macro</p>
    * 
@@ -110,7 +135,7 @@ public class InstancesHelper implements VariableDeclarations, Macro, MacroDeclar
       return this;
     throw new RuntimeException("Macro '" + name + "' undefined!");
   }
-  
+
   /**
    * Evaluates the 'ismissing' macro
    * 
@@ -119,12 +144,14 @@ public class InstancesHelper implements VariableDeclarations, Macro, MacroDeclar
    */
   public Node evaluate(Node... params) throws SemanticException {
     if (params.length != 1)
-      throw new SemanticException("Macro " + IS_MISSING + " takes exactly one argument!");
+      throw new SemanticException("Macro " + IS_MISSING
+        + " takes exactly one argument!");
     if (params[0] instanceof Value)
       return new isMissing((Value) params[0]);
-    throw new SemanticException(IS_MISSING + " is only applicable to a dataset value!");
+    throw new SemanticException(IS_MISSING
+      + " is only applicable to a dataset value!");
   }
-  
+
   private static class isMissing implements BooleanExpression {
 
     private static final long serialVersionUID = -3805035561340865906L;
@@ -134,22 +161,30 @@ public class InstancesHelper implements VariableDeclarations, Macro, MacroDeclar
     public isMissing(Value value) {
       this.value = value;
     }
-    
+
     @Override
     public boolean evaluate() {
       return value.isMissing();
     }
   }
-  
+
   /**
-   * Sets the instance at index i of the supplied dataset to be the current instance
+   * Sets the instance at index i of the supplied dataset to be the current
+   * instance
    * 
    * @param i the index of the instance to be set
+   * @throws UnsupportedOperationException if the full dataset has not been
+   *           retained in memory
    */
   public void setInstance(int i) {
+    if (!dataRetained) {
+      throw new UnsupportedOperationException(
+        "Unable to set the instance based "
+          + "on index because the dataset has not been retained in memory");
+    }
     setInstance(dataset.get(i));
   }
-  
+
   /**
    * Sets the current instance to be the supplied instance
    * 
@@ -160,12 +195,12 @@ public class InstancesHelper implements VariableDeclarations, Macro, MacroDeclar
     this.instance = instance;
     missingAccessed = false;
   }
-  
+
   /**
    * Whether a missing value has been evaluated during computation.</p>
    * 
-   * Will be reset when the {@link #setInstance(int)} or {@link #setInstance(Instance)}
-   * method is called.
+   * Will be reset when the {@link #setInstance(int)} or
+   * {@link #setInstance(Instance)} method is called.
    * 
    * @return whether a missing value has been evaluated during computation
    */
@@ -174,17 +209,17 @@ public class InstancesHelper implements VariableDeclarations, Macro, MacroDeclar
   }
 
   private int getIndex(String attribute) {
- 
+
     if (ATTRIBUTE1.matcher(attribute).matches())
       return Integer.parseInt(attribute.substring(1)) - 1;
     if (ATTRIBUTE2.matcher(attribute).matches())
       return Integer.parseInt(attribute.substring(3)) - 1;
     if (CLASS.equals(attribute))
       return dataset.classIndex();
-    
+
     return -1;
   }
-  
+
   /**
    * Returns whether the variable is declared
    * 
@@ -199,7 +234,7 @@ public class InstancesHelper implements VariableDeclarations, Macro, MacroDeclar
       return true;
     return false;
   }
-  
+
   /**
    * Tries to fetch a variable of an instance value</p>
    * 
@@ -217,22 +252,23 @@ public class InstancesHelper implements VariableDeclarations, Macro, MacroDeclar
 
     if (dataset.attribute(index).isNumeric())
       return new DoubleValue(index);
-    if (dataset.attribute(index).isString() || dataset.attribute(index).isNominal())
+    if (dataset.attribute(index).isString()
+      || dataset.attribute(index).isNominal())
       return new StringValue(index);
-    throw new RuntimeException("Attributes of type '" +
-      dataset.attribute(index).toString() + "' not supported!");
+    throw new RuntimeException("Attributes of type '"
+      + dataset.attribute(index).toString() + "' not supported!");
   }
-  
+
   private abstract class Value implements Node {
 
     private static final long serialVersionUID = 5839070716097467627L;
 
     private final int index;
-    
+
     public Value(int index) {
       this.index = index;
     }
-    
+
     public int getIndex() {
       return index;
     }
@@ -241,7 +277,7 @@ public class InstancesHelper implements VariableDeclarations, Macro, MacroDeclar
       return instance.isMissing(getIndex());
     }
   }
-  
+
   private class DoubleValue extends Value implements DoubleExpression {
 
     private static final long serialVersionUID = -1001674545929082424L;
@@ -250,7 +286,7 @@ public class InstancesHelper implements VariableDeclarations, Macro, MacroDeclar
       super(index);
       assert dataset.attribute(getIndex()).isNumeric();
     }
-    
+
     @Override
     public double evaluate() {
       if (isMissing()) {
@@ -260,16 +296,17 @@ public class InstancesHelper implements VariableDeclarations, Macro, MacroDeclar
       return instance.value(getIndex());
     }
   }
-  
+
   private class StringValue extends Value implements StringExpression {
 
     private static final long serialVersionUID = -249974216283801876L;
 
     public StringValue(int index) {
       super(index);
-      assert dataset.attribute(index).isString() || dataset.attribute(index).isNominal();
+      assert dataset.attribute(index).isString()
+        || dataset.attribute(index).isNominal();
     }
-    
+
     @Override
     public String evaluate() {
       if (isMissing()) {
