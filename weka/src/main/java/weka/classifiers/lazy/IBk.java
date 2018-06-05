2dc9b280a4dac6ae69615ecda5aa90ceed82aa8c
eibe <eibe@e0a1b77d-ad91-4216-81b1-defd5f83fa92>
2018-06-05 21:36:08 +0000

    Fixed bug in specification of number of arguments for neighbour search command-line option.
    git-svn-id: https://svn.cms.waikato.ac.nz/svn/weka/trunk@14826 e0a1b77d-ad91-4216-81b1-defd5f83fa92

--- weka/src/main/java/weka/classifiers/lazy/IBk.java
+++ weka/src/main/java/weka/classifiers/lazy/IBk.java
@@ -626,7 +626,7 @@ public class IBk
     newVector.addElement(new Option(
 	      "\tThe nearest neighbour search algorithm to use "+
           "(default: weka.core.neighboursearch.LinearNNSearch).\n",
-	      "A", 0, "-A"));
+	      "A", 1, "-A"));
 
     newVector.addAll(Collections.list(super.listOptions()));
     
